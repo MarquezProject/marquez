62f0064f25b861561a29c5734a869d47b8dc7607
wslulciuc <willy.lulciuc@wework.com>
2018-07-23 21:54:36 -0700

    Add owner model

---
+++ src/main/java/marquez/api/Owner.java
@@ -0,0 +1,44 @@
+package marquez.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+import javax.validation.constraints.NotNull;
+
+public class Owner {
+
+  @NotNull private final String name;
+
+  @JsonCreator
+  public Owner(@JsonProperty("name") final String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Owner)) return false;
+
+    final Owner other = (Owner) o;
+
+    return Objects.equals(name, other.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("Owner{");
+    sb.append("name=").append(name);
+    sb.append("}");
+    return sb.toString();
+  }
+}
