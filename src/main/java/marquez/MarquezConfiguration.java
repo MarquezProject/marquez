ceff03bfc49a3c3a2d699be7656c36a33a69c808
wslulciuc <willy.lulciuc@wework.com>
2018-07-27 09:27:16 -0700

    Clean up config class

--- src/main/java/marquez/MarquezConfiguration.java
+++ src/main/java/marquez/MarquezConfiguration.java
@@ -5,24 +5,14 @@ import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.NotEmpty;
 
 class MarquezConfiguration extends Configuration {
-  @NotEmpty private String url;
-
-  @Valid @NotNull @JsonProperty private DataSourceFactory database = new DataSourceFactory();
+  @Valid
+  @NotNull
+  @JsonProperty
+  private DataSourceFactory dataSourceFactory = new DataSourceFactory();
 
   public DataSourceFactory getDataSourceFactory() {
-    return database;
-  }
-
-  @JsonProperty
-  public String getUrl() {
-    return url;
-  }
-
-  @JsonProperty
-  public void setHttpURL(String Url) {
-    this.url = Url;
+    return dataSourceFactory;
   }
 }
