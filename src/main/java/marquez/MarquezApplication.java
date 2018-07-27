215e5e15e7697e6d6d1708fe3bbfbfbb3023df10
wslulciuc <willy.lulciuc@wework.com>
2018-07-27 14:04:07 -0700

    Clean up MarquezApplication

--- src/main/java/marquez/MarquezApplication.java
+++ src/main/java/marquez/MarquezApplication.java
@@ -7,11 +7,8 @@ import io.dropwizard.setup.Environment;
 import org.jdbi.v3.core.Jdbi;
 
 public class MarquezApplication extends Application<MarquezConfiguration> {
-
-  @Override
-  public void run(final MarquezConfiguration config, final Environment environment) {
-    final JdbiFactory factory = new JdbiFactory();
-    final Jdbi jdbi = factory.build(environment, config.getDataSourceFactory(), "postgresql");
+  public static void main(final String[] args) throws Exception {
+    new MarquezApplication().run(args);
   }
 
   @Override
@@ -22,7 +19,9 @@ public class MarquezApplication extends Application<MarquezConfiguration> {
   @Override
   public void initialize(Bootstrap<MarquezConfiguration> bootstrap) {}
 
-  public static void main(final String[] args) throws Exception {
-    new MarquezApplication().run(args);
+  @Override
+  public void run(final MarquezConfiguration config, final Environment environment) {
+    final JdbiFactory factory = new JdbiFactory();
+    final Jdbi jdbi = factory.build(environment, config.getDataSourceFactory(), "postgresql");
   }
 }
