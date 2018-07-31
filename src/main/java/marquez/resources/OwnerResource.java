28c3278e196a40def05cd2a2f6e55435e1a9d1c6
wslulciuc <willy.lulciuc@wework.com>
2018-07-31 09:56:37 -0700

    Simplify api resources

---
+++ src/main/java/marquez/resources/OwnerResource.java
@@ -0,0 +1,27 @@
+package marquez.resources;
+
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import marquez.api.Owner;
+import marquez.db.dao.OwnerDAO;
+
+@Path("/owners")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class OwnerResource {
+  private final OwnerDAO dao;
+
+  public OwnerResource(OwnerDAO dao) {
+    this.dao = dao;
+  }
+
+  @GET
+  public List<Owner> listDatasets() {
+    return dao.findAll();
+  }
+}
+
