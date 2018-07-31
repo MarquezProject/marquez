0d5d0ea0952a54d15f0fd8fa2af878877464f1a4
wslulciuc <willy.lulciuc@wework.com>
2018-07-31 16:31:40 -0700

    Add path to create new owners

--- src/main/java/marquez/resources/OwnerResource.java
+++ src/main/java/marquez/resources/OwnerResource.java
@@ -1,24 +1,23 @@
 package marquez.resources;
 
-import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import marquez.api.Owner;
 import marquez.db.dao.OwnerDAO;
 
 @Path("/owners")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class OwnerResource {
   private final OwnerDAO dao;
 
-  public OwnerResource(OwnerDAO dao) {
+  public OwnerResource(final OwnerDAO dao) {
     this.dao = dao;
   }
 
-
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void createOwner(final Owner owner) {
+    dao.insert(owner.getName());
+  }
 }
-
