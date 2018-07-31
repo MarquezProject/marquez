7d1a2277d3aa93524fb8b6c162e5b3a3c4a110cc
wslulciuc <willy.lulciuc@wework.com>
2018-07-30 17:50:34 -0700

    Register http resources on app run

--- src/main/java/marquez/MarquezApplication.java
+++ src/main/java/marquez/MarquezApplication.java
@@ -4,6 +4,12 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import marquez.db.DbConfiguration;
+import marquez.db.dao.DatasetDAO;
+import marquez.db.dao.JobDAO;
+import marquez.db.dao.OwnerDAO;
+import marquez.resources.JobResource;
+import marquez.resources.DatasetResource;
+import marquez.resources.OwnerResource;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.postgres.PostgresPlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -26,11 +32,23 @@ public class MarquezApplication extends Application<MarquezConfiguration> {
   @Override
   public void run(final MarquezConfiguration config, final Environment env) {
     final DbConfiguration dbConfig = config.getDbConfiguration();
-    final String connectionURL = String.format(
-        "jdbc:postgresql://%s:%d/%s", dbConfig.getHost(), dbConfig.getPort(), dbConfig.getName());
+    final String connectionURL =
+        String.format(
+            "jdbc:postgresql://%s:%d/%s",
+            dbConfig.getHost(), dbConfig.getPort(), dbConfig.getName());
+
     final Jdbi jdbi =
         Jdbi.create(connectionURL, dbConfig.getUser(), dbConfig.getPassword())
             .installPlugin(new PostgresPlugin())
             .installPlugin(new SqlObjectPlugin());
+
+    final OwnerDAO ownerDAO = jdbi.onDemand(OwnerDAO.class);
+    env.jersey().register(new OwnerResource(ownerDAO));
+
+    final JobDAO jobDAO = jdbi.onDemand(JobDAO.class);
+    env.jersey().register(new JobResource(jobDAO));
+
+    final DatasetDAO datasetDAO = jdbi.onDemand(DatasetDAO.class);
+    env.jersey().register(new DatasetResource(datasetDAO));
   }
 }
