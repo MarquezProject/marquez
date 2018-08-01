e8ab2e58a2921153b338d689f2e0954a3d6102f5
wslulciuc <willy.lulciuc@wework.com>
2018-08-01 15:47:00 -0700

    User media type constants

--- src/main/java/marquez/resources/PingResource.java
+++ src/main/java/marquez/resources/PingResource.java
@@ -3,10 +3,11 @@ package marquez.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/ping")
-@Produces("text/plain")
+@Produces(MediaType.TEXT_PLAIN)
 public class PingResource {
 
   @GET
