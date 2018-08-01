35b26a85aa07566d0e10f240475e8ebafdb62487
wslulciuc <willy.lulciuc@wework.com>
2018-08-01 15:57:47 -0700

    Minor app config change

--- src/main/java/marquez/MarquezConfiguration.java
+++ src/main/java/marquez/MarquezConfiguration.java
@@ -1,32 +1,29 @@
 package marquez;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.flyway.FlywayFactory;
 import javax.validation.constraints.NotNull;
 
-public class MarquezConfiguration extends Configuration {
-  @NotNull private DataSourceFactory database = new DataSourceFactory();
-  @NotNull private FlywayFactory flyway = new FlywayFactory();
+public final class MarquezConfiguration extends Configuration {
+  @NotNull private final DataSourceFactory database;
+  @NotNull private final FlywayFactory flyway;
 
-  @JsonProperty("database")
+  @JsonCreator
+  public MarquezConfiguration(
+      @JsonProperty("database") final DataSourceFactory database,
+      @JsonProperty("flyway") final FlywayFactory flyway) {
+    this.database = database;
+    this.flyway = flyway;
+  }
+
   public DataSourceFactory getDataSourceFactory() {
     return database;
   }
 
-  @JsonProperty("database")
-  public void setDataSourceFactory(final DataSourceFactory factory) {
-    this.database = factory;
-  }
-
-  @JsonProperty("flyway")
   public FlywayFactory getFlywayFactory() {
     return flyway;
-  }
-
-  @JsonProperty("flyway")
-  public void setFlywayFactory(final FlywayFactory factory) {
-    this.flyway = factory;
   }
 }
