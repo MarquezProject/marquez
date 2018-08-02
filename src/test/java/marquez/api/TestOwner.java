a75399f7a722a649a97d5fdc25f9dab523c6db7a
Julien Le Dem <julien.ledem@wework.com>
2018-08-02 14:36:06 +0200

    add first resource test

---
+++ src/test/java/marquez/api/TestOwner.java
@@ -0,0 +1,37 @@
+package marquez.api;
+
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import io.dropwizard.testing.junit.ResourceTestRule;
+import marquez.db.dao.OwnerDAO;
+import marquez.resources.OwnerResource;
+
+public class TestOwner {
+	private static final OwnerDAO dao = mock(OwnerDAO.class);
+
+	@ClassRule
+    public static final ResourceTestRule resources = ResourceTestRule.builder()
+            .addResource(new OwnerResource(dao))
+            .build();
+
+	private final Owner owner = new Owner("Aureliano");
+
+    @After
+    public void tearDown(){
+        reset(dao);
+    }
+
+    @Test
+    public void testPostOwner() {
+        resources.target("/owners").request().post(entity(owner, APPLICATION_JSON));
+        verify(dao).insert(owner);
+    }
+}
