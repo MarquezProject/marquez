639918aa0e6a9c1f93c9c5861604bfd65d07c7fa
wslulciuc <willy.lulciuc@wework.com>
2018-08-01 22:48:19 -0700

    Add iceberg_table_versions, db_table_versions, dbs and datasets table creation

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -24,13 +24,28 @@ CREATE TABLE ownerships (
   owner_id   INTEGER REFERENCES owners(id)
 );
 
+CREATE TABLE job_versions (
+  id             SERIAL PRIMARY KEY,
+  created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at     TIMESTAMP,
+  input_dataset  VARCHAR(64) NOT NULL,
+  output_dataset VARCHAR(64) NOT NULL,
+  job_id         INTEGER REFERENCES jobs(id),
+  git_repo_uri   VARCHAR(255),
+  git_sha        VARCHAR(255),
+  latest_run_id  INTEGER
+);
+
 CREATE TABLE job_runs (
-  id               SERIAL PRIMARY KEY,
-  created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  run_id           VARCHAR(255) NOT NULL,
-  started_at       TIMESTAMP,
-  endeded_at       TIMESTAMP,
-  latest_heartbeat TIMESTAMP
+  id                        SERIAL PRIMARY KEY,
+  created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  job_version_id            INTEGER REFERENCES job_versions(id),
+  run_id                    VARCHAR(255) NOT NULL,
+  started_at                TIMESTAMP,
+  endeded_at                TIMESTAMP,
+  input_dataset_version_id  INTEGER,
+  output_dataset_version_id INTEGER,
+  latest_heartbeat          TIMESTAMP
 );
 
 CREATE TABLE job_run_states (
@@ -38,16 +53,6 @@ CREATE TABLE job_run_states (
   transitioned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   job_run_id      INTEGER REFERENCES job_runs(id),
   state           INTEGER
-);
-
-CREATE TABLE job_versions (
-  id            SERIAL PRIMARY KEY,
-  created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at    TIMESTAMP,
-  job_id        INTEGER REFERENCES jobs(id),
-  git_repo_uri  VARCHAR(255),
-  git_sha       VARCHAR(255),
-  latest_run_id INTEGER REFERENCES job_runs(id)
 );
 
 CREATE TABLE datasets (
@@ -59,4 +64,30 @@ CREATE TABLE datasets (
   origin          INTEGER,
   current_version INTEGER,
   description     TEXT NOT NULL
+);
+
+CREATE TABLE dbs (
+  id             SERIAL PRIMARY KEY,
+  name           VARCHAR(64) NOT NULL,
+  created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  type           INTEGER,
+  connection_url VARCHAR(255),
+  description    TEXT NOT NULL
+);
+
+CREATE TABLE db_table_versions (
+  id          SERIAL PRIMARY KEY,
+  created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  dataset_id  INTEGER REFERENCES datasets(id),
+  db_id       INTEGER REFERENCES dbs(id),
+  description TEXT NOT NULL
+);
+
+CREATE TABLE iceberg_table_versions (
+  id                   SERIAL PRIMARY KEY,
+  created_at           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  dataset_id           INTEGER REFERENCES datasets(id),
+  previous_snapshot_id BIGINT,
+  current_snapshot_id  BIGINT,
+  metadata_location    VARCHAR(255)
 );
