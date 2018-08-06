48d2d9a836035413293cbd7217692bb48ab4e47d
wslulciuc <willy.lulciuc@wework.com>
2018-08-06 00:35:28 -0700

    Statically import media type

--- src/main/java/marquez/resources/JobResource.java
+++ src/main/java/marquez/resources/JobResource.java
@@ -1,5 +1,6 @@
 package marquez.resources;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static marquez.resources.ResourceUtil.buildLocation;
 
 import com.codahale.metrics.annotation.Timed;
@@ -10,13 +11,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import marquez.api.Job;
 import marquez.db.dao.JobDAO;
 
 @Path("/jobs")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
 public class JobResource {
   private final JobDAO jobDAO;
 
@@ -25,7 +25,7 @@ public class JobResource {
   }
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
+  @Consumes(APPLICATION_JSON)
   @Timed
   public Response create(final Job job) {
     jobDAO.insert(job);
@@ -47,7 +47,7 @@ public class JobResource {
 
   @PUT
   @Path("/{jobName}")
-  @Consumes(MediaType.APPLICATION_JSON)
+  @Consumes(APPLICATION_JSON)
   @Timed
   public Response update(@PathParam("jobName") final String jobName) {
     return Response.ok().build();
--- src/main/java/marquez/resources/OwnerResource.java
+++ src/main/java/marquez/resources/OwnerResource.java
@@ -1,12 +1,12 @@
 package marquez.resources;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static marquez.resources.ResourceUtil.buildLocation;
 
 import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import marquez.api.Owner;
 import marquez.db.dao.OwnerDAO;
@@ -20,7 +20,7 @@ public class OwnerResource {
   }
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
+  @Consumes(APPLICATION_JSON)
   @Timed
   public Response create(final Owner owner) {
     ownerDAO.insert(owner);
