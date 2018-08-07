e528484198bbd7ea8f468399dd157f9bf36bd325
wslulciuc <willy.lulciuc@wework.com>
2018-08-06 22:23:03 -0700

    WIP

--- src/test/java/marquez/TestJobSerialization.java
+++ src/test/java/marquez/TestJobSerialization.java
@@ -10,29 +10,5 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestJobSerialization {
-  static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
-  static final Job JOB =
-      new Job(
-          "first-best-job",
-          new Timestamp(1532038468530L),
-          new Timestamp(1532038468530L),
-          1,
-          2,
-          new Timestamp(1532036468530L),
-          "test",
-          "Best job. First job.");
-
-  @Test
-  public void testSerialization() throws Exception {
-    final String expected =
-        MAPPER.writeValueAsString(MAPPER.readValue(fixture("fixtures/job.json"), Job.class));
-    Assert.assertEquals(MAPPER.writeValueAsString(JOB), expected);
-  }
-
-  @Test
-  public void testDeserialization() throws Exception {
-    final Job resultJob = MAPPER.readValue(fixture("fixtures/job.json"), Job.class);
-    Assert.assertEquals(JOB, resultJob);
-  }
 }
