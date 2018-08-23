62cd66aa94ac3aef610bdbdb70f2d03d3594fd72
Shawn Shah <shawn.shah@wework.com>
2018-08-23 16:19:56 -0700

    implement Owner lookup by name, register RowMapper

--- src/main/java/marquez/db/dao/OwnerDAO.java
+++ src/main/java/marquez/db/dao/OwnerDAO.java
@@ -1,15 +1,22 @@
 package marquez.db.dao;
 
 import marquez.api.Owner;
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+
+
+@RegisterRowMapper(OwnerRow.class)
 public interface OwnerDAO {
-  Owner findByName(@Bind("name") String name)
+  @SqlQuery("SELECT * FROM owners WHERE name = :name")
+  Owner findByName(@Bind("name") String name);
 
   @SqlUpdate("INSERT INTO owners (name) VALUES (:name)")
   void insert(@BindBean final Owner owner);
 
   @SqlUpdate("UPDATE owners SET deleted_at=CURRENT_TIMESTAMP WHERE name = :name")
-  void delete(@BindBean("o") final String name);
+  void delete(final String name);
 }
