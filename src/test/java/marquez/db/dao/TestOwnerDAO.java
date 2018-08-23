2810f3b9762101aab8db0e8e13997502e20d09d9
Shawn Shah <shawn.shah@wework.com>
2018-08-23 16:20:42 -0700

    clear tables on test setup complete first test

--- src/test/java/marquez/db/dao/TestOwnerDAO.java
+++ src/test/java/marquez/db/dao/TestOwnerDAO.java
@@ -1,7 +1,12 @@
 package marquez.db.dao;
 
+import org.jdbi.v3.core.Jdbi;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.Before;
 
 import marquez.api.Owner;
 import marquez.db.dao.fixtures.DAOSetup;
@@ -13,8 +18,20 @@ public class TestOwnerDAO {
 
 	final OwnerDAO ownerDAO = daoSetup.onDemand(OwnerDAO.class);
 
+	@Before
+	public void setUp() {
+		Jdbi jdbi = daoSetup.getJDBI();
+		jdbi.useHandle(handle -> {
+			handle.execute("DELETE FROM ownerships");
+			handle.execute("DELETE FROM owners");
+		});
+	}
+
 	@Test
 	public void testUpdateOwner() {
-		ownerDAO.insert(new Owner("Amaranta"));
+		String name = "Amaranta";
+		ownerDAO.insert(new Owner(name));
+		Owner o = ownerDAO.findByName(name);
+		assertEquals(name, o.getName());
 	}
 }
