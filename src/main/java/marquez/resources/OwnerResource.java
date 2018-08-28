443854f874959162cef5d066d4403a255153d111
Shawn Shah <shawn.shah@wework.com>
2018-08-28 13:53:00 -0700

    fix formatting

--- src/main/java/marquez/resources/OwnerResource.java
+++ src/main/java/marquez/resources/OwnerResource.java
@@ -4,12 +4,12 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import marquez.api.Owner;
 import marquez.db.dao.OwnerDAO;
