394e81dc9f5fba000b8ff01e46e716d6a60051b6
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2018-09-05 15:38:26 -0700

    Add better version of JobRunRow and JobRunStateRow

--- src/main/java/marquez/db/dao/JobRunRow.java
+++ src/main/java/marquez/db/dao/JobRunRow.java
@@ -1,3 +1,16 @@
 package marquez.db.dao;
 
-public class JobRunRow {}
+
+import marquez.api.Job;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+final class JobRunRow implements RowMapper<Job> {
+    @Override
+    public Job map(final ResultSet rs, final StatementContext ctx) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+}
