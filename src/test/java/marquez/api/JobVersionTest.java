063f2cf79711bb4e21619e4f46874185b01c100d
Shawn Shah <shawn.shah@wework.com>
2018-09-06 16:26:06 -0700

    more JobVersion tests

--- src/test/java/marquez/api/JobVersionTest.java
+++ src/test/java/marquez/api/JobVersionTest.java
@@ -1,47 +1,57 @@
 package marquez.api;
 
-import static io.dropwizard.testing.FixtureHelpers.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.UUID;
-import java.util.Date;
-import java.sql.Timestamp;
-
-import io.dropwizard.jackson.Jackson;
-import org.junit.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import marquez.api.JobVersion;
+import io.dropwizard.jackson.Jackson;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+import org.junit.Test;
 
 public class JobVersionTest {
-    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
-    private static UUID guid = UUID.randomUUID();
-    private static UUID jobGuid = UUID.randomUUID();
-    private static UUID version = UUID.randomUUID();
-    private static UUID latestJobRunGuid = UUID.randomUUID();
-    private static String uri = "http://foo.bar";
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+  private static UUID guid = UUID.randomUUID();
+  private static UUID jobGuid = UUID.randomUUID();
+  private static UUID version = UUID.randomUUID();
+  private static UUID latestJobRunGuid = UUID.randomUUID();
+  private static String uri = "http://foo.bar";
 
-    private static final JobVersion jobVersion = buildFixture();
+  private static final JobVersion jobVersion = buildFixture();
 
-    private static JobVersion buildFixture() {
-        return new JobVersion(
-            guid, 
-            jobGuid, 
-            uri, 
-            version, 
-            latestJobRunGuid, 
-            new Timestamp(new Date(0).getTime()), 
-            new Timestamp(new Date(0).getTime()));
-    }
+  private static JobVersion buildFixture() {
+    return new JobVersion(
+        guid,
+        jobGuid,
+        uri,
+        version,
+        latestJobRunGuid,
+        new Timestamp(new Date(0).getTime()),
+        new Timestamp(new Date(0).getTime()));
+  }
 
-    @Test
-    public void testEquals() {
-        assertThat(jobVersion).isEqualTo(buildFixture());
-        assertThat(buildFixture()).isEqualTo(jobVersion);
-    }
+  private static JobVersion buildRandom() {
+    return new JobVersion(
+        UUID.randomUUID(),
+        UUID.randomUUID(),
+        String.format("http://%d.bar", new Random().nextInt()),
+        UUID.randomUUID(),
+        UUID.randomUUID(),
+        new Timestamp(new Date(0).getTime()),
+        new Timestamp(new Date(0).getTime()));
+  }
 
-    @Test
-    public void testHashCode() {
-        assertThat(jobVersion.hashCode()).isEqualTo(buildFixture().hashCode());
-    }
+  @Test
+  public void testEquals() {
+    assertThat(jobVersion).isEqualTo(buildFixture());
+    assertThat(buildFixture()).isEqualTo(jobVersion);
+    assertThat(jobVersion).isNotEqualTo(buildRandom());
+  }
+
+  @Test
+  public void testHashCode() {
+    assertThat(jobVersion.hashCode()).isEqualTo(buildFixture().hashCode());
+    assertThat(jobVersion.hashCode()).isNotEqualTo(buildRandom().hashCode());
+  }
 }
