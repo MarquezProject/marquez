487ffa55e73cce0ada27e3ca6e3af89bf71415c0
Shawn Shah <shawn.shah@wework.com>
2018-09-06 14:31:00 -0700

    Simplify JobDAO insert for v1, comment ownership, set up JobRow mapper

--- src/main/java/marquez/db/dao/JobDAO.java
+++ src/main/java/marquez/db/dao/JobDAO.java
@@ -5,12 +5,13 @@ import marquez.api.Job;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.CreateSqlObject;
 import org.jdbi.v3.sqlobject.SqlObject;
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// @RegisterRowMapper(JobRow.class)
+@RegisterRowMapper(JobRow.class)
 public interface JobDAO extends SqlObject {
   static final Logger LOG = LoggerFactory.getLogger(JobDAO.class);
 
@@ -21,12 +22,11 @@ public interface JobDAO extends SqlObject {
     try (final Handle handle = getHandle()) {
       handle.useTransaction(
           h -> {
-            h.createUpdate(
-                    "INSERT INTO jobs (name, nominal_time, category, description)"
-                        + " VALUES (:name, :nominalTime, :category, :description)")
-                .bindBean(job)
+            h.createUpdate("INSERT INTO jobs (guid, name)" + " VALUES (:guid, :name)")
+                .bind("guid", job.getGuid())
+                .bind("name", job.getName())
                 .execute();
-            createOwnershipDAO().insert(job.getName(), job.getOwnerName());
+            // createOwnershipDAO().insert(job.getName(), job.getOwnerName());
             h.commit();
           });
     } catch (Exception e) {
