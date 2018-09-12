258e1259fb9f4af244a390bd276417bc4f50c348
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2018-09-12 10:36:01 -0700

    Added some better testing

--- src/test/java/marquez/api/JobRunIntegrationTest.java
+++ src/test/java/marquez/api/JobRunIntegrationTest.java
@@ -4,12 +4,15 @@ import static java.lang.String.format;
 import static marquez.api.JobRunState.State.fromInt;
 import static marquez.api.JobRunState.State.toInt;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.io.IOException;
 import java.net.URI;
 import java.sql.Timestamp;
 import java.util.UUID;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import marquez.db.dao.JobRunDAO;
 import marquez.db.dao.JobRunStateDAO;
@@ -20,8 +23,6 @@ import org.slf4j.LoggerFactory;
 
 public class JobRunIntegrationTest {
   private static Logger LOG = LoggerFactory.getLogger(JobRunIntegrationTest.class);
-
-  static final String JOB_RUN_TEST_PREFIX = "job_run_test_";
 
   static final String TEST_JOB_GUID = "063d736b-8232-422b-9a39-20310f72cbdd";
   static final String TEST_JOB_RUN_VERSION_GUID = "ba53171c-9e19-4c1b-a7cd-592b7400dea9";
@@ -41,22 +42,26 @@ public class JobRunIntegrationTest {
             handle -> {
               handle.execute(
                   format(
-                      "merge into jobs (guid, name, category, description) values ('%s', 'my_job', 'testing', 'fake job for reference');",
+                      "merge into jobs (guid, name, category, description) values "
+                          + "('%s', 'my_job', 'testing', 'fake job for reference');",
                       TEST_JOB_GUID));
               handle.execute(
                   format(
-                      "merge into job_versions (guid, input_dataset, output_dataset, job_guid) values ('%s', 'input_set1', 'output_set1', '%s');",
+                      "merge into job_versions (guid, input_dataset, output_dataset, job_guid) values "
+                          + "('%s', 'input_set1', 'output_set1', '%s');",
                       TEST_JOB_RUN_VERSION_GUID, TEST_JOB_GUID));
               handle.execute(
                   format(
-                      "merge into job_run_definitions (guid, job_version_guid, run_args_json, nominal_time) values ('%s', '%s', '--my-favorite-flag', '2018-09-06T05:24:50+00:00');",
+                      "merge into job_run_definitions (guid, job_version_guid, run_args_json, nominal_time) values "
+                          + "('%s', '%s', '--my-favorite-flag', '2018-09-06T05:24:50+00:00');",
                       TEST_JOB_RUN_DEFINITION_GUID, TEST_JOB_RUN_VERSION_GUID));
             });
   }
 
   @AfterClass
   public static void tearDown() {
-    daoSetup
+    //TODO: Fix this.
+    /*daoSetup
         .getJDBI()
         .useHandle(
             handle -> {
@@ -68,6 +73,7 @@ public class JobRunIntegrationTest {
                   format("delete from job_versions where guid = '%s'", TEST_JOB_RUN_VERSION_GUID));
               handle.execute(format("delete from jobs where guid = '%s'", TEST_JOB_GUID));
             });
+            */
   }
 
   @Before
@@ -131,20 +137,37 @@ public class JobRunIntegrationTest {
     assertEquals(JobRunState.State.NEW, fromInt(returnedJobRun.getCurrentState()));
   }
 
-  @Ignore("in development")
+  // @Ignore("in development")
   @Test
-  public void testJobRunCreationEndToEnd() {
+  public void testJobRunCreationEndToEnd() throws IOException {
+    final String jobRunRequestString =
+        format("{\"job_run_definition_guid\": \"%s\"}", TEST_JOB_RUN_DEFINITION_GUID);
+    Entity jobRunRequestJsonAsEntity = Entity.json(jobRunRequestString);
     final Response res =
         daoSetup
             .client()
             .target(URI.create("http://localhost:" + daoSetup.getLocalPort()))
             .path("/job_runs")
-            .request()
-            .post(
-                Entity.json(
-                    format("{'job_run_definition_guid' : '%s'}", TEST_JOB_RUN_DEFINITION_GUID)));
+            .request(MediaType.APPLICATION_JSON)
+            .post(jobRunRequestJsonAsEntity);
     assertEquals(201, res.getStatus());
-    assertEquals("pong", res.readEntity(String.class));
+    CreateJobRunResponse responseBody = res.readEntity(CreateJobRunResponse.class);
+    UUID returnedId = responseBody.getExternalGuid();
+    try {
+      assertNotNull(returnedId);
+      LOG.info("Returned id is: " + returnedId);
+    } finally {
+      daoSetup
+          .getJDBI()
+          .useHandle(
+              handle -> {
+                handle.execute(
+                    format(
+                        "delete from job_run_states where job_run_guid = '%s'",
+                            returnedId.toString()));
+                handle.execute(format("delete from job_runs where guid = '%s'", returnedId.toString()));
+              });
+    }
   }
 
   @Ignore("in development")
