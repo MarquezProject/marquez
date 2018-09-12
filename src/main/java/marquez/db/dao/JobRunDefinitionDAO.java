a8a5a3dbd8f587f504dbaa204f914cbef9717c72
Shawn Shah <shawn.shah@wework.com>
2018-09-12 11:25:56 -0700

    duplicate suppression for Job Run Definition

--- src/main/java/marquez/db/dao/JobRunDefinitionDAO.java
+++ src/main/java/marquez/db/dao/JobRunDefinitionDAO.java
@@ -1,19 +1,23 @@
 package marquez.db.dao;
 
 import java.util.UUID;
-import marquez.api.Owner;
+import marquez.api.JobRunDefinition;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface JobRunDefinitionDAO {
   @SqlQuery("SELECT * FROM job_run_definitions WHERE guid = :guid")
-  Owner findByName(@Bind("guid") UUID guid);
+  JobRunDefinition findByName(@Bind("guid") UUID guid);
+
+  @SqlQuery("SELECT * FROM job_run_definitions WHERE hash = :hash")
+  JobRunDefinition findByHash(@Bind("hash") UUID hash);
 
   @SqlUpdate(
-      "INSERT INTO job_run_definitions (guid, job_version_guid, run_args_json) VALUES (:guid, :job_version_guid, :run_args)")
+      "INSERT INTO job_run_definitions (guid, hash, job_version_guid, run_args_json) VALUES (:guid, :hash, :job_version_guid, :run_args)")
   void insert(
       @Bind("guid") final UUID guid,
+      @Bind("hash") final UUID definitionHash,
       @Bind("job_version_guid") final UUID job_version_guid,
       @Bind("run_args") final String runArgsJson);
 }
