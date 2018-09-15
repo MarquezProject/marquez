5711eb4a8769e4e60b4f309b8990f8cf099d433d
Shawn Shah <shawn.shah@wework.com>
2018-09-15 08:31:59 -0700

    pull additional fields from jobs and job_versions for findBy* methods

--- src/main/java/marquez/db/dao/JobRunDefinitionDAO.java
+++ src/main/java/marquez/db/dao/JobRunDefinitionDAO.java
@@ -9,10 +9,12 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 @RegisterRowMapper(JobRunDefinitionRow.class)
 public interface JobRunDefinitionDAO {
-  @SqlQuery("SELECT jrd.guid, jrd.job_version_guid, jrd.run_args_json, jrd.nominal_time, j.name, j.current_ownership, jv.uri FROM job_run_definitions jrd INNER JOIN job_versions jv ON (jv.guid = jrd.job_version_guid) INNER JOIN jobs j ON(j.guid = jv.job_guid) WHERE jrd.guid = :guid")
+  @SqlQuery(
+      "SELECT jrd.guid, jrd.job_version_guid, jrd.run_args_json, jrd.nominal_time, j.name, j.current_ownership, jv.uri FROM job_run_definitions jrd INNER JOIN job_versions jv ON (jv.guid = jrd.job_version_guid) INNER JOIN jobs j ON(j.guid = jv.job_guid) WHERE jrd.guid = :guid")
   JobRunDefinition findByGuid(@Bind("guid") UUID guid);
 
-  @SqlQuery("SELECT * FROM job_run_definitions WHERE content_hash = :hash")
+  @SqlQuery(
+      "SELECT jrd.guid, jrd.job_version_guid, jrd.run_args_json, jrd.nominal_time, j.name, j.current_ownership, jv.uri FROM job_run_definitions jrd INNER JOIN job_versions jv ON (jv.guid = jrd.job_version_guid) INNER JOIN jobs j ON(j.guid = jv.job_guid) WHERE jrd.content_hash = :hash")
   JobRunDefinition findByHash(@Bind("hash") UUID hash);
 
   @SqlUpdate(
