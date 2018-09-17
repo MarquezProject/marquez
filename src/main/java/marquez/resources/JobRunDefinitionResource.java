c15e43818cc963f76e7e4da1a3e9c43148fe387d
Shawn Shah <shawn.shah@wework.com>
2018-09-17 12:24:45 -0700

    refactor Job Run Definition creation

--- src/main/java/marquez/resources/JobRunDefinitionResource.java
+++ src/main/java/marquez/resources/JobRunDefinitionResource.java
@@ -3,7 +3,6 @@ package marquez.resources;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import com.codahale.metrics.annotation.Timed;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.UUID;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
@@ -42,6 +41,59 @@ public final class JobRunDefinitionResource extends BaseResource {
     this.ownerDAO = ownerDAO;
   }
 
+  private Owner findOrCreateOwner(final String ownerName) {
+    Owner owner = this.ownerDAO.findByName(ownerName);
+    if (owner == null) {
+      owner = new Owner(ownerName);
+      this.ownerDAO.insert(UUID.randomUUID(), owner);
+    }
+    return owner;
+  }
+
+  private Job findOrCreateJob(final String jobName, Owner owner) {
+    Job matchingJob = this.jobDAO.findByName(jobName);
+    if (matchingJob != null) {
+      return matchingJob;
+    }
+    UUID jobGuid = UUID.randomUUID();
+    Job newJob = new Job(jobGuid, jobName, owner.getName(), null, null, null);
+    this.jobDAO.insert(newJob);
+    return newJob;
+  }
+
+  private UUID findOrCreateJobVersion(CreateJobRunDefinitionRequest request, Job job) {
+    JobVersion reqJV = JobVersion.create(request);
+    UUID computedVersion = reqJV.computeVersionGuid();
+    JobVersion matchingJobVersion = this.jobVersionDAO.findByVersion(computedVersion);
+    if (matchingJobVersion != null) {
+      return matchingJobVersion.getGuid();
+    }
+    UUID jobVersionGuid = UUID.randomUUID();
+    this.jobVersionDAO.insert(jobVersionGuid, computedVersion, job.getGuid(), request.getURI());
+    return jobVersionGuid;
+  }
+
+  private JobRunDefinition findOrCreateJobRunDefinition(
+      CreateJobRunDefinitionRequest request, UUID jobVersionGuid) {
+    JobRunDefinition reqJrd = JobRunDefinition.create(request, jobVersionGuid);
+    UUID definitionHash = reqJrd.computeDefinitionHash();
+    JobRunDefinition existingJrd = jobRunDefDAO.findByHash(definitionHash);
+
+    if (existingJrd != null) {
+      return existingJrd;
+    }
+
+    UUID jobRunDefGuid = UUID.randomUUID();
+    this.jobRunDefDAO.insert(
+        jobRunDefGuid,
+        definitionHash,
+        jobVersionGuid,
+        request.getRunArgsJson(),
+        request.getNominalStartTime(),
+        request.getNominalEndTime());
+    return jobRunDefDAO.findByGuid(jobRunDefGuid);
+  }
+
   @POST
   @Consumes(APPLICATION_JSON)
   @Produces(APPLICATION_JSON)
@@ -53,69 +105,19 @@ public final class JobRunDefinitionResource extends BaseResource {
       return Response.status(Status.BAD_REQUEST).entity(err).type(APPLICATION_JSON).build();
     }
 
-    // register the new owner, if necessary
-    Owner owner = this.ownerDAO.findByName(request.getOwnerName());
-    if (owner == null) {
-      this.ownerDAO.insert(UUID.randomUUID(), new Owner(request.getOwnerName()));
-    }
-
-    // find or create the job
-    UUID jobGuid;
-    Job matchingJob = this.jobDAO.findByName(request.getName());
-    if (matchingJob == null) {
-      jobGuid = UUID.randomUUID();
-      Job newJob = new Job(jobGuid, request.getName(), request.getOwnerName(), null, null, null);
-      this.jobDAO.insert(newJob);
-    } else {
-      jobGuid = matchingJob.getGuid();
-    }
-
-    // find or create the job version
-    JobVersion reqJV = JobVersion.create(request);
-    UUID computedVersion = reqJV.computeVersionGuid();
-    JobVersion matchingJobVersion = this.jobVersionDAO.findByVersion(computedVersion);
-
-    UUID jobVersionGuid;
-    if (matchingJobVersion == null) {
-      // insert new job version
-      jobVersionGuid = UUID.randomUUID();
-      this.jobVersionDAO.insert(jobVersionGuid, computedVersion, jobGuid, request.getURI());
-    } else {
-      jobVersionGuid = matchingJobVersion.getGuid();
-    }
-
-    // find or create a new Job Run Definition
-    JobRunDefinition reqJrd = JobRunDefinition.create(request, jobVersionGuid);
-    UUID definitionHash = reqJrd.computeDefinitionHash();
-    JobRunDefinition existingJrd = jobRunDefDAO.findByHash(definitionHash);
-    UUID resJrdGuid;
-
-    Status resStatus;
-    if (existingJrd == null) {
-      UUID jobRunDefGuid = UUID.randomUUID();
-      this.jobRunDefDAO.insert(
-          jobRunDefGuid,
-          definitionHash,
-          jobVersionGuid,
-          request.getRunArgsJson(),
-          request.getNominalStartTime(),
-          request.getNominalEndTime());
-      resJrdGuid = jobRunDefGuid;
-      resStatus = Status.CREATED;
-    } else {
-      resJrdGuid = existingJrd.getGuid();
-      resStatus = Status.OK;
-    }
-
-    CreateJobRunDefinitionResponse res = new CreateJobRunDefinitionResponse(resJrdGuid);
     try {
-      String jsonRes = mapper.writeValueAsString(res);
-      return Response.status(resStatus)
-          .header("Location", "/job_run_definition/" + resJrdGuid)
-          .entity(jsonRes)
+      Owner owner = findOrCreateOwner(request.getOwnerName());
+      Job job = findOrCreateJob(request.getName(), owner);
+      UUID jobVersionGuid = findOrCreateJobVersion(request, job);
+      JobRunDefinition jobRunDefinition = findOrCreateJobRunDefinition(request, jobVersionGuid);
+      CreateJobRunDefinitionResponse res =
+          new CreateJobRunDefinitionResponse(jobRunDefinition.getGuid());
+      return Response.status(Response.Status.OK)
+          .header("Location", "/job_run_definition/" + jobRunDefinition.getGuid())
+          .entity(res)
           .type(APPLICATION_JSON)
           .build();
-    } catch (JsonProcessingException e) {
+    } catch (Exception e) {
       return Response.serverError().build();
     }
   }
