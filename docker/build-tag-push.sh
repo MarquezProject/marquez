6a78e3835d65e3ed5c624a5ce4630faa057084fb
wslulciuc <willy.lulciuc@wework.com>
2018-10-14 19:51:58 -0700

    Cleanup build-tag-push.sh

--- docker/build-tag-push.sh
+++ docker/build-tag-push.sh
@@ -4,13 +4,16 @@
 
 set -eu
 
+declare -r org=projectmarquez
+declare -r repo=marquez
+
 project_root=$(git rev-parse --show-toplevel)
 cd $project_root/docker
 
 # Version of Marquez image to build
 version=$1
 
-docker build --no-cache -t projectmarquez/marquez:${version} .
-docker tag projectmarquez/marquez:${version} projectmarquez/marquez:latest
-docker push projectmarquez/marquez:${version}
-docker push projectmarquez/marquez:latest
+docker build --no-cache -t ${org}/${repo}:${version} .
+docker tag ${org}/${repo}:${version} ${org}/${repo}:latest
+docker push ${org}/${repo}:${version}
+docker push ${org}/${repo}:latest
