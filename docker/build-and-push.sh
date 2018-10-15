c905285229bd15da9f03ae79b057ffda2f0732d5
wslulciuc <willy.lulciuc@wework.com>
2018-10-14 23:59:07 -0700

    Add version check in build-and-push.sh

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -4,14 +4,22 @@
 
 set -eu
 
+# X.Y.Z
+readonly SEMVER_REGEX=/^\d+\.\d+\.\d+$/
+# Docker Hub repository
 readonly ORG=projectmarquez
 readonly REPO=marquez
 
 project_root=$(git rev-parse --show-toplevel)
-cd $project_root/docker
+cd $project_root
 
 # Version of Marquez image to build
 version=$1
+
+if [[ ! $version =~ $SEMVER_REGEX ]]; then
+  echo "ERROR: Version must match ${SEMVER_REGEX}"
+  exit 1
+fi
 
 echo "Building image (tag: ${version})..."
 
