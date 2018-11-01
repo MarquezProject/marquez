a76ec058da4afe979f0f11f1872c24cceebcec5a
Shawn Shah <shawn.shah@wework.com>
2018-11-01 16:24:46 -0700

    add namespace DAO and row mapper

--- src/main/java/marquez/dao/NamespaceDAO.java
+++ src/main/java/marquez/dao/NamespaceDAO.java
@@ -2,28 +2,25 @@ package marquez.dao;
 
 import java.util.List;
 import marquez.core.models.Namespace;
-import org.jdbi.v3.core.Handle;
-import org.jdbi.v3.sqlobject.CreateSqlObject;
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@RegisterRowMapper(NamespaceRow.class)
+@RegisterRowMapper(NamespaceRow.class)
 public interface NamespaceDAO extends SqlObject {
   static final Logger LOG = LoggerFactory.getLogger(NamespaceDAO.class);
 
   @SqlUpdate("INSERT INTO namespaces(uuid, name, description, current_owner) VALUES(:namespace.name, :namespace.description, :namespace.currentOwner)")
   void insert(@BindBean("namespace") Namespace namespace);
 
-  @SqlQuery("SELECT * FROM jobs WHERE name = :name")
-  Job findByName(@Bind("name") String name);
+  @SqlQuery("SELECT * FROM namespaces WHERE name = :name")
+  Namespace findByName(@Bind("name") String name);
 
-  @SqlQuery("SELECT * FROM jobs LIMIT :limit")
-  List<Job> findAll(@Bind("limit") int limit);
-
-  @SqlQuery("SELECT * FROM jobs LIMIT 1")
-  List<Job> findAllInNamespace(@Bind("namespace") String namespace);
+  @SqlQuery("SELECT * FROM namespaces")
+  List<Namespace> findAll();
 }
---
+++ src/main/java/marquez/dao/NamespaceRow.java
@@ -0,0 +1,36 @@
+package marquez.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+import marquez.core.models.Namespace;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NamespaceRow implements RowMapper<Namespace> {
+  static final Logger logger = LoggerFactory.getLogger(NamespaceDAO.class);
+
+  @Override
+  public Namespace map(ResultSet rs, StatementContext ctx) throws SQLException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSSSS-XX");
+    Timestamp createdAt;
+    try {
+      createdAt = new Timestamp(dateFormat.parse(rs.getString("created_at")).getTime());
+    } catch (ParseException e) {
+      logger.error("failed to parse timestamp", e);
+      createdAt = null;
+    }    
+
+    return new Namespace(
+        UUID.fromString(rs.getString("guid")),
+        createdAt,
+        rs.getString("name"),
+        rs.getString("description"),
+        rs.getString("current_ownership"));
+  }
+}
