2337e95f97ee3958cfddd9c91cfd2f4db274f5bb
Shawn Shah <shawn.shah@wework.com>
2018-11-01 16:25:20 -0700

    JobService and basic tests

--- src/main/java/marquez/core/services/JobService.java
+++ src/main/java/marquez/core/services/JobService.java
@@ -3,9 +3,15 @@ package marquez.core.services;
 import marquez.core.exceptions.JobServiceException;
 import marquez.dao.JobDAO;
 import marquez.dao.JobVersionDAO;
+import marquez.dao.JobRunDAO;
 import marquez.api.Job;
 import marquez.api.JobVersion;
+import marquez.api.JobRun;
+import marquez.api.JobRunState;
 import java.util.List;
+import java.util.UUID;
+import java.sql.Timestamp;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,18 +19,43 @@ import org.slf4j.LoggerFactory;
 class JobService {
     private JobDAO jobDAO;
     private JobVersionDAO jobVersionDAO;
-    static final Logger LOG = LoggerFactory.getLogger(JobDAO.class);
+    private JobRunDAO jobRunDAO;
+    static final Logger logger = LoggerFactory.getLogger(JobDAO.class);
 
-    public JobService(JobDAO jobDAO, JobVersionDAO jobVersionDAO) {
+    public JobService(JobDAO jobDAO, JobVersionDAO jobVersionDAO, JobRunDAO jobRunDAO) {
         this.jobDAO = jobDAO;
         this.jobVersionDAO = jobVersionDAO;
+        this.jobRunDAO = jobRunDAO;
     }
 
-    public void create(String namespace, Job job) {
-    }
+    //// PUBLIC METHODS ////
 
-    private boolean createVersion(String namespace, Job job) {
-        return true;
+    public Job create(String namespace, Job jobToCreate) throws JobServiceException {
+        Job job;
+        try {
+            job = this.jobDAO.findByName(jobToCreate.getName());
+            if (job == null) {
+                job = this.createJob(namespace, job);
+            } 
+        } catch (Exception e) {
+            String err = "failed to create job";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+
+        try{
+            UUID versionID = JobService.computeVersion(job);
+            JobVersion existingJobVersion = this.jobVersionDAO.findByVersion(versionID);
+            if (existingJobVersion == null) {
+              this.createVersion(namespace, job, versionID);
+            }
+        } catch (Exception e) {
+            String err = "error finding/creating job version";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+
+        return job;
     }
 
     public List<Job> getAll(String namespace) throws JobServiceException {
@@ -32,18 +63,110 @@ class JobService {
         try {
             jobs = this.jobDAO.findAllInNamespace(namespace);
         } catch (Exception e) {
-            // log exception
-            LOG.error("caught exception: " + e.getMessage());
+            logger.error("caught exception while fetching jobs in namespace ", e);
             throw new JobServiceException("error fetching jobs");
         }
         return jobs;
     }
 
-    public JobVersion[] getAllVersions(String namespace, String jobName) {
-        return new JobVersion[]{};
+    public List<JobVersion> getAllVersions(String namespace, String jobName) throws JobServiceException {
+        List<JobVersion> jobVersions;
+        try {
+            jobVersions = this.jobVersionDAO.find(namespace, jobName);
+        } catch (Exception e) {
+            logger.error("caught exception while fetching versions of job", e);
+            throw new JobServiceException("error fetching job versions");
+        }
+        return jobVersions;     
     }
 
-    public JobVersion getVersionLatest(String namespace, String jobName){
-        return null;
+    public JobVersion getVersionLatest(String namespace, String jobName) throws JobServiceException {
+        JobVersion latestVersion;
+        try {
+            latestVersion = this.jobVersionDAO.findLatest(namespace, jobName);
+        } catch (Exception e) {
+            String err = "error fetching latest version of job";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+        return latestVersion;      
+    }
+
+    public UUID createJobRun(
+        String namespace, 
+        String jobName, 
+        String nominalStarTime, 
+        String nominalEndTime, 
+        String runArgsJson) 
+    throws JobServiceException {
+        // get latest JobVersion for Job
+        JobVersion jobVersion = this.getVersionLatest(namespace, jobName);
+
+        // create a JobRun linked to that JobVersion
+        UUID jobRunID = UUID.randomUUID();
+
+        try{    
+            JobRun jobRun = new JobRun(
+                jobRunID, 
+                new Timestamp(new Date().getTime()), 
+                new Timestamp(new Date().getTime()),
+                UUID.randomUUID(), // TODO: remove
+                JobRunState.State.toInt(JobRunState.State.NEW),
+                jobVersion.getGuid());
+            this.jobRunDAO.insert(jobRun);
+        } catch (Exception e) {
+            String err = "error creating job run";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+        return jobRunID;
+    }
+
+    public JobRun updateJobRunState(UUID jobRunID, JobRunState.State state) throws JobServiceException {
+        try {   
+            this.jobRunDAO.updateState(jobRunID, JobRunState.State.toInt(state));
+            return this.jobRunDAO.findJobRunById(jobRunID);
+        } catch (Exception e){
+            String err = "error updating job run state";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+    }
+
+    public JobRun getJobRun(UUID jobRunID) throws JobServiceException {
+        JobRun jobRun;
+        try {
+            jobRun = this.jobRunDAO.findJobRunById(jobRunID);
+        } catch (Exception e) {
+            String err = "error fetching job run";
+            logger.error(err, e);
+            throw new JobServiceException(err);
+        }
+        return jobRun;
+    }
+
+
+    //// PRIVATE METHODS ////
+
+    private static UUID computeVersion(Job job) {
+        byte[] raw = String.format("%s:%s", job.getGuid(), job.getLocation()).getBytes();
+        return UUID.nameUUIDFromBytes(raw);    
+    }
+
+    private Job createJob(String namespace, Job job) throws JobServiceException {
+        Job newJob = new Job(UUID.randomUUID(), job.getName(), job.getOwnerName(), null, null, null, null);
+        this.jobDAO.insert(newJob);
+        return newJob;
+    }
+
+    private boolean createVersion(String namespace, Job job, UUID versionID) {
+        try{
+            this.jobVersionDAO.insert(UUID.randomUUID(), versionID, job.getGuid(), job.getLocation());
+        } catch (Exception e) {
+            String err = "error creating new job version";
+            logger.error(err, e);
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/marquez/core/services/JobServiceTest.java
+++ src/test/java/marquez/core/services/JobServiceTest.java
@@ -3,46 +3,89 @@ package marquez.core.services;
 import static org.mockito.Mockito.mock;
 import org.junit.Test;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import marquez.api.Job;
+import marquez.api.JobVersion;
 import marquez.dao.JobDAO;
 import marquez.dao.JobVersionDAO;
+import marquez.dao.JobRunDAO;
 import java.util.List;
 import java.util.UUID;
 import java.util.ArrayList;
 import java.sql.Timestamp;
 import java.util.Date;
-import java.sql.SQLException;
 import marquez.core.exceptions.JobServiceException;
-
-import static org.mockito.Mockito.when;
+import org.junit.Before;
+import org.junit.After;
 
 
 public class JobServiceTest {
     final String TEST_NS = "test_namespace";
+    private static final JobDAO jobDAO = mock(JobDAO.class);
+    private static final JobVersionDAO jobVersionDAO = mock(JobVersionDAO.class);
+    private static final JobRunDAO jobRunDAO = mock(JobRunDAO.class);
+
+
+    JobService jobService;
+    
+
+    @Before
+    public void setUp() {
+        jobService = new JobService(jobDAO, jobVersionDAO, jobRunDAO);
+    }
+
+    @After
+    public void tearDown() {
+        reset(jobDAO);
+        reset(jobVersionDAO);
+    }
 
     @Test
     public void testGetAll_OK() throws Exception {
         List<Job> jobs = new ArrayList<Job>();
-        jobs.add(new Job(UUID.randomUUID(), "job", "owner", new Timestamp(new Date(0).getTime()), "category", "a job"));
-        jobs.add(new Job(UUID.randomUUID(), "job2", "owner2", new Timestamp(new Date(0).getTime()), "category", "a job2"));
+        jobs.add(new Job(UUID.randomUUID(), "job", "owner", new Timestamp(new Date(0).getTime()), "category", "a job", null));
+        jobs.add(new Job(UUID.randomUUID(), "job2", "owner2", new Timestamp(new Date(0).getTime()), "category", "a job2", null));
+        when(jobDAO.findAllInNamespace(TEST_NS)).thenReturn(jobs);
 
-        JobDAO jobDAOMock = mock(JobDAO.class);
-        JobVersionDAO jobVersionDAOMock = mock(JobVersionDAO.class);
-        when(jobDAOMock.findAllInNamespace(TEST_NS)).thenReturn(jobs);
-
-        JobService js = new JobService(jobDAOMock, jobVersionDAOMock);
-        List<Job> jobsFound = js.getAll(TEST_NS);
-        Assert.assertEquals(jobs, jobsFound);
+        Assert.assertEquals(jobs, jobService.getAll(TEST_NS));
     }
 
-    @Test(expected = JobServiceException.class)
-    public void testGetAll_Err() throws Exception {
-        JobDAO jobDAOMock = mock(JobDAO.class);
-        JobVersionDAO jobVersionDAOMock = mock(JobVersionDAO.class);
-        when(jobDAOMock.findAllInNamespace(TEST_NS)).thenThrow(Exception.class);
+    @Test
+    public void testGetAll_NoJobs_OK() throws Exception {
+        List<Job> jobs = new ArrayList<Job>();
+        when(jobDAO.findAllInNamespace(TEST_NS)).thenReturn(jobs);
+        Assert.assertEquals(jobs, jobService.getAll(TEST_NS));
+    }
 
-        JobService js = new JobService(jobDAOMock, jobVersionDAOMock);
-        js.getAll(TEST_NS);
+    @Test
+    public void testGetAllVersions_OK() throws Exception {
+        String jobName = "a job";
+        UUID jobGuid = UUID.randomUUID();
+        Timestamp createdAt, updatedAt;
+
+        List<JobVersion> jobVersions = new ArrayList<JobVersion>();
+        createdAt = updatedAt = new Timestamp(new Date(0).getTime());
+        jobVersions.add(new JobVersion(UUID.randomUUID(), jobGuid, "git://foo.com/v1.git", UUID.randomUUID(), null, createdAt, updatedAt));
+        createdAt = updatedAt = new Timestamp(new Date(1).getTime());
+        jobVersions.add(new JobVersion(UUID.randomUUID(), jobGuid, "git://foo.com/v2.git", UUID.randomUUID(), null, createdAt, updatedAt));
+
+        when(jobVersionDAO.find(TEST_NS, jobName)).thenReturn(jobVersions);
+        Assert.assertEquals(jobVersions, jobService.getAllVersions(TEST_NS, jobName));
+    }
+
+    @Test
+    public void testGetAllVersions_NoVersions_OK() throws Exception {
+        String jobName = "a job";
+        List<JobVersion> jobVersions = new ArrayList<JobVersion>();
+        when(jobVersionDAO.find(TEST_NS, jobName)).thenReturn(jobVersions);
+        Assert.assertEquals(jobVersions, jobService.getAllVersions(TEST_NS, jobName));
     }
 }
