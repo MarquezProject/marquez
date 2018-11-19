ce9698b4ac7437671e50c1ad9b3bded6407cd9b7
wslulciuc <willy.lulciuc@wework.com>
2018-11-19 02:18:19 -0800

    Minor changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,6 @@
 
 set -eu
 
-if [ -f /usr/src/app/wait-for-it.sh ]; then /usr/src/app/wait-for-it.sh "${POSTGRES_HOST}:5432"; fi
+if [ -f ./wait-for-it.sh ]; then ./wait-for-it.sh "${POSTGRES_HOST}:5432"; fi
 
 java -jar marquez-all.jar server "${MARQUEZ_CONFIG}"
