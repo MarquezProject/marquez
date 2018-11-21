0e01a68cde76cf3182b7ba0062d56b820ce18f99
Shawn Shah <shawn.shah@wework.com>
2018-11-20 17:51:27 -0800

    update job_run_args table with digest and remove UUID

---
+++ src/main/resources/db/migration/V6__update_run_args.sql
@@ -0,0 +1,9 @@
+ALTER TABLE job_runs DROP COLUMN job_run_args_guid;
+
+ALTER TABLE job_run_args ADD COLUMN hex_digest VARCHAR(64) NOT NULL UNIQUE;
+ALTER TABLE job_run_args ADD COLUMN args_json VARCHAR(512) NOT NULL;
+ALTER TABLE job_run_args DROP COLUMN content_hash;
+ALTER TABLE job_run_args DROP COLUMN args;
+
+ALTER TABLE job_runs ADD job_run_args_hex_digest VARCHAR(64);
+ALTER TABLE job_runs ADD FOREIGN KEY(job_run_args_hex_digest) REFERENCES job_run_args(hex_digest);
