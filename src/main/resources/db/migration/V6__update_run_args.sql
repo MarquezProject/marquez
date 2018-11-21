dd6d038db01f80cd02ad17ed9c7af30135a479f4
Shawn Shah <shawn.shah@wework.com>
2018-11-20 22:12:54 -0800

    remove guid from job_run_args

--- src/main/resources/db/migration/V6__update_run_args.sql
+++ src/main/resources/db/migration/V6__update_run_args.sql
@@ -4,6 +4,7 @@ ALTER TABLE job_run_args ADD COLUMN hex_digest VARCHAR(64) NOT NULL UNIQUE;
 ALTER TABLE job_run_args ADD COLUMN args_json VARCHAR(512) NOT NULL;
 ALTER TABLE job_run_args DROP COLUMN content_hash;
 ALTER TABLE job_run_args DROP COLUMN args;
+ALTER TABLE job_run_args DROP guid;
 
 ALTER TABLE job_runs ADD job_run_args_hex_digest VARCHAR(64);
 ALTER TABLE job_runs ADD FOREIGN KEY(job_run_args_hex_digest) REFERENCES job_run_args(hex_digest);
