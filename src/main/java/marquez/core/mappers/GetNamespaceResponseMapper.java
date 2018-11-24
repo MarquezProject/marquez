5658903ffb6227f9be8031be7f0b050c5d2e3c78
wslulciuc <willy.lulciuc@wework.com>
2018-11-24 15:17:34 -0800

    continued: Add mapper TODOs

--- src/main/java/marquez/core/mappers/GetNamespaceResponseMapper.java
+++ src/main/java/marquez/core/mappers/GetNamespaceResponseMapper.java
@@ -6,9 +6,11 @@ import marquez.core.models.Namespace;
 
 // TODO: Move to marquez.api.mappers pgk
 public class GetNamespaceResponseMapper extends Mapper<Namespace, GetNamespaceResponse> {
+  // TODO: Remove use of CoreNamespaceToApiNamespaceMapper
   private final CoreNamespaceToApiNamespaceMapper namespaceMapper =
       new CoreNamespaceToApiNamespaceMapper();
 
+  // TODO: GetNamespaceResponseMapper.map() should accept marquez.api.models.Namespace instead
   public GetNamespaceResponse map(@NotNull Namespace namespace) {
     return new GetNamespaceResponse(namespaceMapper.map(namespace));
   }
