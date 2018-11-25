1bb77b6cd65b6ee3d98290dcf380d05365953791
wslulciuc <willy.lulciuc@wework.com>
2018-11-25 13:33:49 -0800

    Update mapper and finish tests

--- src/main/java/marquez/core/mappers/Mapper.java
+++ src/main/java/marquez/core/mappers/Mapper.java
@@ -1,16 +1,14 @@
 package marquez.core.mappers;
 
-import static java.util.Objects.requireNonNull;
-
 import java.util.Optional;
 
 public abstract class Mapper<A, B> {
   public Optional<B> mapAsOptional(A value) {
-    return Optional.ofNullable(map(requireNonNull(value)));
+    return Optional.ofNullable(map(value));
   }
 
   public Optional<B> mapIfPresent(Optional<A> value) {
-    return requireNonNull(value).map(this::map);
+    return value.map(this::map);
   }
 
   public abstract B map(A value);
--- src/test/java/marquez/core/mappers/MapperTest.java
+++ src/test/java/marquez/core/mappers/MapperTest.java
@@ -1,4 +1,40 @@
 package marquez.core.mappers;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+import org.junit.Test;
+
 public class MapperTest {
+  @Test
+  public void testMapAsOptional() {
+    Optional<B> optB = B_MAPPER.mapAsOptional(new A());
+    assertTrue(optB.isPresent());
+  }
+
+  @Test
+  public void testMapAsOptionalFromNull() {
+    A nullA = null;
+    Optional<B> optB = B_MAPPER.mapAsOptional(nullA);
+    assertFalse(optB.isPresent());
+  }
+
+  @Test
+  public void testMapIfPresent() {
+    Optional<A> optA = Optional.of(new A());
+    Optional<B> optB = B_MAPPER.mapIfPresent(optA);
+    assertTrue(optB.isPresent());
+  }
+
+  private static final Mapper<A, B> B_MAPPER =
+      new Mapper<A, B>() {
+        public B map(A value) {
+          return value == null ? null : new B();
+        }
+      };
+
+  static final class A {}
+
+  static final class B {}
 }
