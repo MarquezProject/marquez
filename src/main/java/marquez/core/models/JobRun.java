ceab2a494959f2913a9075eba496f5a7b5d8513d
Shawn Shah <shawn.shah@wework.com>
2018-12-02 17:36:24 -0800

    remove state transition enforcement code from JobRun / JobRunState

--- src/main/java/marquez/core/models/JobRun.java
+++ src/main/java/marquez/core/models/JobRun.java
@@ -1,10 +1,6 @@
 package marquez.core.models;
 
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import lombok.Data;
 
@@ -19,30 +15,6 @@ public final class JobRun {
   private final String runArgsHexDigest;
   private final String runArgs;
 
-  private static Map<JobRunState.State, Set<JobRunState.State>> validTransitions = new HashMap<>();
-
-  static {
-    validTransitions.put(
-        JobRunState.State.NEW,
-        new HashSet<JobRunState.State>() {
-          {
-            add(JobRunState.State.RUNNING);
-          }
-        });
-    validTransitions.put(
-        JobRunState.State.RUNNING,
-        new HashSet<JobRunState.State>() {
-          {
-            add(JobRunState.State.COMPLETED);
-            add(JobRunState.State.FAILED);
-            add(JobRunState.State.ABORTED);
-          }
-        });
-    validTransitions.put(JobRunState.State.FAILED, new HashSet<>());
-    validTransitions.put(JobRunState.State.COMPLETED, new HashSet<>());
-    validTransitions.put(JobRunState.State.ABORTED, new HashSet<>());
-  }
-
   public JobRun(
       final UUID guid,
       final Integer currentState,
@@ -50,12 +22,12 @@ public final class JobRun {
       final String runArgsHexDigest,
       final String runArgs) {
     this.guid = guid;
-    this.startedAt = null;
-    this.endedAt = null;
     this.currentState = currentState;
     this.jobVersionGuid = jobVersionGuid;
     this.runArgsHexDigest = runArgsHexDigest;
     this.runArgs = runArgs;
+    this.startedAt = null;
+    this.endedAt = null;
   }
 
   public JobRun(
--- src/main/java/marquez/core/models/JobRunState.java
+++ src/main/java/marquez/core/models/JobRunState.java
@@ -1,17 +1,14 @@
 package marquez.core.models;
 
 import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.Data;
 
 @Data
-public final class JobRunState {
+public class JobRunState {
   private final UUID guid;
   private final Timestamp transitionedAt;
   private final UUID jobRunGuid;
@@ -29,80 +26,27 @@ public final class JobRunState {
   }
 
   public enum State {
-    NEW {
-      @Override
-      public boolean isFinished() {
-        return false;
-      }
+    NEW,
+    RUNNING,
+    COMPLETED,
+    FAILED,
+    ABORTED;
 
-      public Set<State> getValidTransitions() {
-        return new HashSet<State>(Arrays.asList(State.RUNNING));
-      }
-    },
-
-    RUNNING {
-      @Override
-      public boolean isFinished() {
-        return false;
-      }
-
-      public Set<State> getValidTransitions() {
-        return new HashSet<State>(
-            Arrays.asList(State.RUNNING, State.FAILED, State.COMPLETED, State.ABORTED));
-      }
-    },
-
-    COMPLETED {
-      @Override
-      public boolean isFinished() {
-        return true;
-      }
-
-      public Set<State> getValidTransitions() {
-        return Collections.emptySet();
-      }
-    },
-    FAILED {
-      @Override
-      public boolean isFinished() {
-        return true;
-      }
-
-      public Set<State> getValidTransitions() {
-        return Collections.emptySet();
-      }
-    },
-
-    ABORTED {
-      @Override
-      public boolean isFinished() {
-        return true;
-      }
-
-      public Set<State> getValidTransitions() {
-        return Collections.emptySet();
-      }
-    };
-
-    public abstract boolean isFinished();
-
-    public abstract Set<State> getValidTransitions();
-
-    public static Map<State, Integer> stateToIntMap;
-    public static Map<Integer, State> intToStateMap;
+    static Map<State, Integer> stateToIntMap = new HashMap<State, Integer>();
+    static Map<Integer, State> intToStateMap = new HashMap<Integer, State>();
 
     static {
-      stateToIntMap = new HashMap<State, Integer>();
       stateToIntMap.put(NEW, 0);
       stateToIntMap.put(RUNNING, 1);
       stateToIntMap.put(COMPLETED, 2);
       stateToIntMap.put(FAILED, 3);
       stateToIntMap.put(ABORTED, 4);
 
-      intToStateMap = new HashMap<Integer, State>();
-      for (State s : stateToIntMap.keySet()) {
-        intToStateMap.put(stateToIntMap.get(s), s);
-      }
+      intToStateMap =
+          stateToIntMap
+              .entrySet()
+              .stream()
+              .collect(Collectors.toMap(o -> o.getValue(), o -> o.getKey()));
     }
 
     public static int toInt(State s) {
