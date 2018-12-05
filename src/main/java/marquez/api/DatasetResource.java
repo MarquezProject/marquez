f55fb3a4804f96c5cfe59e040d46f9b34d6870ff
wslulciuc <willy.lulciuc@wework.com>
2018-12-05 09:08:18 -0800

    Add dataset resource

---
+++ src/main/java/marquez/api/DatasetResource.java
@@ -0,0 +1,46 @@
+package marquez.api;
+
+import static java.util.Objects.requireNonNull;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.ResponseMetered;
+import com.codahale.metrics.annotation.Timed;
+import java.util.List;
+import java.util.Optional;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import marquez.api.models.DatasetResponse;
+import marquez.api.models.ListDatasetsResponse;
+import marquez.api.mappers.DatasetResponseMapper;
+import marquez.services.DatasetService;
+import marquez.services.models.Dataset;
+
+@Path("/api/v1")
+public final class DatasetResource {
+  private final DatasetResponseMapper datasetResponseMapper = new DatasetResponseMapper();
+  private final DatasetService datasetService;
+
+  public DatasetResource(final DatasetService datasetService) {
+    this.datasetService = requireNonNull(datasetService);
+  }
+
+  @GET
+  @ResponseMetered
+  @ExceptionMetered
+  @Timed
+  @Path("/namespaces/{namespace}/datasets")
+  @Produces(APPLICATION_JSON)
+  public Response list(
+      @PathParam("namespace") String namespace,
+      @QueryParam("limit") Integer limit,
+      @QueryParam("offset") Integer offset) {
+    final List<Dataset> datasets = datasetService.getAll(namespace, limit, offset);
+    final List<DatasetResponse> datasetResponse = datasetResponseMapper.map(datasets);
+    return Response.ok(new ListDatasetsResponse(datasetResponse)).build();
+  }
+}
