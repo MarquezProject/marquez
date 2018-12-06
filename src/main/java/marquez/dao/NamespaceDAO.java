85515a148479cf1563bf64a5a80d1b11afcad9d2
Shawn Shah <shawn.shah@wework.com>
2018-12-05 18:11:48 -0800

    remove logger

--- src/main/java/marquez/dao/NamespaceDAO.java
+++ src/main/java/marquez/dao/NamespaceDAO.java
@@ -8,13 +8,9 @@ import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RegisterRowMapper(NamespaceRow.class)
 public interface NamespaceDAO extends SqlObject {
-  static final Logger LOG = LoggerFactory.getLogger(NamespaceDAO.class);
-
   @SqlUpdate(
       "INSERT INTO namespaces(uuid, name, description, current_owner) VALUES(:namespace.name, :namespace.description, :namespace.currentOwner)")
   void insert(@BindBean("namespace") Namespace namespace);
