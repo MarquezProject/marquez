5116f57e2b3214376b0476202375fab4e8a4ecec
wslulciuc <willy.lulciuc@wework.com>
2018-12-10 11:25:09 -0800

    Simplify base create dataset version request

--- src/main/java/marquez/api/models/CreateDatasetVersionRequest.java
+++ src/main/java/marquez/api/models/CreateDatasetVersionRequest.java
@@ -2,19 +2,14 @@ package marquez.api.models;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
-@RequiredArgsConstructor
-@EqualsAndHashCode
-@ToString
+@Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
   @JsonSubTypes.Type(value = CreateDbDatasetVersionRequest.class, name = "DB"),
 })
 public abstract class CreateDatasetVersionRequest {
-  @NonNull @Getter private final DatasetType type;
+  @NonNull private final DatasetType type;
 }
