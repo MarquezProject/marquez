c6ac1fdb3ff6e755131c4f64645ca3cc9f524fa5
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2018-12-12 10:01:08 -0800

    Lombok-ifying jobrunstate

--- src/main/java/marquez/api/JobRunState.java
+++ src/main/java/marquez/api/JobRunState.java
@@ -1,57 +1,17 @@
 package marquez.api;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.sql.Timestamp;
-import java.util.Objects;
 import java.util.UUID;
-import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public final class JobRunState {
 
-  @NotNull private final Timestamp transitionedAt;
-  @NotNull private final UUID jobRunGuid;
-  @NotNull private final String state;
-
-  @JsonCreator
-  public JobRunState(
-      @JsonProperty("transitionedAt") final Timestamp transitionedAt,
-      @JsonProperty("jobRunGuid") final UUID jobRunGuid,
-      @JsonProperty("currentState") final String state) {
-    this.transitionedAt = transitionedAt;
-    this.jobRunGuid = jobRunGuid;
-    this.state = state;
-  }
-
-  public String getState() {
-    return state;
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (!(o instanceof JobRunState)) return false;
-
-    final JobRunState other = (JobRunState) o;
-
-    return Objects.equals(transitionedAt, other.transitionedAt)
-        && Objects.equals(jobRunGuid, other.jobRunGuid)
-        && Objects.equals(state, other.state);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(transitionedAt, jobRunGuid, state);
-  }
-
-  @Override
-  public String toString() {
-    final StringBuilder sb = new StringBuilder();
-    sb.append("JobRunState{");
-    sb.append("transitionedAt=").append(transitionedAt);
-    sb.append("jobRunGuid=").append(jobRunGuid);
-    sb.append("currentState=").append(state);
-    sb.append("}");
-    return sb.toString();
-  }
+  private Timestamp transitionedAt;
+  private UUID jobRunGuid;
+  private String state;
 }
