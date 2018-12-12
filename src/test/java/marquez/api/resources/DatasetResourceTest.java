9d83286808c53a86dd811fc1c32f02ecf12aaa5a
wslulciuc <willy.lulciuc@wework.com>
2018-12-11 20:46:26 -0800

    minor cleanup to DatasetResourceTest

--- src/test/java/marquez/api/resources/DatasetResourceTest.java
+++ src/test/java/marquez/api/resources/DatasetResourceTest.java
@@ -20,9 +20,9 @@ import marquez.service.models.Dataset;
 import org.junit.Test;
 
 public class DatasetResourceTest {
-  private static final Namespace NAMESPACE = new Namespace("test");
   private static final Urn URN = new Urn("urn:a:b.c");
   private static final Dataset DATASET = new Dataset(URN, Instant.now(), "test description");
+  private static final Namespace NAMESPACE = new Namespace("test");
   private static final Integer LIMIT = 100;
   private static final Integer OFFSET = 0;
 
