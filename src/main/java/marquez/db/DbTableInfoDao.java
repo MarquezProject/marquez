6514ac52d705b0511890546e6b2d0d1896600cbe
wslulciuc <willy.lulciuc@wework.com>
2018-12-14 00:09:48 -0800

    Add DbTableInfoDao

--- src/main/java/marquez/db/DbTableInfoDao.java
+++ src/main/java/marquez/db/DbTableInfoDao.java
@@ -3,7 +3,7 @@ package marquez.db;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import marquez.common.models.Dataset;
+import marquez.common.models.Db;
 import marquez.common.models.Schema;
 import marquez.db.mappers.DbTableInfoRowMapper;
 import marquez.db.models.DbTableInfoRow;
@@ -16,15 +16,11 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 @RegisterRowMapper(DbTableInfoRowMapper.class)
 public interface DbTableInfoDao {
   @SqlUpdate(
-      "INSERT INTO db_table_info (uuid, db, table) "
-          + "VALUES (:uuid, :dataset.value, :schema.value)")
-  void insert(
-      @Bind("uuid") UUID uuid,
-      @BindBean("dataset") Dataset dataset,
-      @BindBean("schema") Schema schema);
+      "INSERT INTO db_table_info (uuid, db, table) " + "VALUES (:uuid, :db.value, :schema.value)")
+  void insert(@Bind("uuid") UUID uuid, @BindBean("db") Db db, @BindBean("schema") Schema schema);
 
   @SqlQuery("SELECT * FROM dbs WHERE uuid = :uuid")
-  Optional<DbTableInfoRow> find(@Bind("uuid") UUID uuid);
+  Optional<DbTableInfoRow> findBy(@Bind("uuid") UUID uuid);
 
   @SqlQuery("SELECT * FROM dbs LIMIT :limit OFFSET :offset")
   List<DbTableInfoRow> findAll(@Bind("limit") Integer limit, @Bind("offset") Integer offset);
