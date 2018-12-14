b1dd9326ab967d3c48f6c85c00bb27b375deea34
wslulciuc <willy.lulciuc@wework.com>
2018-12-14 09:57:29 -0800

    Return dataset on DatasetService.create()

--- src/main/java/marquez/service/DatasetService.java
+++ src/main/java/marquez/service/DatasetService.java
@@ -3,6 +3,7 @@ package marquez.service;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.NonNull;
 import marquez.common.models.Namespace;
 import marquez.common.models.Urn;
@@ -21,7 +22,7 @@ public class DatasetService {
   }
 
   public Dataset create(@NonNull Namespace namespace, @NonNull DbTableVersion dbTableVersion) {
-    final DatasetRow row =
+    final UUID datasetUuid =
         datasetDao.insert(
             namespace,
             dbTableVersion.getDataSource(),
@@ -30,12 +31,14 @@ public class DatasetService {
             dbTableVersion.getSchema(),
             dbTableVersion.getTable(),
             dbTableVersion.getDescription().orElse(null));
-    return datasetMapper.map(row);
+    final Optional<DatasetRow> datasetRow = datasetDao.findBy(datasetUuid);
+    final Optional<Dataset> dataset = datasetRow.map(datasetMapper::map);
+    return dataset.orElseThrow(IllegalStateException::new);
   }
 
   public Optional<Dataset> get(@NonNull Namespace namespace, @NonNull Urn urn) {
-    final Optional<DatasetRow> row = datasetDao.findBy(urn);
-    return row.map(datasetMapper::map);
+    final Optional<DatasetRow> datasetRow = datasetDao.findBy(urn);
+    return datasetRow.map(datasetMapper::map);
   }
 
   public List<Dataset> getAll(
