fba81f1821a7268b060985b3e8b2b25793aef203
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2018-12-14 13:18:59 -0800

    Resource fix

--- src/main/java/marquez/resources/JobResource.java
+++ src/main/java/marquez/resources/JobResource.java
@@ -1,20 +1,6 @@
 package marquez.resources;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
 import com.codahale.metrics.annotation.Timed;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import marquez.api.CreateJobRequest;
 import marquez.api.CreateJobRunRequest;
@@ -29,7 +15,22 @@ import marquez.core.models.JobRun;
 import marquez.core.services.JobService;
 import marquez.core.services.NamespaceService;
 
-@Path("/api/v1/namespaces/{namespace}/jobs")
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/api/v1")
 @Produces(APPLICATION_JSON)
 @Slf4j
 public final class JobResource extends BaseResource {
