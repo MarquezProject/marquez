de7a1bb74c7e4f9fb2255d6a9d151a257b9176f6
wslulciuc <willy.lulciuc@wework.com>
2018-12-17 18:15:31 -0800

    Add @Timed to health and ping endpoints

--- src/main/java/marquez/api/resources/HealthResource.java
+++ src/main/java/marquez/api/resources/HealthResource.java
@@ -1,16 +1,19 @@
 package marquez.api.resources;
 
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/health")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(TEXT_PLAIN)
 public class HealthResource {
 
   @GET
+  @Timed
   public Response checkHealth() {
     return Response.ok("OK").build();
   }
--- src/main/java/marquez/api/resources/PingResource.java
+++ src/main/java/marquez/api/resources/PingResource.java
@@ -1,16 +1,19 @@
 package marquez.api.resources;
 
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/ping")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(TEXT_PLAIN)
 public class PingResource {
 
   @GET
+  @Timed
   public Response ping() {
     return Response.ok("pong").build();
   }
