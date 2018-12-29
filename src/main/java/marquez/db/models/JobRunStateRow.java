af6274ebfb17349e74f9f8fbc0bbad809a660fab
wslulciuc <willy.lulciuc@wework.com>
2018-12-29 13:57:04 -0800

    Use @Data in JobRunStateRow class

--- src/main/java/marquez/db/models/JobRunStateRow.java
+++ src/main/java/marquez/db/models/JobRunStateRow.java
@@ -3,19 +3,13 @@ package marquez.db.models;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.Data;
 
-@RequiredArgsConstructor
-@EqualsAndHashCode
-@ToString
+@Data
 @Builder
 public final class JobRunStateRow {
-  @Getter @NonNull private final UUID uuid;
-  @Getter @NonNull private final UUID jobRunUuid;
-  @Getter @NonNull private final String state;
-  private final Instant transitionedAt;
+  @NonNull private final UUID uuid;
+  @NonNull private final Instant transitionedAt;
+  @NonNull private final UUID jobRunUuid;
+  @NonNull private final String state;
 }
