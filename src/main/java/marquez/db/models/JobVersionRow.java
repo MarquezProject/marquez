95adc4ae4b7857e4d7fd2b1e83dab85ac17e67f2
wslulciuc <willy.lulciuc@wework.com>
2018-12-29 13:58:06 -0800

    Make JobVersionRow.createdAt required

--- src/main/java/marquez/db/models/JobVersionRow.java
+++ src/main/java/marquez/db/models/JobVersionRow.java
@@ -1,9 +1,9 @@
 package marquez.db.models;
 
 import java.time.Instant;
-import java.util.UUID;
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,18 +17,14 @@ import lombok.ToString;
 @Builder
 public final class JobVersionRow {
   @Getter @NonNull private final UUID uuid;
+  @Getter @NonNull private final Instant createdAt;
   @Getter @NonNull private final List<String> inputDatasetUrns;
   @Getter @NonNull private final List<String> outputDatasetUrns;
   @Getter @NonNull private final UUID jobUuid;
   @Getter @NonNull private final UUID version;
   @Getter @NonNull private final String location;
-  private final Instant createdAt;
   private final Instant updatedAt;
   private final UUID latestJobRunUuid;
-
-  public Optional<Instant> getCreatedAt() {
-    return Optional.ofNullable(createdAt);
-  }
 
   public Optional<Instant> getUpdatedAt() {
     return Optional.ofNullable(updatedAt);
