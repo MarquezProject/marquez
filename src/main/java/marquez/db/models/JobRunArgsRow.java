821b2cdf67255ac015605aaa380a8cdefc586356
wslulciuc <willy.lulciuc@wework.com>
2018-12-29 14:00:45 -0800

    Make JobRunArgsRow.createdAt required

--- src/main/java/marquez/db/models/JobRunArgsRow.java
+++ src/main/java/marquez/db/models/JobRunArgsRow.java
@@ -3,20 +3,15 @@ package marquez.db.models;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 
-@RequiredArgsConstructor
-@EqualsAndHashCode
-@ToString
+@Data
 @Builder
 public final class JobRunArgsRow {
   @Getter @NonNull private final UUID uuid;
+  @Getter @NonNull private final Instant createdAt;
   @Getter @NonNull private final UUID jobRunUuid;
   @Getter @NonNull private final Long hash;
   @Getter @NonNull private final String args;
-  @Getter private final Instant createdAt;
 }
