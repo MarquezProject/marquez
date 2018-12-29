1e4ac111af8121fcbd8ca57288ad2a007eb08b99
wslulciuc <willy.lulciuc@wework.com>
2018-12-29 14:22:49 -0800

    Add NamespaceRow class

---
+++ src/main/java/marquez/db/models/NamespaceRow.java
@@ -0,0 +1,27 @@
+package marquez.db.models;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public final class NamespaceRow {
+  @Getter @NonNull private final UUID uuid;
+  @Getter @NonNull private final Instant createdAt;
+  @Getter @NonNull private final String currentOwnership;
+  @Getter @NonNull private final String namespace;
+  private final String description;
+
+  public Optional<String> getDescription() {
+    return Optional.ofNullable(description);
+  }
+}
