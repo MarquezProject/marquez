926be6606fd09b2eceb78b9f7ec918406a309e72
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-01-23 14:19:09 -0800

    Add initial install code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='marquez_python',
+      version='0.1.0',
+      description='Set of libraries for interacting with the Marquez Service',
+      url='https://github.com/MarquezProject/marquez-python',
+      author='The We Company',
+      author_email='aleksandr.shulman@wework.com',
+      license='Apache 2.0',
+      packages=['marquez_python'],
+      zip_safe=False)
+
