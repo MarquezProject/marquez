6b11ad35649019dd751c8b56c1a4893a0f85bff5
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-01-29 15:30:24 -0800

    Upping version to 1.1

--- marquez_client/configuration.py
+++ marquez_client/configuration.py
@@ -224,5 +224,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 1.0.0".\
+               "SDK Package Version: 1.1.0".\
                format(env=sys.platform, pyversion=sys.version)
