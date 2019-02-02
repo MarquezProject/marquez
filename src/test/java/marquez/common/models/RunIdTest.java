78e9229631420908bee68b0e8c331fff72bf7838
wslulciuc <willy.lulciuc@wework.com>
2019-02-02 10:39:49 -0800

    Update tests in RunIdTest

--- src/test/java/marquez/common/models/RunIdTest.java
+++ src/test/java/marquez/common/models/RunIdTest.java
@@ -6,42 +6,40 @@ import java.util.UUID;
 import org.junit.Test;
 
 public class RunIdTest {
-  private static final String ID_STRING = "f662a43d-ab87-4b4b-a9ba-828b53c3368b";
-  private static final UUID RUN_ID = UUID.fromString(ID_STRING);
-
   @Test
   public void testNewRunId() {
-    final RunId runId = new RunId(ID_STRING);
-    assertEquals(RUN_ID, runId.getValue());
+    final String value = "f662a43d-ab87-4b4b-a9ba-828b53c3368b";
+    final UUID expected = UUID.fromString(value);
+    assertEquals(expected, RunId.fromString(value).getValue());
   }
 
   @Test(expected = NullPointerException.class)
-  public void testRunIdNull() {
-    final String nullRunId = null;
-    new RunId(nullRunId);
+  public void testNewRunId_throwsException_onNullValue() {
+    final String nullValue = null;
+    RunId.fromString(nullValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testRunIdEmpty() {
-    final String emptyRunId = "";
-    new RunId(emptyRunId);
+  public void testNewRunId_throwsException_onEmptyValue() {
+    final String emptyValue = "";
+    RunId.fromString(emptyValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testRunIdBlank() {
-    final String blankRunId = " ";
-    new RunId(blankRunId);
+  public void testNewRunId_throwsException_onBlankValue() {
+    final String blankValue = " ";
+    RunId.fromString(blankValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testRunIdGreaterThan36() {
-    final String runIdGreaterThan36 = "5f217783-ff48-4650-aab4-ek1306afb3960";
-    new RunId(runIdGreaterThan36);
+  public void testNewRunId_throwsException_onGreaterThan36Value() {
+    final String greaterThan36Value = "5f217783-ff48-4650-aab4-ek1306afb3960";
+    RunId.fromString(greaterThan36Value);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testRunIdLessThan36() {
-    final String runIdLessThan36 = "5f217783-ff48-4650-ek1306afb3960";
-    new RunId(runIdLessThan36);
+  public void testNewRunId_throwsException_onLessThan36Value() {
+    final String lessThan36Value = "5f217783-ff48-4650-ek1306afb3960";
+    RunId.fromString(lessThan36Value);
   }
 }
