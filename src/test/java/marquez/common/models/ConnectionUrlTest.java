401942fe8e82c5324d064bcadd995e1daf57e605
wslulciuc <willy.lulciuc@wework.com>
2019-02-02 16:27:13 -0800

    Add DB_PORT in ConnectionUrlTest

--- src/test/java/marquez/common/models/ConnectionUrlTest.java
+++ src/test/java/marquez/common/models/ConnectionUrlTest.java
@@ -9,12 +9,14 @@ import org.junit.experimental.categories.Category;
 @Category(UnitTests.class)
 public class ConnectionUrlTest {
   private static final DataSource DATA_SOURCE = DataSource.fromString("postgresql");
+  private static final int DB_PORT = 5432;
   private static final DbName DB_NAME = DbName.fromString("test");
 
   @Test
   public void testNewConnectionUrl() {
     final String rawValue =
-        String.format("jdbc:%s://localhost:5432/%s", DATA_SOURCE.getValue(), DB_NAME.getValue());
+        String.format(
+            "jdbc:%s://localhost:%d/%s", DATA_SOURCE.getValue(), DB_PORT, DB_NAME.getValue());
     final ConnectionUrl connectionUrl = ConnectionUrl.fromString(rawValue);
     assertEquals(DATA_SOURCE, connectionUrl.getDataSource());
     assertEquals(DB_NAME, connectionUrl.getDbName());
@@ -40,16 +42,16 @@ public class ConnectionUrlTest {
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testNewConnectionUrl_throwsException_onUnknownProtocol() {
-    final String rawValueWithUnknownProtocol =
-        String.format("foo:postgresql://localhost:5432/%s", DB_NAME.getValue());
-    ConnectionUrl.fromString(rawValueWithUnknownProtocol);
+  public void testNewConnectionUrl_throwsException_onUnknownProtocolValue() {
+    final String unknownProtocolValue =
+        String.format("foo:postgresql://localhost:%d/%s", DB_PORT, DB_NAME.getValue());
+    ConnectionUrl.fromString(unknownProtocolValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testNewConnectionUrl_throwsException_onMissingParts() {
-    final String rawValueWithMissingParts =
+  public void testNewConnectionUrl_throwsException_onMissingPartValue() {
+    final String missingPartValue =
         String.format("jdbc:postgresql://localhost/%s", DB_NAME.getValue());
-    ConnectionUrl.fromString(rawValueWithMissingParts);
+    ConnectionUrl.fromString(missingPartValue);
   }
 }
