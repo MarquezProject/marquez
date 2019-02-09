84705422dcf1953b2bcfcef773d3d3472a67b7b4
wslulciuc <willy.lulciuc@wework.com>
2019-02-09 13:29:24 -0800

    Remove common.sh usage and add comments build-and-push.sh

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #
-# Usage: $ ./build-tag-push.sh <version>
+# Usage: $ ./build-and-push.sh <version>
 
 set -eu
-
-source common.sh
 
 readonly SEMVER_REGEX="^[0-9]+(\.[0-9]+){2}$" # X.Y.Z
 readonly ORG="marquezproject"
 readonly REPO="marquez"
 readonly NAME="${ORG}/${REPO}"
 
+# Change working directory to project root
 project_root=$(git rev-parse --show-toplevel)
 cd "${project_root}"
 
 # Version X.Y.Z of Marquez image to build
 version="${1}"
 
+# Ensure valid version 
 if [[ ! "${version}" =~ ${SEMVER_REGEX} ]]; then
   error "Version must match ${SEMVER_REGEX}"
 fi
 
-info "Building image (tag: ${version})..."
+echo "Building image (tag: ${version})..."
 
+# Build and tag image
 docker build --no-cache --tag "${NAME}:${version}" .
 docker tag "${NAME}:${version}" "${NAME}:latest"
+
+# Push image to Docker Hub
 docker push "${NAME}:${version}"
 docker push "${NAME}:latest"
 
-info "DONE!"
+echo "DONE!"
