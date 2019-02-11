477560f91fc9307c55c21eaf466f58c919929aeb
Rodrigo Araya <rodrigo.araya@gmail.com>
2019-02-11 10:52:20 -0800

    cleaning up packaging config (#2)

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-import io
 import setuptools
 
-with io.open('README.md', encoding='utf-8') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="marquez-airflow",
-    version="0.0.1",
-    author="Author",
-    author_email="author@example.com",
+    version="0.0.2",
+    author="Marquez Team",
+    author_email="",
     description="Marquez integration with Airflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MarquezProject/marquez-airflow",
     packages=setuptools.find_packages(),
     install_requires = [
-        "apache-airflow>=1.10.0"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "marquez-python>=1.2.1",
+        "apache-airflow>=1.9.0"
     ],
 )
