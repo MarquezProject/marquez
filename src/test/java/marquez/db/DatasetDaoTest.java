e06884ec23a166fdf0cb76dafd99c870be7c4c91
Shawn Shah <shawn.shah@wework.com>
2019-02-12 00:06:02 -0800

    the unit test

---
+++ src/test/java/marquez/db/DatasetDaoTest.java
@@ -0,0 +1,54 @@
+package marquez.db;
+
+import static org.junit.Assert.assertEquals;
+
+import marquez.common.models.Namespace;
+import marquez.core.models.Generator;
+import marquez.db.models.DataSourceRow;
+import marquez.db.models.DatasetRow;
+import marquez.db.models.DbTableInfoRow;
+import marquez.db.models.DbTableVersionRow;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jdbi.v3.testing.JdbiRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatasetDaoTest {
+  static final Logger logger = LoggerFactory.getLogger(DatasetDaoTest.class);
+  private DatasetDao datasetDAO;
+  private marquez.core.models.Namespace namespace = Generator.genNamespace();
+  private NamespaceDao namespaceDAO;
+
+  @Rule
+  public final JdbiRule dbRule =
+      JdbiRule.embeddedPostgres().withPlugin(new SqlObjectPlugin()).migrateWithFlyway();
+
+  @Before
+  public void setup() {
+    Jdbi jdbi = dbRule.getJdbi();
+    datasetDAO = jdbi.onDemand(DatasetDao.class);
+    namespaceDAO = jdbi.onDemand(NamespaceDao.class);
+    namespaceDAO.insert(namespace);
+  }
+
+  private void insertRandomDataset() {
+    DataSourceRow dataSourceRow = Generator.genDataSourceRow();
+    DatasetRow datasetRow = Generator.genDatasetRow(namespace.getGuid(), dataSourceRow.getUuid());
+    DbTableInfoRow dbTableInfoRow = Generator.genDbTableInfowRow();
+    DbTableVersionRow dbTableVersionRow =
+        Generator.genDbTableVersionRow(datasetRow.getUuid(), dbTableInfoRow.getUuid());
+    datasetDAO.insertAll(dataSourceRow, datasetRow, dbTableInfoRow, dbTableVersionRow);
+  }
+
+  @Test
+  public void testFindAll() throws Exception {
+    assertEquals(0, datasetDAO.findAll(Namespace.of(namespace.getName()), 10, 0).size());
+    insertRandomDataset();
+    insertRandomDataset();
+    assertEquals(2, datasetDAO.findAll(Namespace.of(namespace.getName()), 10, 0).size());
+  }
+}
