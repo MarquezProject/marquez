e72191626054e13d76de5b6894acbf8a27668bbf
wslulciuc <willy.lulciuc@wework.com>
2019-02-16 06:37:09 -0800

    Define DataSourceRow constructor

--- src/main/java/marquez/db/models/DataSourceRow.java
+++ src/main/java/marquez/db/models/DataSourceRow.java
@@ -1,21 +1,36 @@
 package marquez.db.models;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
+import javax.annotation.Nullable;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@RequiredArgsConstructor
 @EqualsAndHashCode
 @ToString
 @Builder
 public final class DataSourceRow {
-  @Getter @NonNull private final UUID uuid;
-  @Getter @NonNull private final String name;
-  @Getter @NonNull private final String connectionUrl;
-  @Getter private final Instant createdAt;
+  @Getter private final UUID uuid;
+  @Getter private final String name;
+  @Getter private final String connectionUrl;
+  private final Instant createdAt;
+
+  public DataSourceRow(
+      @NonNull final UUID uuid,
+      @NonNull final String name,
+      @NonNull final String connectionUrl,
+      @Nullable final Instant createdAt) {
+    this.uuid = uuid;
+    this.name = name;
+    this.connectionUrl = connectionUrl;
+    this.createdAt = createdAt;
+  }
+
+  public Optional<Instant> getCreatedAt() {
+    return Optional.ofNullable(createdAt);
+  }
 }
