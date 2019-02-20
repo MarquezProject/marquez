0d01ff633628455595b6f4fca3af6ec715da7135
ashulmanWeWork <41963174+ashulmanWeWork@users.noreply.github.com>
2019-02-20 15:30:04 -0800

    Added some badges (#9)
    * Added some badges
    
    * Minor fix
    
    * Adjust status section

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import find_packages, setup  # noqa: H301
 
 NAME = "marquez-python"
-VERSION = "0.1.4"
+VERSION = "0.2.0"
 # To install the library, run the following
 #
 # python setup.py install
