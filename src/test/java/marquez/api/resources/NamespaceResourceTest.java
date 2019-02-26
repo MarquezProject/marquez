0c8971400a070ef228a6678230b617c7aa55e5a6
wslulciuc <willy.lulciuc@wework.com>
2019-02-25 21:02:56 -0800

    Apply formatting

--- src/test/java/marquez/api/resources/NamespaceResourceTest.java
+++ src/test/java/marquez/api/resources/NamespaceResourceTest.java
@@ -101,7 +101,8 @@ public class NamespaceResourceTest extends NamespaceBaseTest {
   }
 
   @Test
-  public void testListNamespaceWithSingleResultSet() throws MarquezServiceException, ResourceException {
+  public void testListNamespaceWithSingleResultSet()
+      throws MarquezServiceException, ResourceException {
     final List<Namespace> existingCoreModelNamespaces = Collections.singletonList(TEST_NAMESPACE);
     NamespaceService namespaceService = mock(NamespaceService.class);
     NamespaceResource namespaceResource = new NamespaceResource(namespaceService);
