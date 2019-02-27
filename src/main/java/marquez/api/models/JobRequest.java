65778494f23758abe26a956921e3680a62d7eca7
wslulciuc <willy.lulciuc@wework.com>
2019-02-27 07:44:52 -0800

    Annotate constructor with @JsonCreator

--- src/main/java/marquez/api/models/JobRequest.java
+++ src/main/java/marquez/api/models/JobRequest.java
@@ -1,29 +1,25 @@
 package marquez.api.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.List;
+import java.util.Optional;
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@JsonCreator))
+@EqualsAndHashCode
+@ToString
 public final class JobRequest {
+  @Getter @NotNull private List<String> inputDatasetUrns;
+  @Getter @NotNull private List<String> outputDatasetUrns;
+  @Getter @NotNull private String location;
+  @Nullable private String description;
 
-  @JsonProperty("location")
-  @NotNull
-  private String location;
-
-  @JsonProperty("description")
-  private String description;
-
-  @JsonProperty("inputDatasetUrns")
-  @NotNull
-  public List<String> inputDataSetUrns;
-
-  @JsonProperty("outputDatasetUrns")
-  @NotNull
-  public List<String> outputDatasetUrns;
+  public Optional<String> getDescription() {
+    return Optional.ofNullable(description);
+  }
 }
