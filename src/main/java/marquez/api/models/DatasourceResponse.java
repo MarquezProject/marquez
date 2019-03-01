af83f0c7b92b6c9b70f1ebb672c2188706b011f9
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-02-28 18:07:55 -0800

    Adjust response to use @Value annotation

--- src/main/java/marquez/api/models/DatasourceResponse.java
+++ src/main/java/marquez/api/models/DatasourceResponse.java
@@ -1,21 +1,10 @@
 package marquez.api.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Value
 public class DatasourceResponse {
-
-  @JsonProperty("createdAt")
-  private String createdAt;
-
-  @JsonProperty("name")
   private String name;
-
-  @JsonProperty("connectionUrl")
+  private String createdAt;
   private String connectionUrl;
 }
