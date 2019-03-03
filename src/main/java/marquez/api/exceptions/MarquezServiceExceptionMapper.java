3c87ef0d6a00edad58f75c801ab5055e87d0e3ea
wslulciuc <willy.lulciuc@wework.com>
2019-03-03 09:00:57 -0800

    Add license and use ErrorResponse

--- src/main/java/marquez/api/exceptions/MarquezServiceExceptionMapper.java
+++ src/main/java/marquez/api/exceptions/MarquezServiceExceptionMapper.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package marquez.api.exceptions;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
@@ -5,6 +19,7 @@ import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import marquez.api.models.ErrorResponse;
 import marquez.service.exceptions.MarquezServiceException;
 
 public final class MarquezServiceExceptionMapper
@@ -13,7 +28,7 @@ public final class MarquezServiceExceptionMapper
   public Response toResponse(MarquezServiceException e) {
     return Response.status(INTERNAL_SERVER_ERROR)
         .type(APPLICATION_JSON_TYPE)
-        .entity(new ErrorMessage(INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage()))
+        .entity(new ErrorResponse(INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage()))
         .build();
   }
 }
