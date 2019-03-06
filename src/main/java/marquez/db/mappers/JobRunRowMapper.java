bdd593e732fa6b80f9fabb891705834204d8c040
hxw <xhuang110@myseneca.ca>
2019-03-06 12:17:14 -0500

    updated to return JobRunRow

--- src/main/java/marquez/db/mappers/JobRunRowMapper.java
+++ src/main/java/marquez/db/mappers/JobRunRowMapper.java
@@ -19,15 +19,16 @@ import java.sql.SQLException;
 import java.util.UUID;
 import lombok.NonNull;
 import marquez.db.Columns;
-import marquez.service.models.JobRun;
+//import marquez.service.models.JobRun;
+import marquez.db.models.JobRunRow;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
-public final class JobRunRowMapper implements RowMapper<JobRun> {
+public final class JobRunRowMapper implements RowMapper<JobRunRow> {
   @Override
-  public JobRun map(@NonNull ResultSet results, @NonNull StatementContext context)
+  public JobRunRow map(@NonNull ResultSet results, @NonNull StatementContext context)
       throws SQLException {
-    return new JobRun(
+    return new JobRunRow(
         results.getObject(Columns.ROW_UUID, UUID.class),
         results.getInt(Columns.CURRENT_RUN_STATE),
         results.getObject(Columns.JOB_VERSION_UUID, UUID.class),
