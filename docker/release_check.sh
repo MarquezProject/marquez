d02eba4da46b6050f80a988ae9b797efd5dc319a
Aleks Shulman <ashulman@NYC-US07347-MBP.attlocal.net>
2019-03-10 15:09:25 -0700

    v1 of release checking

---
+++ docker/release_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+function does_package_version_match()
+{
+  expected_version=$(git describe --tags | HEAD)
+  current_version=$(python ./setup.py --version)
+  echo "current_version is ${current_version}"
+  echo "expected_version is ${expected_version}"
+
+  if [[ "${current_version}" != "${expected_version}" ]]; then
+
+    echo "no match"
+    return 1
+  else
+
+    echo "match"
+    return 0
+  fi
+}
+
+does_package_version_match
