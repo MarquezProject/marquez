40e10d6e35bfe2e1903a863b01120741d940f1c8
wslulciuc <willy.lulciuc@wework.com>
2019-03-15 16:27:42 -0700

    continued: Add UrnPatternTest

--- src/test/java/marquez/common/models/UrnPatternTest.java
+++ src/test/java/marquez/common/models/UrnPatternTest.java
@@ -1,32 +1,25 @@
 package marquez.common.models;
 
-import static org.junit.Assert.assertNotNull;
+import static java.util.stream.Collectors.joining;
 
+import java.util.StringJoiner;
+import java.util.stream.Stream;
 import marquez.UnitTests;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(UnitTests.class)
 public class UrnPatternTest {
+  private static final int ALLOWED_PART_SIZE = 64;
+  private static final int PART_SIZE_GREATER_THAN_ALLOWED = ALLOWED_PART_SIZE + 1;
   private static final String NAMESPACE = "test";
   private static final int NUM_OF_PARTS = 2;
-
-  @Test
-  public void testNewUrnPattern() {
-    final UrnPattern pattern = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
-    assertNotNull(pattern);
-  }
+  private static final UrnPattern PATTERN = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
 
   @Test(expected = NullPointerException.class)
   public void testNewUrnPattern_throwsException_onNullNamespace() {
     final String nullNamespace = null;
     UrnPattern.from(nullNamespace, NUM_OF_PARTS);
-  }
-
-  @Test(expected = NullPointerException.class)
-  public void testNewUrnPattern_throwsException_onNullNumOfParts() {
-    final Integer nullNumOfParts = null;
-    UrnPattern.from(NAMESPACE, nullNumOfParts);
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -41,36 +34,71 @@ public class UrnPatternTest {
     UrnPattern.from(blankNamespace, NUM_OF_PARTS);
   }
 
+  @Test(expected = NullPointerException.class)
+  public void testNewUrnPattern_throwsException_onNullNumOfParts() {
+    final Integer nullNumOfParts = null;
+    UrnPattern.from(NAMESPACE, nullNumOfParts);
+  }
   @Test
   public void testThrowIfNoMatch() {
     final String value = "urn:test:a:b";
-    final UrnPattern pattern = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
-    pattern.throwIfNoMatch(value);
+    PATTERN.throwIfNoMatch(value);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testThrowIfNoMatch_throwsException_onNoMatchValue() {
+    final String noMatchValue = "urn:other:a:b";
+    PATTERN.throwIfNoMatch(noMatchValue);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testThrowIfNoMatch_throwsException_onNoPrefixValue() {
+    final String noPrefixValue = "other:a:b";
+    PATTERN.throwIfNoMatch(noPrefixValue);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNewDatasetUrn_throwsException_onMissingPartValue() {
+    final String missingPartValue = "urn:test:a";
+    PATTERN.throwIfNoMatch(missingPartValue);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testThrowIfNoMatch_throwsException_onExtraPartValue() {
+    final String extraPartValue = "urn:test:a:b:c";
+    PATTERN.throwIfNoMatch(extraPartValue);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testThrowIfNoMatch_throwsException_onNonAlphanumericPartValue() {
+    final String nonAlphanumericPartValue = "urn:test:a:b$c^";
+    PATTERN.throwIfNoMatch(nonAlphanumericPartValue);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testThrowIfNoMatch_throwsException_onPartGreaterThan64Value() {
+    final String partGreaterThan64Value = newUrnWithPartGreaterThan64();
+    PATTERN.throwIfNoMatch(partGreaterThan64Value);
   }
 
   @Test(expected = NullPointerException.class)
   public void testThrowIfNoMatch_throwsException_onNullValue() {
     final String nullValue = null;
-    final UrnPattern pattern = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
-    pattern.throwIfNoMatch(nullValue);
+    PATTERN.throwIfNoMatch(nullValue);
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testNewUrnPattern_throwsException_onEmptyValue() {
-    final String emptyValue = "";
-    UrnPattern.from(emptyValue, NUM_OF_PARTS);
+  private String newUrnWithPartGreaterThan64() {
+    final String part0 = newUrnPart("a", ALLOWED_PART_SIZE);
+    final String part1GreaterThan64 = newUrnPart("b", PART_SIZE_GREATER_THAN_ALLOWED);
+
+    return new StringJoiner(UrnPattern.DELIM)
+        .add(UrnPattern.PREFIX)
+        .add(part0)
+        .add(part1GreaterThan64)
+        .toString();
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testNewUrnPattern_throwsException_onBlankValue() {
-    final String blankValue = " ";
-    UrnPattern.from(blankValue, NUM_OF_PARTS);
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void testThrowIfNoMatch_throwsException_onNoMatchValue() {
-    final String noMatchValue = "urn:a:b";
-    final UrnPattern pattern = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
-    pattern.throwIfNoMatch(noMatchValue);
+  private String newUrnPart(String part, Integer limit) {
+    return Stream.generate(() -> part).limit(limit).collect(joining());
   }
 }
