8da0de9034f3764342ef6828bd27e1b658a9e98d
ashulmanWeWork <41963174+ashulmanWeWork@users.noreply.github.com>
2019-03-15 17:00:45 -0700

    [Datasource Service - Part 4]: Adjust datasource create response code (#397)
    * Fix datasource creation response code
    
    * Adding additional datasourcedao test case
    
    * Stylistic fix - refactoring datasourceresponse creation in the create response
    Also doing a static import of CREATED instead of fully-qualified name
    
    * Stylistic fix
    
    * Fix

--- src/main/java/marquez/api/resources/DatasourceResource.java
+++ src/main/java/marquez/api/resources/DatasourceResource.java
@@ -15,6 +15,7 @@
 package marquez.api.resources;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.CREATED;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.ResponseMetered;
@@ -71,7 +72,7 @@ public final class DatasourceResource {
     final Datasource createdDatasource =
         datasourceService.create(
             connectionUrl, DatasourceName.fromString(datasourceRequest.getName()));
-    return Response.ok(DatasourceResponseMapper.map(createdDatasource)).build();
+    return Response.status(CREATED).entity(DatasourceResponseMapper.map(createdDatasource)).build();
   }
 
   @GET
