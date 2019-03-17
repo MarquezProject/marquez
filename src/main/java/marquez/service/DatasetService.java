0db24f93cfc79af18ad24787e2cf0cfddf0a3472
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-03-17 13:34:37 -0700

    Fixes

--- src/main/java/marquez/service/DatasetService.java
+++ src/main/java/marquez/service/DatasetService.java
@@ -47,14 +47,14 @@ public class DatasetService {
   public Dataset create(
       @NonNull NamespaceName namespaceName, @NonNull DbTableVersion dbTableVersion)
       throws MarquezServiceException {
-    final DatasourceRow dataSourceRow = DatasourceRowMapper.map(dbTableVersion);
+    final DatasourceRow datasourceRow = DatasourceRowMapper.map(dbTableVersion);
     final DatasetRow datasetRow =
-        DatasetRowMapper.map(namespaceName, dataSourceRow, dbTableVersion);
+        DatasetRowMapper.map(namespaceName, datasourceRow, dbTableVersion);
     final DbTableInfoRow dbTableInfoRow = DbTableInfoRowMapper.map(dbTableVersion);
     final DbTableVersionRow dbTableVersionRow =
         DbTableVersionRowMapper.map(datasetRow, dbTableInfoRow, dbTableVersion);
     try {
-      datasetDao.insertAll(dataSourceRow, datasetRow, dbTableInfoRow, dbTableVersionRow);
+      datasetDao.insertAll(datasourceRow, datasetRow, dbTableInfoRow, dbTableVersionRow);
       final Optional<DatasetRow> datasetRowIfFound = datasetDao.findBy(datasetRow.getUuid());
       return datasetRowIfFound.map(DatasetMapper::map).orElseThrow(MarquezServiceException::new);
     } catch (UnableToExecuteStatementException e) {
