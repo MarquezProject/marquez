2fa16a3146b8c7f8539bc07b5da102bc7d3500bf
Willy Lulciuc <willy.lulciuc@wework.com>
2019-03-18 11:37:44 -0700

    Cleanup class DatasourceUrn and DatasetUrn (#398)
    * Cleanup class DatasourceUrn and DatasetUrn
    
    * Add UrnType.numberOfParts() and more cleanup to urn classes
    
    * Replace JsonProperty with JsonCreator
    
    * Remove UrnType
    
    * Add class Urn
    
    * Add @Getter to class urn
    
    * Limit method/variable scope in class UrnPattern
    
    * Add abstract pattern() to base class Urn
    
    * Remove UrnTest for abstract class Urn
    
    * Include periods in urn pattern error message
    
    * Make urn type explicit on string build
    
    * Fix DatasourceUrnTest.testFromString()
    
    * Set urn namespace
    
    * Remove @Slf4j from class UrnPattern
    
    * Add urn value to pattern error message
    
    * continued: Add urn value to pattern error message
    
    * Minor reorg of fields in urn classes
    
    * Add UrnPatternTest
    
    * continued: Add UrnPatternTest
    
    * continued: Add UrnPatternTest
    
    * Apply formatting
    
    * Fix DatasetUrn.Value string
    
    * continued: Fix DatasetUrn.Value string
    
    * Add DatasourceUrnTest
    
    * Build Urn.Value in tests
    
    * Add license to UrnPatternTest.java
    
    * Make DatasetUrn.fromString() tests explicit int test name
    
    * Minor rename of tests
    
    * Add DatasourceNameTest
    
    * Ensure part is not blank in Urn.valueFrom()
    
    * UrnPatternTest.testThrowIfNoMatch_noExceptionThrown()
    
    * Add check to ensure numOfParts > 0
    
    * Minor test rename in DatasetUrnTest
    
    * Add UrnPatternTest.testFrom_throwsException_onNegativeNumOfParts()

--- src/main/java/marquez/common/models/DatasourceUrn.java
+++ src/main/java/marquez/common/models/DatasourceUrn.java
@@ -1,25 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package marquez.common.models;
 
-import java.util.regex.Pattern;
-import lombok.EqualsAndHashCode;
+import static marquez.common.Preconditions.checkNotBlank;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.NonNull;
 
-@EqualsAndHashCode(callSuper = false)
 public final class DatasourceUrn extends Urn {
-  private static final int NUM_COMPONENTS = 2;
-  private static final String URN_TYPE = "datasource";
-  private static final Pattern REGEX = buildPattern(URN_TYPE, NUM_COMPONENTS);
+  private static final String NAMESPACE = "datasource";
+  private static final int NUM_OF_PARTS = 2;
+  private static final UrnPattern PATTERN = UrnPattern.from(NAMESPACE, NUM_OF_PARTS);
 
-  public DatasourceUrn(@NonNull String value) {
-    super(value, REGEX);
+  private DatasourceUrn(@NonNull final String value) {
+    super(checkNotBlank(value));
   }
 
-  public static DatasourceUrn from(@NonNull String type, @NonNull String name) {
-    final String value = fromComponents(URN_TYPE, type, name);
+  public static DatasourceUrn from(@NonNull DatasourceType type, @NonNull DatasourceName name) {
+    final String value = valueFrom(NAMESPACE, type.toString(), name.getValue());
     return fromString(value);
   }
 
+  @JsonCreator
   public static DatasourceUrn fromString(String value) {
     return new DatasourceUrn(value);
+  }
+
+  @Override
+  public String namespace() {
+    return NAMESPACE;
+  }
+
+  @Override
+  UrnPattern pattern() {
+    return PATTERN;
   }
 }
--- src/test/java/marquez/common/models/DatasourceUrnTest.java
+++ src/test/java/marquez/common/models/DatasourceUrnTest.java
@@ -1,23 +1,74 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package marquez.common.models;
 
 import static org.junit.Assert.assertEquals;
 
+import marquez.UnitTests;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+@Category(UnitTests.class)
 public class DatasourceUrnTest {
+  private static final String NAMESPACE = "datasource";
+  private static final String VALUE = String.format("urn:%s:postgresql:test", NAMESPACE);
+
+  private static final DatasourceType DATASOURCE_TYPE = DatasourceType.POSTGRESQL;
+  private static final DatasourceName DATASOURCE_NAME = DatasourceName.fromString("test");
+
   @Test
-  public void testFromString() {
-    String urn = "urn:datasource:a:b";
-    assertEquals(urn, DatasourceUrn.fromString(urn).toString());
+  public void testNewDatasourceUrn_from() {
+    final DatasourceUrn urn = DatasourceUrn.from(DATASOURCE_TYPE, DATASOURCE_NAME);
+    assertEquals(VALUE, urn.getValue());
+    assertEquals(NAMESPACE, urn.namespace());
+  }
+
+  @Test
+  public void testNewDatasourceUrn_fromString() {
+    final DatasourceUrn urn = DatasourceUrn.fromString(VALUE);
+    assertEquals(VALUE, urn.getValue());
+    assertEquals(NAMESPACE, urn.namespace());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testFrom_throwsException_onNullDatasourceType() {
+    final DatasourceType nullDatasourceType = null;
+    DatasourceUrn.from(nullDatasourceType, DATASOURCE_NAME);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void tesFrom_throwsException_onNullDatasourceName() {
+    final DatasourceName nullDatasourceName = null;
+    DatasourceUrn.from(DATASOURCE_TYPE, nullDatasourceName);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testFromString_throwsException_onNullValue() {
+    final String nullValue = null;
+    DatasourceUrn.fromString(nullValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testOfTooFewComponents() {
-    DatasourceUrn.fromString("urn:datasource:a");
+  public void testFromString_throwsException_onEmptyValue() {
+    final String emptyValue = "";
+    DatasourceUrn.fromString(emptyValue);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testOfTooManyComponents() {
-    DatasourceUrn.fromString("urn:datasource:a:b:c");
+  public void testFromString_throwsException_onBlankValue() {
+    final String blankValue = " ";
+    DatasourceUrn.fromString(blankValue);
   }
 }
