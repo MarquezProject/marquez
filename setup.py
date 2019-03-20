b19f36799327c1c21ffe75d903d1e28817a01df0
Rodrigo Araya <rodrigo.araya@gmail.com>
2019-03-20 12:41:07 -0700

    Revert "Bump version: 0.1.7 → 0.1.8"
    This reverts commit 5ee6022de775f3257dca256059bde123843f9012.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@
 from setuptools import find_packages, setup  # noqa: H301
 
 NAME = "marquez-python"
-VERSION = "0.1.8"
+VERSION = "0.1.7"
 # To install the library, run the following
 #
 # python setup.py install
