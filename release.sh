fda4b80dcbbbd30975ba47fdb3250b120694e85b
ashulmanWeWork <41963174+ashulmanWeWork@users.noreply.github.com>
2019-03-20 12:34:27 -0700

    Add auto release (#20)
    * v1 of release checking
    
    * Temporarily disable upload to allow for testing
    
    * Escape string to avoid issue
    
    * Remove colon to prevent circleCI issue
    
    * Add a readme comment
    
    * Improve error message
    
    * Add license info
    
    * Adjusting instructions
    
    * Some changes to push automatically
    
    * Changes made
    
    * Adjust to new workflow
    
    * v1 of release checking
    
    * Upping version to 1.7
    
    * Bump version: 0.1.7 → 0.1.8
    
    * Bump version: 0.1.8 → 0.1.9
    
    * Adjust script
    
    * Bump version: 0.1.9 → 0.2.0
    
    * Bump version: 0.2.0 → 1.0.0
    
    * Bump version: 1.0.0 → 2.0.0
    
    * Fix to release script to parse type correctly
    
    * Bump version: 2.0.0 → 2.0.1
    
    * Bump version: 2.0.1 → 3.0.0
    
    * Bump version: 3.0.0 → 3.1.0
    
    * Bump version: 3.1.0 → 3.1.1
    
    * Bump version: 3.1.1 → 4.0.0
    
    * Move release check
    
    * Inline simple functions
    
    * Add master branch check
    
    * Fix to versioning
    
    * Remove unnecessary change
    
    * Minor fix
    
    * Fix script error
    
    * Reset version
    
    * Fix to restore upload

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e -x
+
+# Usage: $ ./prepare_for_release.py <type>
+# type - [major | minor | patch]. Default is patch.
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "${branch}" != "master" ]]; then
+  echo "You may only tag a commit on the 'master' branch"
+  exit 1;
+fi
+
+type=${1}
+if [ -z "${type}" ]
+then
+  echo "defaulting to 'patch'"
+  type="patch"
+else
+  echo "update type is: ${type}"
+fi
+
+version=$(python ./setup.py --version)  
+echo "Upgrading version from the current version of ${version}"
+
+
+# The {new_version} is not a bash variable - it's a bumpversion notation for the new version.
+# Please see bumpversion --help for more information.
+bumpversion --current-version ${version} --commit --tag --tag-name {new_version} ${type} ./setup.py
+git push --tags origin master
+echo "Done pushing to master"
