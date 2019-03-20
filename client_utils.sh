5dcb362810acaa169bd1b42f26baedfedea0e719
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-03-20 12:13:18 -0700

    Store temporary state

--- client_utils.sh
+++ client_utils.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
+set +x
 
-export MARQUEZ_PYTHON_CLIENT_CLONE_DIR="/tmp/marquez-python-client-codegen"
+export MARQUEZ_CLONE_DIR="/tmp/marquez"
+export MARQUEZ_PYTHON_CLIENT_CODEGEN_CLONE_DIR="/tmp/marquez-python-client-codegen"
 export OPEN_API_GENERATOR_CLONE_DIR="/tmp/marquez-python-client-codegen"
 
 get_current_package_version()
@@ -8,24 +11,36 @@ get_current_package_version()
   echo $(python ./setup.py --version)
 }
 
+get_current_marquez_python_client_codegen_version()
+{
+  echo $(python ${MARQUEZ_PYTHON_CLIENT_CLONE_DIR}/setup.py --version)
+}
+
 clone_marquez_python_client_codegen()
 {
-  clone_dir=${MARQUEZ_PYTHON_CLIENT_CLONE_DIR}
+  clone_dir=${MARQUEZ_PYTHON_CLIENT_CODEGEN_CLONE_DIR}
   rm -rf ${clone_dir} || true
-  git clone git@github.com:MarquezProject/marquez-python-client-codegen.git ${clone_dir}
+  git clone https://github.com/ashulmanwework/marquez-python-client-codegen.git ${clone_dir}
 }
+
+clone_marquez()
+{
+  clone_dir=${MARQUEZ_CLONE_DIR}
+  rm -rf ${clone_dir} || true
+
+  git clone https://github.com/ashulmanwework/marquez.git ${clone_dir}
 
 clone_openapi_generator()
 {
   clone_dir=${OPEN_API_GENERATOR_CLONE_DIR}
   rm -rf ${clone_dir} || true
 
-  git clone git@github.com:OpenAPITools/openapi-generator.git ${clone_dir}
+  git clone https://github.com/OpenAPITools/openapi-generator.git ${clone_dir}
 }
 
 regenerate_api_spec()
 {
-  cd ${MARQUEZ_PYTHON_CLIENT_CLONE_DIR}  
+  cd ${MARQUEZ_PYTHON_CLIENT_CODEGEN_CLONE_DIR}  
   CURRENT_VERSION=$(get_current_package_version)
   
   clone_openapi-generator
@@ -37,10 +52,15 @@ regenerate_api_spec()
   java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate \
    -i ~/git-projects/marquez/docs/openapi.yml \
    -g python \
-   -o /tmp/${MARQUEZ_PYTHON_CLIENT_CLONE_DIR} -c ./config.json \
+   -o /tmp/${MARQUEZ_PYTHON_CLIENT_CODEGEN_CLONE_DIR} -c ./config.json \
    --skip-validate-spec  
 }
 
-
+refresh_codegen()
+{
+  clone_marquez_python_client_codegen
+  clone_marquez
+  #rege
+  
 
 
