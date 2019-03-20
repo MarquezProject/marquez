232eaf6ce89981dee801887553ddb7165bb4f660
Rodrigo Araya <rodrigo.araya@gmail.com>
2019-03-20 13:31:15 -0700

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@
 from setuptools import find_packages, setup  # noqa: H301
 
 NAME = "marquez-python"
-VERSION = "0.1.10"
+VERSION = "0.1.11"
 # To install the library, run the following
 #
 # python setup.py install
