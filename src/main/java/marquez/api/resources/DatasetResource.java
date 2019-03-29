3e0c586f90f6657e9c05a8f049138cb0de6277ea
wslulciuc <willy.lulciuc@wework.com>
2019-03-29 13:43:50 -0700

    Add dataset creation endpoint

--- src/main/java/marquez/api/resources/DatasetResource.java
+++ src/main/java/marquez/api/resources/DatasetResource.java
@@ -15,41 +15,73 @@
 package marquez.api.resources;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-import static marquez.api.mappers.DatasetResponseMapper.map;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.ResponseMetered;
 import com.codahale.metrics.annotation.Timed;
 import java.util.List;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import lombok.NonNull;
+import marquez.api.exceptions.DatasourceUrnNotFoundException;
+import marquez.api.exceptions.NamespaceNotFoundException;
+import marquez.api.mappers.DatasetMapper;
+import marquez.api.mappers.DatasetResponseMapper;
+import marquez.api.models.DatasetRequest;
 import marquez.api.models.DatasetResponse;
 import marquez.api.models.DatasetsResponse;
 import marquez.common.models.NamespaceName;
 import marquez.service.DatasetService;
+import marquez.service.DatasourceService;
 import marquez.service.NamespaceService;
 import marquez.service.exceptions.MarquezServiceException;
 import marquez.service.models.Dataset;
+import marquez.service.models.Datasource;
 
 @Path("/api/v1")
 public final class DatasetResource {
   private final NamespaceService namespaceService;
+  private final DatasourceService datasourceService;
   private final DatasetService datasetService;
 
   public DatasetResource(
       @NonNull final NamespaceService namespaceService,
+      @NonNull final DatasourceService datasourceService,
       @NonNull final DatasetService datasetService) {
     this.namespaceService = namespaceService;
+    this.datasourceService = datasourceService;
     this.datasetService = datasetService;
+  }
+
+  @POST
+  @ResponseMetered
+  @ExceptionMetered
+  @Timed
+  @Path("/namespaces/{namespace}/datasets")
+  @Consumes(APPLICATION_JSON)
+  @Produces(APPLICATION_JSON)
+  public Response create(
+      @NonNull @PathParam("namespace") NamespaceName namespaceName,
+      @NonNull @Valid DatasetRequest request)
+      throws MarquezServiceException {
+    throwIfNotExists(namespaceName);
+    final Datasource datasource =
+        datasourceService
+            .get(request.getDatasourceUrn())
+            .orElseThrow(() -> new DatasourceUrnNotFoundException(request.getDatasourceUrn()));
+    final Dataset newDataset = DatasetMapper.map(request);
+    final Dataset dataset =
+        datasetService.create(namespaceName, datasource.getName(), datasource.getUrn(), newDataset);
+    final DatasetResponse response = DatasetResponseMapper.map(dataset);
+    return Response.ok(response).build();
   }
 
   @GET
@@ -59,22 +91,20 @@ public final class DatasetResource {
   @Path("/namespaces/{namespace}/datasets")
   @Produces(APPLICATION_JSON)
   public Response list(
-      @PathParam("namespace") NamespaceName namespaceName,
-      @QueryParam("limit") @DefaultValue("100") Integer limit,
-      @QueryParam("offset") @DefaultValue("0") Integer offset)
-      throws MarquezServiceException, WebApplicationException {
+      @NonNull @PathParam("namespace") NamespaceName namespaceName,
+      @NonNull @QueryParam("limit") @DefaultValue("100") Integer limit,
+      @NonNull @QueryParam("offset") @DefaultValue("0") Integer offset)
+      throws MarquezServiceException {
+    throwIfNotExists(namespaceName);
+    final List<Dataset> datasets = datasetService.getAll(namespaceName, limit, offset);
+    final DatasetsResponse response = DatasetResponseMapper.toDatasetsResponse(datasets);
+    return Response.ok(response).build();
+  }
+
+  private void throwIfNotExists(@NonNull NamespaceName namespaceName)
+      throws MarquezServiceException {
     if (!namespaceService.exists(namespaceName)) {
-      throw new WebApplicationException(
-          String.format("The namespace %s does not exist.", namespaceName.getValue()), NOT_FOUND);
-    }
-    try {
-      String namespaceNameString = namespaceName.getValue();
-      final List<Dataset> datasets =
-          datasetService.getAll(NamespaceName.fromString(namespaceNameString), limit, offset);
-      final List<DatasetResponse> datasetResponses = map(datasets);
-      return Response.ok(new DatasetsResponse(datasetResponses)).build();
-    } catch (Exception e) {
-      throw new InternalServerErrorException("unexpected error while fetching datasets", e);
+      throw new NamespaceNotFoundException(namespaceName);
     }
   }
 }
