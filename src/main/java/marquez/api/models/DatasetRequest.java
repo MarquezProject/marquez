95000b26a105fe36b54689e751c332699e5ec386
wslulciuc <willy.lulciuc@wework.com>
2019-03-29 10:51:22 -0700

    Use types in class DatasetRequest

--- src/main/java/marquez/api/models/DatasetRequest.java
+++ src/main/java/marquez/api/models/DatasetRequest.java
@@ -1,23 +1,25 @@
 package marquez.api.models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Optional;
 import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-import org.hibernate.validator.constraints.NotBlank;
+import marquez.common.models.DatasetName;
+import marquez.common.models.DatasourceUrn;
+import marquez.common.models.Description;
 
-@AllArgsConstructor(onConstructor = @__(@JsonCreator))
+@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public final class DatasetRequest {
-  @Getter @NotBlank private final String name;
-  @Getter @NotBlank private final String datasourceUrn;
-  @Nullable private String description;
+  @Getter @NotNull private final DatasetName name;
+  @Getter @NotNull private final DatasourceUrn datasourceUrn;
+  @Nullable private final Description description;
 
-  public Optional<String> getDescription() {
+  public Optional<Description> getDescription() {
     return Optional.ofNullable(description);
   }
 }
