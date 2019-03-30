575c77edf8b3c7ef49e876d9dad51bdd7ed62ea2
wslulciuc <willy.lulciuc@wework.com>
2019-03-30 13:09:58 -0700

    Use @Data in class DatasourceRow

--- src/main/java/marquez/db/models/DatasourceRow.java
+++ src/main/java/marquez/db/models/DatasourceRow.java
@@ -15,28 +15,16 @@
 package marquez.db.models;
 
 import java.time.Instant;
-import java.util.Optional;
 import java.util.UUID;
-import javax.annotation.Nullable;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.Data;
 
-@RequiredArgsConstructor
-@EqualsAndHashCode
-@ToString
+@Data
 @Builder
 public final class DatasourceRow {
-  @Getter @NonNull private final UUID uuid;
-  @Getter @NonNull private final String urn;
-  @Getter @NonNull private final String name;
-  @Getter @NonNull private final String connectionUrl;
-  @Nullable private final Instant createdAt;
-
-  public Optional<Instant> getCreatedAt() {
-    return Optional.ofNullable(createdAt);
-  }
+  private UUID uuid;
+  private Instant createdAt;
+  private String name;
+  private String urn;
+  private String connectionUrl;
 }
