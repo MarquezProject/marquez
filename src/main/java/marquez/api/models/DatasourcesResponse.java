3bacefd2437faed1ae9e9a0b7febff398fe7a516
wslulciuc <willy.lulciuc@wework.com>
2019-03-30 15:07:52 -0700

    Annotate class DatasourcesResponse with Data

--- src/main/java/marquez/api/models/DatasourcesResponse.java
+++ src/main/java/marquez/api/models/DatasourcesResponse.java
@@ -14,16 +14,11 @@
 
 package marquez.api.models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Data;
+import lombok.NonNull;
 
-@AllArgsConstructor(onConstructor = @__(@JsonCreator))
-@EqualsAndHashCode
-@ToString
+@Data
 public class DatasourcesResponse {
-  @Getter private final List<DatasourceResponse> datasources;
+  @NonNull private final List<DatasourceResponse> datasources;
 }
