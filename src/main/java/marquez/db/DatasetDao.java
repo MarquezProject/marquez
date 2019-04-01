b438b38cf33683b14b544a4153b29530d57b8762
wslulciuc <willy.lulciuc@wework.com>
2019-04-01 10:34:51 -0700

    Add DatasetDao.insertAndGet() and more refactoring

--- src/main/java/marquez/db/DatasetDao.java
+++ src/main/java/marquez/db/DatasetDao.java
@@ -19,12 +19,15 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import marquez.common.models.DatasetUrn;
+import marquez.common.models.DatasourceUrn;
 import marquez.common.models.NamespaceName;
+import marquez.db.exceptions.RowNotFoundException;
 import marquez.db.mappers.DatasetRowMapper;
 import marquez.db.models.DatasetRow;
 import marquez.db.models.DatasourceRow;
 import marquez.db.models.DbTableInfoRow;
 import marquez.db.models.DbTableVersionRow;
+import marquez.db.models.NamespaceRow;
 import org.jdbi.v3.sqlobject.CreateSqlObject;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
@@ -35,15 +38,42 @@ import org.jdbi.v3.sqlobject.transaction.Transaction;
 @RegisterRowMapper(DatasetRowMapper.class)
 public interface DatasetDao {
   @CreateSqlObject
+  NamespaceDao createNamespaceDao();
+
+  @CreateSqlObject
   DatasourceDao createDatasourceDao();
 
   @CreateSqlObject
   DbTableVersionDao createDbTableVersionDao();
 
-  @SqlUpdate(
-      "INSERT INTO datasets (guid, namespace_guid, datasource_uuid, urn, description) "
-          + "VALUES (:uuid, :namespaceUuid, :datasourceUuid, :urn, :description)")
-  void insert(@BindBean DatasetRow datasetRow);
+  @SqlQuery(
+      "INSERT INTO datasets (uuid, namespace_uuid, datasource_uuid, name, urn, description) "
+          + "VALUES (:uuid, :namespaceUuid, :datasourceUuid, :name, :urn, :description) "
+          + "RETURNING *")
+  Optional<DatasetRow> insertAndGet(@BindBean DatasetRow datasetRow);
+
+  @Transaction
+  default Optional<DatasetRow> insertAndGet(
+      NamespaceName namespaceName, DatasourceUrn datasourceUrn, DatasetRow datasetRow)
+      throws RowNotFoundException {
+    final NamespaceRow namespaceRow =
+        createNamespaceDao()
+            .findBy(namespaceName)
+            .orElseThrow(
+                () ->
+                    new RowNotFoundException(
+                        "Namespace row not found: " + namespaceName.getValue()));
+    final DatasourceRow datasourceRow =
+        createDatasourceDao()
+            .findBy(datasourceUrn)
+            .orElseThrow(
+                () ->
+                    new RowNotFoundException(
+                        "Datasource row not found: " + datasourceUrn.getValue()));
+    datasetRow.setNamespaceUuid(namespaceRow.getUuid());
+    datasetRow.setDatasourceUuid(datasourceRow.getUuid());
+    return insertAndGet(datasetRow);
+  }
 
   @Transaction
   default void insertAll(
@@ -52,28 +82,31 @@ public interface DatasetDao {
       DbTableInfoRow dbTableInfoRow,
       DbTableVersionRow dbTableVersionRow) {
     createDatasourceDao().insert(datasourceRow);
-    insert(datasetRow);
+    insertAndGet(datasetRow);
     createDbTableVersionDao().insertAll(dbTableInfoRow, dbTableVersionRow);
     updateCurrentVersion(datasetRow.getUuid(), Instant.now(), dbTableVersionRow.getUuid());
   }
 
+  @SqlQuery("SELECT EXISTS (SELECT 1 FROM datasets WHERE urn = :value)")
+  boolean exists(@BindBean DatasetUrn datasetUrn);
+
   @SqlUpdate(
-      "UPDATE datasets SET updated_at = :updatedAt, current_version_uuid = :currentVersion "
-          + "WHERE guid = :uuid")
+      "UPDATE datasets SET updated_at = :updatedAt, current_version_uuid = :currentVersionUuid "
+          + "WHERE uuid = :uuid")
   void updateCurrentVersion(UUID uuid, Instant updatedAt, UUID currentVersion);
 
   @SqlQuery("SELECT * FROM datasets WHERE uuid = :uuid")
   Optional<DatasetRow> findBy(UUID uuid);
 
-  @SqlQuery("SELECT * FROM datasets WHERE urn = :urn.value")
-  Optional<DatasetRow> findBy(@BindBean("urn") DatasetUrn urn);
+  @SqlQuery("SELECT * FROM datasets WHERE urn = :value")
+  Optional<DatasetRow> findBy(@BindBean DatasetUrn datasetUrn);
 
   @SqlQuery(
       "SELECT * "
           + "FROM datasets d "
           + "INNER JOIN namespaces n "
-          + "     ON (n.guid = d.namespace_guid AND n.name=:namespace.value)"
+          + "    ON (n.guid = d.namespace_uuid AND n.name = :value)"
+          + "ORDER BY name "
           + "LIMIT :limit OFFSET :offset")
-  List<DatasetRow> findAll(
-      @BindBean("namespace") NamespaceName namespaceName, Integer limit, Integer offset);
+  List<DatasetRow> findAll(@BindBean NamespaceName namespaceName, Integer limit, Integer offset);
 }
