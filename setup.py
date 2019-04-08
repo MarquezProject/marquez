9b092319a3614a2bedabf4975fce757de8c23fab
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-04-08 13:17:18 -0500

    Bump version: 0.1.12 → 0.1.13

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@
 from setuptools import find_packages, setup  # noqa: H301
 
 NAME = "marquez-python"
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 # To install the library, run the following
 #
 # python setup.py install
