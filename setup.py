78a3832ed5317be9b10774c186247e2c87626f64
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-04-25 10:45:41 -0700

    Bump version: 0.1.13 → 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 from setuptools import find_packages, setup
 
 NAME = "marquez-python"
-VERSION = "0.1.13"
+VERSION = "0.2.0"
 # To install the library, run the following
 #
 # python setup.py install
