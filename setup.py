7e248a0c56a737b70dc8419e1119c11a16dc84a0
wslulciuc <willy.lulciuc@wework.com>
2019-05-30 07:46:08 -0700

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 from setuptools import find_packages, setup
 
 NAME = "marquez-python"
-VERSION = "0.4.1"
+VERSION = "0.5.0"
 # To install the library, run the following
 #
 # python setup.py install
