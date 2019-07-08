ea934a78e113fbcbfb5d56c20b4e3485d4bb0833
Soumya Smruti Mishra <soumyasmruti@users.noreply.github.com>
2019-07-08 09:22:11 -0700

    update usage message

--- docker/wait-for-db.sh
+++ docker/wait-for-db.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Usage: $ ./wait-for-db.sh <host> <port>
+# Usage: $ ./wait-for-db.sh <host> <port> [timeout]
 
 set -eu
 
