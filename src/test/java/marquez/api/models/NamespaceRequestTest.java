86d4e48005085e3185ddc9f11d0ab363d6e2d159
wslulciuc <willy.lulciuc@wework.com>
2019-07-21 13:52:30 -0700

    Add to and from json tests for class NamespaceRequest

--- src/test/java/marquez/api/models/NamespaceRequestTest.java
+++ src/test/java/marquez/api/models/NamespaceRequestTest.java
@@ -14,21 +14,30 @@
 
 package marquez.api.models;
 
+import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static marquez.common.models.CommonModelGenerator.newDescription;
 import static marquez.common.models.CommonModelGenerator.newOwnerName;
 import static marquez.common.models.Description.NO_DESCRIPTION;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
 import marquez.UnitTests;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(UnitTests.class)
 public class NamespaceRequestTest {
+  private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
   private static final String OWNER_NAME_VALUE = newOwnerName().getValue();
   private static final String DESCRIPTION_VALUE = newDescription().getValue();
   private static final String NO_DESCRIPTION_VALUE = NO_DESCRIPTION.getValue();
+
+  private static final NamespaceRequest REQUEST =
+      new NamespaceRequest("analytics", "Contains datasets such as room bookings for each office.");
+  private static final NamespaceRequest REQUEST_NO_DESCRIPTION =
+      new NamespaceRequest("analytics", null);
 
   @Test
   public void testNewRequest() {
@@ -41,6 +50,39 @@ public class NamespaceRequestTest {
   public void testNewRequest_noDescription() {
     final NamespaceRequest expected = new NamespaceRequest(OWNER_NAME_VALUE, NO_DESCRIPTION_VALUE);
     final NamespaceRequest actual = new NamespaceRequest(OWNER_NAME_VALUE, NO_DESCRIPTION_VALUE);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void testNewRequest_fromJson() throws Exception {
+    final NamespaceRequest actual =
+        MAPPER.readValue(fixture("fixtures/namespace.json"), NamespaceRequest.class);
+    assertThat(actual).isEqualTo(REQUEST);
+  }
+
+  @Test
+  public void testNewRequest_toJson() throws Exception {
+    final String expected =
+        MAPPER.writeValueAsString(
+            MAPPER.readValue(fixture("fixtures/namespace.json"), NamespaceRequest.class));
+    final String actual = MAPPER.writeValueAsString(REQUEST);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void testNewRequest_fromJson_noDescription() throws Exception {
+    final NamespaceRequest actual =
+        MAPPER.readValue(fixture("fixtures/namespace_no_description.json"), NamespaceRequest.class);
+    assertThat(actual).isEqualTo(REQUEST_NO_DESCRIPTION);
+  }
+
+  @Test
+  public void testNewRequest_toJson_noDescription() throws Exception {
+    final String expected =
+        MAPPER.writeValueAsString(
+            MAPPER.readValue(
+                fixture("fixtures/namespace_no_description.json"), NamespaceRequest.class));
+    final String actual = MAPPER.writeValueAsString(REQUEST_NO_DESCRIPTION);
     assertThat(actual).isEqualTo(expected);
   }
 }
