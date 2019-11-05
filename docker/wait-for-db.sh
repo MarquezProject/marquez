1a41ed435e2927522aee7aba044fdc30e56ba8e6
wslulciuc <willy.lulciuc@wework.com>
2019-11-04 22:44:24 -0800

    Fix logic in until loop

--- docker/wait-for-db.sh
+++ docker/wait-for-db.sh
@@ -20,12 +20,12 @@ host="${1}"
 port="${2}"
 timeout="${3:-10}"
 
-until [ PGPASSWORD="${POSTGRES_PASSWORD}" psql \
+until PGPASSWORD="${POSTGRES_PASSWORD}" psql \
         --host="${host}" \
         --port="${port}" \
         --username "${POSTGRES_USER}" \
         --dbname "${POSTGRES_DB}" \
-        --command '\q' > /dev/null 2>&1 ]; do
+        --command '\q' > /dev/null 2>&1; do
   echo "Waiting for postgres to become available..."
   sleep 1
   timeout=$(( timeout-1 ))
