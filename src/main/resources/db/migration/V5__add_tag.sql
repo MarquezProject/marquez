446048267586ffaa14da201835067b3a11df89c2
Rukun Fan <rukun.fan@wework.com>
2019-12-03 13:41:28 -0800

    adding schema required by tagging feature (#661)
    * add tag schema
    
    * add tagging endpoint
    
    * Revert "add tagging endpoint"
    
    This reverts commit 2cb0cf847cc5f2c9f016feb68f491c3a66d6a449.
    
    * fix schema comments
    
    * fix formating issue

---
+++ src/main/resources/db/migration/V5__add_tag.sql
@@ -0,0 +1,19 @@
+CREATE TABLE tags (
+  uuid        UUID PRIMARY KEY,
+  created_at  TIMESTAMP NOT NULL,
+  updated_at  TIMESTAMP NOT NULL,
+  name        VARCHAR(255) NOT NULL UNIQUE,
+  description TEXT
+);
+CREATE TABLE dataset_tag_mapping (
+  dataset_uuid UUID REFERENCES datasets(uuid),
+  tag_uuid     UUID REFERENCES tags(uuid),
+  tagged_at    TIMESTAMP NOT NULL,
+  PRIMARY KEY (tag_uuid, dataset_uuid)
+);
+CREATE TABLE dataset_fields_tag_mapping (
+  dataset_field_uuid UUID REFERENCES dataset_fields(uuid),
+  tag_uuid           UUID REFERENCES tags(uuid),
+  tagged_at          TIMESTAMP NOT NULL,
+  PRIMARY KEY (tag_uuid, dataset_field_uuid)
+);
