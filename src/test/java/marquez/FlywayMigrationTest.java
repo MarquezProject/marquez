d9cb2f070c9dcf23fd970f4566069ecd6fa4a4eb
Rodrigo Araya <rodrigo.araya@gmail.com>
2019-12-07 18:20:36 -0800

    fix codestyle

--- src/test/java/marquez/FlywayMigrationTest.java
+++ src/test/java/marquez/FlywayMigrationTest.java
@@ -22,39 +22,40 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-
 @Category(IntegrationTests.class)
 public class FlywayMigrationTest {
-    private static final String CONFIG_FILE = "config-flyway-initSql.test.yml";
-    private static final String CONFIG_FILE_PATH = ResourceHelpers.resourceFilePath(CONFIG_FILE);
+  private static final String CONFIG_FILE = "config-flyway-initSql.test.yml";
+  private static final String CONFIG_FILE_PATH = ResourceHelpers.resourceFilePath(CONFIG_FILE);
 
-    private static final MarquezDb DB =
-        MarquezDb.create();
+  private static final MarquezDb DB = MarquezDb.create();
 
-    static {
-        DB.withInitScript("db-init.sql").start();
-    }
+  static {
+    DB.withInitScript("db-init.sql").start();
+  }
 
-    @ClassRule
-    public static final JdbiRule dbRule =
-        JdbiRule.externalPostgres(
-            DB.getHost(), DB.getPort(), DB.getUsername(), DB.getPassword(), DB.getDatabaseName());
+  @ClassRule
+  public static final JdbiRule dbRule =
+      JdbiRule.externalPostgres(
+          DB.getHost(), DB.getPort(), DB.getUsername(), DB.getPassword(), DB.getDatabaseName());
 
-    @ClassRule
-    public static final DropwizardAppRule<MarquezConfig> APP =
-        new DropwizardAppRule<>(
-            MarquezApp.class,
-            CONFIG_FILE_PATH,
-            ConfigOverride.config("db.url", DB.getJdbcUrl()),
-            ConfigOverride.config("db.user", DB.getUsername()),
-            ConfigOverride.config("db.password", DB.getPassword()));
+  @ClassRule
+  public static final DropwizardAppRule<MarquezConfig> APP =
+      new DropwizardAppRule<>(
+          MarquezApp.class,
+          CONFIG_FILE_PATH,
+          ConfigOverride.config("db.url", DB.getJdbcUrl()),
+          ConfigOverride.config("db.user", DB.getUsername()),
+          ConfigOverride.config("db.password", DB.getPassword()));
 
-    @Test
-    public void testRole() {
-        int numMarquezTables =
-            dbRule.getHandle()
-                .createQuery("SELECT * FROM pg_tables WHERE tableowner = 'marquez';")
-                .map(x -> x).list().size();
-        assert numMarquezTables > 0;
-    }
+  @Test
+  public void testRole() {
+    int numMarquezTables =
+        dbRule
+            .getHandle()
+            .createQuery("SELECT * FROM pg_tables WHERE tableowner = 'marquez';")
+            .map(x -> x)
+            .list()
+            .size();
+    assert numMarquezTables > 0;
+  }
 }
