9e5227d63bebce4e40c88f9ab74b5d5c75784c63
Grant Foster <grantdfoster@gmail.com>
2020-04-01 15:05:36 -0400

    feature/routing (#78)
    updates routing in setupProxy config to serve /datasets and /jobs endpoints

--- setupProxy.js
+++ setupProxy.js
@@ -1,14 +1,15 @@
 const proxy = require('http-proxy-middleware')
 const express = require('express')
 
-var apiOptions = {
+const apiOptions = {
   target: `http://${process.env.MARQUEZ_HOST}:${process.env.MARQUEZ_PORT}/`
 }
+const app = express()
+const path = __dirname + '/dist'
 
-var app = express()
-
-const path = __dirname + '/dist/'
-app.use(express.static(path))
+app.use('/', express.static(path))
+app.use('/datasets/:name', express.static(path))
+app.use('/jobs/:name', express.static(path))
 app.use(proxy('/api/v1', apiOptions))
 
 app.listen(3000, function() {
