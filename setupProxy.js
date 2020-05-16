bdbbcf78415a3c25b46cc3a63aabd729cbefefd6
wslulciuc <willy@datakin.com>
2020-05-16 11:18:50 -0700

    Add /healthcheck
    Signed-off-by: wslulciuc <willy@datakin.com>

--- setupProxy.js
+++ setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require('http-proxy-middleware')
 const express = require('express')
+const router = express.Router()
 
 const apiOptions = {
   target: `http://${process.env.MARQUEZ_HOST}:${process.env.MARQUEZ_PORT}/`
@@ -11,6 +12,12 @@ app.use('/', express.static(path))
 app.use('/datasets/:name', express.static(path))
 app.use('/jobs/:name', express.static(path))
 app.use(proxy('/api/v1', apiOptions))
+
+router.get('/healthcheck', function (req, res) {
+  res.send('OK')
+})
+
+app.use(router)
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!')
