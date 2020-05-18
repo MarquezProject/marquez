a7320190fc024f40e379a4244a130454d2a3a443
wslulciuc <willy@datakin.com>
2020-05-18 13:24:51 -0700

    Simplify namespace meta
    Signed-off-by: wslulciuc <willy@datakin.com>

--- src/main/java/marquez/client/models/Namespace.java
+++ src/main/java/marquez/client/models/Namespace.java
@@ -14,30 +14,32 @@
 
 package marquez.client.models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.type.TypeReference;
 import java.time.Instant;
-import java.util.Optional;
 import javax.annotation.Nullable;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.ToString;
 import marquez.client.Utils;
 
-@AllArgsConstructor(onConstructor = @__(@JsonCreator))
-@EqualsAndHashCode
-@ToString
-public final class Namespace {
-  @Getter @NonNull private final String name;
-  @Getter @NonNull private final Instant createdAt;
-  @Getter @NonNull private final Instant updatedAt;
-  @Getter @NonNull private final String ownerName;
-  @Nullable private final String description;
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public final class Namespace extends NamespaceMeta {
+  @Getter private final String name;
+  @Getter private final Instant createdAt;
+  @Getter private final Instant updatedAt;
 
-  public Optional<String> getDescription() {
-    return Optional.ofNullable(description);
+  public Namespace(
+      @NonNull final String name,
+      @NonNull final Instant createdAt,
+      @NonNull final Instant updatedAt,
+      final String ownerName,
+      @Nullable final String description) {
+    super(ownerName, description);
+    this.name = name;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 
   public static Namespace fromJson(@NonNull final String json) {
--- src/main/java/marquez/client/models/NamespaceMeta.java
+++ src/main/java/marquez/client/models/NamespaceMeta.java
@@ -16,17 +16,22 @@ package marquez.client.models;
 
 import java.util.Optional;
 import javax.annotation.Nullable;
-import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Value;
+import lombok.ToString;
 import marquez.client.Utils;
 
-@Value
-@Builder
+@EqualsAndHashCode
+@ToString
 public class NamespaceMeta {
-  @Getter @NonNull String ownerName;
-  @Nullable String description;
+  @Getter private final String ownerName;
+  @Nullable private final String description;
+
+  public NamespaceMeta(@NonNull final String ownerName, @Nullable final String description) {
+    this.ownerName = ownerName;
+    this.description = description;
+  }
 
   public Optional<String> getDescription() {
     return Optional.ofNullable(description);
@@ -34,5 +39,28 @@ public class NamespaceMeta {
 
   public String toJson() {
     return Utils.toJson(this);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+    private String ownerName;
+    @Nullable private String description;
+
+    public Builder ownerName(@NonNull String ownerName) {
+      this.ownerName = ownerName;
+      return this;
+    }
+
+    public Builder description(@Nullable String description) {
+      this.description = description;
+      return this;
+    }
+
+    public NamespaceMeta build() {
+      return new NamespaceMeta(ownerName, description);
+    }
   }
 }
