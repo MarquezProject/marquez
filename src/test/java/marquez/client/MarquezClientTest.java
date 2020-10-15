c19dfcc6c0b0800f57efacf9d5d802c1ac8b6b21
wslulciuc <willy@datakin.com>
2020-10-14 17:16:40 -0700

    Add test for optional run ID in job meta
    Signed-off-by: wslulciuc <willy@datakin.com>

--- src/test/java/marquez/client/MarquezClientTest.java
+++ src/test/java/marquez/client/MarquezClientTest.java
@@ -531,6 +531,28 @@ public class MarquezClientTest {
   }
 
   @Test
+  public void testCreateJobWithRunId() throws Exception {
+    final URL url = buildUrlFor("/namespaces/%s/jobs/%s", NAMESPACE_NAME, JOB_NAME);
+
+    final JobMeta meta =
+            JobMeta.builder()
+                    .type(JOB_TYPE)
+                    .inputs(INPUTS)
+                    .outputs(OUTPUTS)
+                    .location(LOCATION)
+                    .description(JOB_DESCRIPTION)
+                    .context(JOB_CONTEXT)
+                    .runId(newRunId())
+                    .build();
+    final String metaAsJson = JsonGenerator.newJsonFor(meta);
+    final String jobAsJson = JsonGenerator.newJsonFor(JOB);
+    when(http.put(url, metaAsJson)).thenReturn(jobAsJson);
+
+    final Job job = client.createJob(NAMESPACE_NAME, JOB_NAME, meta);
+    assertThat(job).isEqualTo(JOB);
+  }
+
+  @Test
   public void testGetJob() throws Exception {
     final URL url = buildUrlFor("/namespaces/%s/jobs/%s", NAMESPACE_NAME, JOB_NAME);
 
