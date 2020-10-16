99c8790f1106f70e987fb053ef0245d4734256a3
wslulciuc <willy@datakin.com>
2020-10-16 13:07:53 -0700

    continued: Set source and target compatibility to java 11
    Signed-off-by: wslulciuc <willy@datakin.com>

--- .circleci/get-jdk11.sh
+++ .circleci/get-jdk11.sh
@@ -17,5 +17,8 @@
 set -e
 
 sudo apt-get update && sudo apt-get install openjdk-11-jdk
+sudo update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac
+java -version
 
 echo "DONE!"
