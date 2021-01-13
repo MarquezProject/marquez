7f9ade44da2352636f5dfb008e031ea869b1c2d1
henneberger <git@danielhenneberger.com>
2021-01-12 16:21:13 -0800

    Update spark instrumentation to use OpenLineage
    Signed-off-by: henneberger <git@danielhenneberger.com>

---
+++ experimental/integrations/prototype-spark/marquez-spark-agent/src/main/java/marquez/spark/agent/client/OpenLineageClient.java
@@ -0,0 +1,182 @@
+package marquez.spark.agent.client;
+
+import static org.apache.hc.core5.http.HttpHeaders.AUTHORIZATION;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ok2c.hc5.json.http.JsonRequestProducers;
+import com.ok2c.hc5.json.http.JsonResponseConsumers;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.async.methods.BasicHttpRequests;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.cookie.StandardCookieSpec;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.Message;
+import org.apache.hc.core5.http.ssl.TLS;
+import org.apache.hc.core5.reactor.IOReactorConfig;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.util.Timeout;
+
+@Slf4j
+public class OpenLineageClient {
+  private final CloseableHttpAsyncClient http;
+  private final String apiKey;
+  @Getter protected static final ObjectMapper objectMapper = createMapper();
+
+  public OpenLineageClient(CloseableHttpAsyncClient http, String apiKey) {
+    this.http = http;
+    this.http.start();
+    this.apiKey = apiKey;
+  }
+
+  public static OpenLineageClient create(final String apiKey) {
+    final CloseableHttpAsyncClient http =
+        HttpAsyncClients.customHttp2()
+            .setTlsStrategy(
+                ClientTlsStrategyBuilder.create()
+                    .setSslContext(SSLContexts.createSystemDefault())
+                    .setTlsVersions(TLS.V_1_3, TLS.V_1_2)
+                    .build())
+            .setIOReactorConfig(IOReactorConfig.custom().setSoTimeout(Timeout.ofSeconds(5)).build())
+            .setDefaultRequestConfig(
+                RequestConfig.custom()
+                    .setConnectTimeout(Timeout.ofSeconds(5))
+                    .setResponseTimeout(Timeout.ofSeconds(5))
+                    .setCookieSpec(StandardCookieSpec.STRICT)
+                    .build())
+            .setUserAgent(getUserAgent())
+            .build();
+    return new OpenLineageClient(http, apiKey);
+  }
+
+  public <T> void post(URI uri, Object obj) throws MarquezHttpException {
+    post(uri, obj, Void.class);
+  }
+
+  public <T> T post(URI uri, Object obj, Class<T> clazz) throws MarquezHttpException {
+    return executeSync(
+        BasicHttpRequests.post(uri),
+        obj,
+        new TypeReference<T>() {
+          @Override
+          public Type getType() {
+            return clazz;
+          }
+        });
+  }
+
+  public <T> T executeSync(HttpRequest request, Object obj, TypeReference<T> ref)
+      throws MarquezHttpException {
+    CompletableFuture<ResponseMessage<T>> future = executeAsync(request, obj, ref);
+    try {
+      ResponseMessage<T> message = future.get();
+      if (message.completedSuccessfully()) {
+        return message.getBody();
+      }
+      throw new MarquezHttpException(message.getError());
+    } catch (ExecutionException | InterruptedException ignored) {
+      throw new MarquezHttpException();
+    }
+  }
+
+  public CompletableFuture<ResponseMessage<Void>> postAsync(URI uri, Object obj) {
+    return postAsync(uri, obj, Void.class);
+  }
+
+  public <T> CompletableFuture<ResponseMessage<T>> postAsync(URI uri, Object obj, Class<T> clazz) {
+    return executeAsync(
+        BasicHttpRequests.post(uri),
+        obj,
+        new TypeReference<T>() {
+          @Override
+          public Type getType() {
+            return clazz;
+          }
+        });
+  }
+
+  protected <T> CompletableFuture<ResponseMessage<T>> executeAsync(
+      HttpRequest request, Object obj, TypeReference<T> ref) {
+    addAuthToReqIfKeyPresent(request);
+
+    Future<Message<HttpResponse, JsonNode>> future =
+        http.execute(
+            JsonRequestProducers.create(request, obj, objectMapper),
+            JsonResponseConsumers.create(objectMapper.getFactory()),
+            null);
+
+    return CompletableFuture.supplyAsync(
+        () -> {
+          try {
+            Message<HttpResponse, JsonNode> message = future.get();
+            return createMessage(message, ref);
+          } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        });
+  }
+
+  private <T> ResponseMessage<T> createMessage(
+      Message<HttpResponse, JsonNode> message, TypeReference<T> ref) {
+    if (!completedSuccessfully(message)) {
+      return new ResponseMessage<>(
+          message.getHead().getCode(),
+          null,
+          objectMapper.convertValue(message.getBody(), HttpError.class));
+    }
+
+    return new ResponseMessage<>(
+        message.getHead().getCode(), objectMapper.convertValue(message.getBody(), ref), null);
+  }
+
+  private boolean completedSuccessfully(Message<HttpResponse, JsonNode> message) {
+    final int code = message.getHead().getCode();
+    if (code >= 400 && code < 600) { // non-2xx
+      return false;
+    }
+    return true;
+  }
+
+  public static ObjectMapper createMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.setSerializationInclusion(Include.NON_NULL);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+    return mapper;
+  }
+
+  private void addAuthToReqIfKeyPresent(final HttpRequest request) {
+    if (apiKey != null) {
+      request.addHeader(AUTHORIZATION, "Bearer " + apiKey);
+    }
+  }
+
+  protected static String getUserAgent() {
+    return "openlineage-java" + "/1.0";
+  }
+
+  public void close() {
+    try {
+      http.close();
+    } catch (IOException e) {
+      e.printStackTrace(System.out);
+    }
+  }
+}
--- experimental/integrations/prototype-spark/marquez-spark-agent/src/main/java/marquez/spark/agent/lifecycle/Rdds.java
+++ experimental/integrations/prototype-spark/marquez-spark-agent/src/main/java/marquez/spark/agent/lifecycle/Rdds.java
@@ -4,13 +4,11 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import org.apache.spark.Dependency;
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.scheduler.SparkListenerJobStart;
 import org.apache.spark.scheduler.StageInfo;
 import org.apache.spark.storage.RDDInfo;
-
 import scala.collection.JavaConversions;
 
 public class Rdds {
@@ -30,7 +28,12 @@ public class Rdds {
     sb.append("start: ").append(jobStart.properties().toString()).append("\n");
     List<StageInfo> stageInfos = JavaConversions.seqAsJavaList(jobStart.stageInfos());
     for (StageInfo stageInfo : stageInfos) {
-      sb.append("  ").append("stageInfo: ").append(stageInfo.stageId()).append(" ").append(stageInfo.name()).append("\n");
+      sb.append("  ")
+          .append("stageInfo: ")
+          .append(stageInfo.stageId())
+          .append(" ")
+          .append(stageInfo.name())
+          .append("\n");
       List<RDDInfo> rddInfos = JavaConversions.seqAsJavaList(stageInfo.rddInfos());
       for (RDDInfo rddInfo : rddInfos) {
         sb.append("    ").append("rddInfo: ").append(rddInfo).append("\n");
