24169b667c5ab65d5d3f014de93b84292bb36399
henneberger <git@danielhenneberger.com>
2021-02-03 09:34:09 -0800

    remove agent args from string to prevent leaking api string
    Signed-off-by: henneberger <git@danielhenneberger.com>

--- experimental/integrations/marquez-spark-agent/src/main/java/marquez/spark/agent/MarquezAgent.java
+++ experimental/integrations/marquez-spark-agent/src/main/java/marquez/spark/agent/MarquezAgent.java
@@ -23,7 +23,7 @@ public class MarquezAgent {
 
   public static void premain(
       String agentArgs, Instrumentation inst, ContextFactory contextFactory) {
-    log.info("MarquezAgent.premain " + agentArgs);
+    log.info("MarquezAgent.premain ");
     SparkListener.init(contextFactory);
     instrument(inst);
     addShutDownHook();
