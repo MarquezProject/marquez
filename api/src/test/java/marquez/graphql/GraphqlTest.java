01bbe45d51b55b940d65b05d98b287ebf6e1c7af
Michael Collado <mike@datakin.com>
2021-03-09 16:28:55 -0800

    Removed wildcard imports to match spotless requirements
    Signed-off-by: Michael Collado <mike@datakin.com>

--- api/src/test/java/marquez/graphql/GraphqlTest.java
+++ api/src/test/java/marquez/graphql/GraphqlTest.java
@@ -1,6 +1,7 @@
 package marquez.graphql;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import graphql.ExecutionResult;
