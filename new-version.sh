8c84f39c73d01a2e556e0ce14636cd5086ed63f0
wslulciuc <willy@datakin.com>
2021-04-09 13:57:41 -0700

    Add support for next version and tagging
    Signed-off-by: wslulciuc <willy@datakin.com>

--- new-version.sh
+++ new-version.sh
@@ -20,11 +20,13 @@
 set -e
 
 usage() {
-  echo "usage: ./$(basename -- ${0}) VERSION"
+  echo "usage: ./$(basename -- ${0}) NEW_VERSION NEXT_VERSION"
   exit 1
 }
 
-readonly SEMVER_REGEX="^[0-9]+(\.[0-9]+){2}(-rc\.[0-9]+)?$" # X.Y.Z-rc.*
+readonly SEMVER_REGEX="^[0-9]+(\.[0-9]+){2}(-rc\.[0-9]+)?(-SNAPSHOT)?$" # X.Y.Z
+                                                                        # X.Y.Z-rc.*
+                                                                        # X.Y.Z-SNAPSHOT
 
 # Change working directory to project root
 project_root=$(git rev-parse --show-toplevel)
@@ -32,12 +34,12 @@ cd "${project_root}"
 
 # Verify bump2version is installed
 if [[ ! $(type -P bump2version) ]]; then
- echo "bump2version not installed! Please see https://github.com/c4urself/bump2version#installation"
- exit 1
+  echo "bump2version not installed! Please see https://github.com/c4urself/bump2version#installation"
+  exit 1
 fi
 
 branch=$(git symbolic-ref --short HEAD)
-if [[ "${branch}" != "main" ]]; then
+if [[ "${branch}" == "main" ]]; then
   echo "Error: You may only release on 'main'!"
   exit 1;
 fi
@@ -47,20 +49,40 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 NEW_VERSION="${1}"
+NEXT_VERSION="${2}"
 
-# Ensure valid version
-if [[ ! "${NEW_VERSION}" =~ ${SEMVER_REGEX} ]]; then
-  echo "Error: Version '${NEW_VERSION}' must match '${SEMVER_REGEX}'"
-  exit 1
-fi
-
-# Bump python module versions
-PYTHON_MODULES=(clients/python/ integrations/airflow/)
-for PYTHON_MODULE in "${PYTHON_MODULES[@]}"; do
- (cd "${PYTHON_MODULE}" && bump2version manual --new-version "${NEW_VERSION}")
+# Ensure valid versions
+VERSIONS=($NEW_VERSION $NEXT_VERSION)
+for VERSION in "${VERSIONS[@]}"; do
+  if [[ ! "${VERSION}" =~ ${SEMVER_REGEX} ]]; then
+    echo "Error: Version '${VERSION}' must match '${SEMVER_REGEX}'"
+    exit 1
+  fi
 done
 
-# Bump java module versions
+# (1) Bump python module versions
+PYTHON_MODULES=(clients/python/ integrations/airflow/)
+for PYTHON_MODULE in "${PYTHON_MODULES[@]}"; do
+  (cd "${PYTHON_MODULE}" && bump2version manual --new-version "${NEW_VERSION}")
+done
+
+# (2) Bump java module versions
 sed -i "" "s/version=.*/version=${NEW_VERSION}/g" gradle.properties
+
+# (3) Prepare release commit
+git commit -am "Prepare for release ${NEW_VERSION}"
+
+# (4) Prepare release tag
+git fetch --all --tags > /dev/null 2>&1
+git tag -a "${NEW_VERSION}" -m "marquez ${NEW_VERSION}"
+
+# (5) Prepare next development version
+sed -i "" "s/version=.*/version=${NEXT_VERSION}/g" gradle.properties
+
+# (6) Prepare next development version commit
+git commit -am "Prepare next development version"
+
+# (7) Push commits and tag
+git push origin main && git push origin "${NEW_VERSION}"
 
 echo "DONE!"
