05f3ad7cd1e680700adc988ad3297fa0ec3136a2
Willy Lulciuc <willy@datakin.com>
2021-04-09 16:13:13 -0700

    Eenable release via CI (#1200)
    * Add release cmds for modules in new-version.sh
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Don't allow dirty releases
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Remove versions in gradle.properties from java module
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Add support for next version and tagging
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Remove version bump steps from CI
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Remove plugin researchgate.gradle-release
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Remove plugin researchgate.gradle-release
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Set JDK8_HOME in java release CI job
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Fix regex to accept rc or snapshot
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Add requirements section for script
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Use RELEASE_VERSION
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Catch any errors when fetching tags upstream
    
    Signed-off-by: wslulciuc <willy@datakin.com>

--- new-version.sh
+++ new-version.sh
@@ -15,68 +15,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Usage: $ ./new-version.sh <VERSION>
+# Requirements:
+#   * You're on the 'main' branch
+#   * You've installed 'bump2version'
+#
+# Usage: $ ./new-version.sh <RELEASE_VERSION> <NEXT_VERSION>
 
 set -e
 
-# PYTHON
+usage() {
+  echo "usage: ./$(basename -- ${0}) <RELEASE_VERSION> <NEXT_VERSION>"
+  exit 1
+}
+
+readonly SEMVER_REGEX="^[0-9]+(\.[0-9]+){2}((-rc\.[0-9]+)?|(-SNAPSHOT)?)$" # X.Y.Z
+                                                                           # X.Y.Z-rc.*
+                                                                           # X.Y.Z-SNAPSHOT
+
+# Change working directory to project root
+project_root=$(git rev-parse --show-toplevel)
+cd "${project_root}"
 
 # Verify bump2version is installed
 if [[ ! $(type -P bump2version) ]]; then
- echo "bump2version not installed! Please see https://github.com/c4urself/bump2version#installation"
- exit 1
-fi
-
-if [[ -z "${type}" ]]; then
-  # Default to 'patch'
-  type="patch"
-fi
-
-
-# get new version tag from CI
-if [[ -n "$1" ]]; then
-  # Default to current tag
-  NEW_VERSION=$1
-elif [[ -n ${CIRCLE_TAG} ]]; then
-  NEW_VERSION=${CIRCLE_TAG}
-else
-  echo "Version not passed to script. Use ./new-version.sh VERSION"
+  echo "bump2version not installed! Please see https://github.com/c4urself/bump2version#installation"
   exit 1
 fi
 
-# Bump marquez_client version
-VERSION=$(python ./clients/python/setup.py --version)
-bump2version \
-  --config-file ./clients/python/setup.cfg \
-  --current-version "${VERSION}" \
-  --new-version "${NEW_VERSION}" \
-  --no-commit \
-  --no-tag \
-  --allow-dirty \
-  "${type}" ./clients/python/marquez_client/version.py
+branch=$(git symbolic-ref --short HEAD)
+if [[ "${branch}" != "main" ]]; then
+  echo "Error: You may only release on 'main'!"
+  exit 1;
+fi
 
+if [[ $# -eq 0 ]] ; then
+  usage
+fi
 
-# Bump marquez_airflow version
-VERSION=$(python ./integrations/airflow/setup.py --version)
-bump2version \
-  --config-file ./integrations/airflow/setup.cfg \
-  --current-version "${VERSION}" \
-  --new-version "${NEW_VERSION}" \
-  --no-commit \
-  --no-tag \
-  --allow-dirty \
-  "${type}" ./integrations/airflow/marquez_airflow/version.py
+RELEASE_VERSION="${1}"
+NEXT_VERSION="${2}"
 
-
-GRADLE_PROPS=(
-  gradle.properties
-  api/gradle.properties
-  integrations/spark/gradle.properties
-)
-
-for FILE in "${GRADLE_PROPS[@]}"
-do
-  sed -i 's/\(^version=.*$\)/version='"${NEW_VERSION}"'/g' "$FILE"
+# Ensure valid versions
+VERSIONS=($RELEASE_VERSION $NEXT_VERSION)
+for VERSION in "${VERSIONS[@]}"; do
+  if [[ ! "${VERSION}" =~ ${SEMVER_REGEX} ]]; then
+    echo "Error: Version '${VERSION}' must match '${SEMVER_REGEX}'"
+    exit 1
+  fi
 done
+
+# (1) Bump python module versions
+PYTHON_MODULES=(clients/python/ integrations/airflow/)
+for PYTHON_MODULE in "${PYTHON_MODULES[@]}"; do
+  (cd "${PYTHON_MODULE}" && bump2version manual --new-version "${RELEASE_VERSION}")
+done
+
+# (2) Bump java module versions
+sed -i "" "s/version=.*/version=${RELEASE_VERSION}/g" gradle.properties
+
+# (3) Prepare release commit
+git commit -am "Prepare for release ${RELEASE_VERSION}"
+
+# (4) Prepare release tag
+git fetch --all --tags
+git tag -a "${RELEASE_VERSION}" -m "marquez ${RELEASE_VERSION}"
+
+# (5) Prepare next development version
+sed -i "" "s/version=.*/version=${NEXT_VERSION}/g" gradle.properties
+
+# (6) Prepare next development version commit
+git commit -am "Prepare next development version"
+
+# (7) Push commits and tag
+git push origin main && git push origin "${RELEASE_VERSION}"
 
 echo "DONE!"
