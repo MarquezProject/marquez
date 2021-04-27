955843856e683568614c2cac57db5f12619a9590
Michael Collado <mike@datakin.com>
2021-04-27 14:12:05 -0700

    Clarified STATE_TRANSITIONS doc with definitions, code example, and updated diagrams
    Signed-off-by: Michael Collado <mike@datakin.com>

--- generate_dot_images.sh
+++ generate_dot_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for l in `ls docs/assets/dot`; do
-  docker run --rm -v ${PWD}/docs/assets/dot:/graphs -i nshine/dot dot -Tpng -O /graphs/$l
+for l in `ls docs/assets/dot/*.dot`; do
+  name=$(basename $l)
+  docker run -v ${PWD}/docs/assets/dot:/graphs -i fgrehm/graphviz dot -Tpng -O /graphs/$name
 done
