f1cc01a21dc3f08d91ba545fcfeda54b9c2760b9
olek <oleksandr.dvornik@gmail.com>
2021-06-21 13:37:54 +0200

    spotlessApply
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>

--- integrations/spark/src/main/java/marquez/spark/agent/facets/UnknownEntryFacet.java
+++ integrations/spark/src/main/java/marquez/spark/agent/facets/UnknownEntryFacet.java
@@ -1,8 +1,8 @@
 package marquez.spark.agent.facets;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import java.util.List;
 import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import marquez.spark.agent.client.LineageEvent;
@@ -23,8 +23,7 @@ public class UnknownEntryFacet extends LineageEvent.BaseFacet {
 
   @Value
   public static class FacetEntry {
-    @JsonRawValue
-    String description;
+    @JsonRawValue String description;
     List<AttributeField> inputAttributes;
     List<AttributeField> outputAttributes;
   }
