38ead7b1bbfd25e71cf76be17e04a6a75beb0c47
olek <oleksandr.dvornik@gmail.com>
2021-06-21 13:33:48 +0200

    fix LibraryTest, adding additional facet to test results
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>

--- integrations/spark/src/main/java/marquez/spark/agent/facets/UnknownEntryFacet.java
+++ integrations/spark/src/main/java/marquez/spark/agent/facets/UnknownEntryFacet.java
@@ -2,6 +2,7 @@ package marquez.spark.agent.facets;
 
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import marquez.spark.agent.client.LineageEvent;
@@ -22,6 +23,7 @@ public class UnknownEntryFacet extends LineageEvent.BaseFacet {
 
   @Value
   public static class FacetEntry {
+    @JsonRawValue
     String description;
     List<AttributeField> inputAttributes;
     List<AttributeField> outputAttributes;
