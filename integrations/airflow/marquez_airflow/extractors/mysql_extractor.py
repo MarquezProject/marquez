76b68782c24daaf6551053713107a214b57cfed0
Nico Ritschel <nico@atm.com>
2021-06-22 20:32:27 -0700

    Change MySql Airflow extractor to safely load optional dep
    Signed-off-by: Nico Ritschel <nico@atm.com>

--- integrations/airflow/marquez_airflow/extractors/mysql_extractor.py
+++ integrations/airflow/marquez_airflow/extractors/mysql_extractor.py
@@ -1,11 +1,27 @@
+import logging
+
 from airflow.hooks.mysql_hook import MySqlHook
-from airflow.operators.mysql_operator import MySqlOperator
 
 from marquez_airflow.extractors.postgres_extractor import PostgresExtractor
 
+log = logging.getLogger(__file__)
+
+
+# MySql is optional dependency.
+def try_load_operator():
+    try:
+        from airflow.operators.mysql_operator import MySqlOperator
+        return MySqlOperator
+    except Exception:
+        log.warn('Did not find mysql_operator library or failed to import it')
+        return None
+
+
+Operator = try_load_operator()
+
 
 class MySqlExtractor(PostgresExtractor):
-    operator_class = MySqlOperator
+    operator_class = Operator
     source_type = 'MYSQL'
 
     def __init__(self, operator):
