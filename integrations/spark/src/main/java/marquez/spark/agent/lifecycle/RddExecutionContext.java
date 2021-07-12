fab49f663d40e4f301f7508dc021365775cd4467
Michael Collado <40346148+collado-mike@users.noreply.github.com>
2021-07-12 13:42:52 -0700

    Fix Spark openlineage.* configuration parameters and add integration test container execution (#1493)
    * Fixed Spark openlineage parameters read from SparkConf
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Added initial Spark container integration test
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Added RDD integration test and added reflection for analyzing RDD outputs from JobConf
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Improved logging and added log4j default properties
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Changed Spark integration test to use @Testcontainers annotations
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Add spark integration tests to circleci build
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Changed circle config to use host machine directly
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Fixed spark shadowJar task to obfuscate more packages
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Fix documentation to reference correct params
    
    Signed-off-by: Michael Collado <mike@datakin.com>

--- integrations/spark/src/main/java/marquez/spark/agent/lifecycle/RddExecutionContext.java
+++ integrations/spark/src/main/java/marquez/spark/agent/lifecycle/RddExecutionContext.java
@@ -3,12 +3,15 @@ package marquez.spark.agent.lifecycle;
 import static scala.collection.JavaConversions.asJavaCollection;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -28,14 +31,20 @@ import marquez.spark.agent.client.LineageEvent.RunFacet;
 import marquez.spark.agent.client.OpenLineageClient;
 import marquez.spark.agent.facets.ErrorFacet;
 import marquez.spark.agent.lifecycle.plan.PlanUtils;
+import marquez.spark.agent.lifecycle.plan.ScalaConversionUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.parquet.Strings;
 import org.apache.spark.Dependency;
 import org.apache.spark.SparkContext;
 import org.apache.spark.SparkContext$;
+import org.apache.spark.TaskContext;
+import org.apache.spark.internal.io.HadoopMapRedWriteConfigUtil;
 import org.apache.spark.rdd.HadoopRDD;
+import org.apache.spark.rdd.MapPartitionsRDD;
 import org.apache.spark.rdd.NewHadoopRDD;
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.scheduler.ActiveJob;
@@ -45,6 +54,10 @@ import org.apache.spark.scheduler.ResultStage;
 import org.apache.spark.scheduler.SparkListenerJobEnd;
 import org.apache.spark.scheduler.SparkListenerJobStart;
 import org.apache.spark.scheduler.Stage;
+import org.apache.spark.util.SerializableJobConf;
+import scala.Function2;
+import scala.collection.Iterator;
+import scala.collection.Seq;
 import scala.runtime.AbstractFunction0;
 
 @Slf4j
@@ -53,6 +66,7 @@ public class RddExecutionContext implements ExecutionContext {
   private final Optional<SparkContext> sparkContextOption;
   private List<URI> inputs;
   private List<URI> outputs;
+  private String jobSuffix;
 
   public RddExecutionContext(int jobId, MarquezContext marquezContext) {
     this.marquezContext = marquezContext;
@@ -72,9 +86,69 @@ public class RddExecutionContext implements ExecutionContext {
   @Override
   public void setActiveJob(ActiveJob activeJob) {
     RDD<?> finalRDD = activeJob.finalStage().rdd();
+    this.jobSuffix = nameRDD(finalRDD);
     Set<RDD<?>> rdds = Rdds.flattenRDDs(finalRDD);
     this.inputs = findInputs(rdds);
-    this.outputs = findOutputs(rdds);
+    Configuration jc = new JobConf();
+    if (activeJob.finalStage() instanceof ResultStage) {
+      Function2<TaskContext, Iterator<?>, ?> fn = ((ResultStage) activeJob.finalStage()).func();
+      try {
+        Field f = fn.getClass().getDeclaredField("config$1");
+        f.setAccessible(true);
+
+        HadoopMapRedWriteConfigUtil configUtil = (HadoopMapRedWriteConfigUtil) f.get(fn);
+        Field confField = HadoopMapRedWriteConfigUtil.class.getDeclaredField("conf");
+        confField.setAccessible(true);
+        SerializableJobConf conf = (SerializableJobConf) confField.get(configUtil);
+        jc = conf.value();
+      } catch (IllegalAccessException | NoSuchFieldException nfe) {
+        log.warn("Unable to access job conf from RDD", nfe);
+      }
+      log.info("Found job conf from RDD {}", jc);
+    } else {
+      jc = SparkListener.getConfigForRDD(finalRDD);
+    }
+    this.outputs = findOutputs(finalRDD, jc);
+  }
+
+  static String nameRDD(RDD<?> rdd) {
+    String rddName = (String) rdd.name();
+    if (rddName == null
+
+        // HadoopRDDs are always named for the path. Don't name the RDD for a file. Otherwise, the
+        // job name will end up differing each time we read a path with a date or other variable
+        // directory name
+        || (rdd instanceof HadoopRDD
+            && Arrays.stream(FileInputFormat.getInputPaths(((HadoopRDD) rdd).getJobConf()))
+                .anyMatch(p -> p.toString().contains(rdd.name())))
+        // If the map RDD is named the same as its dependent, just use map_partition
+        // This happens, e.g., when calling sparkContext.textFile(), as it creates a HadoopRDD, maps
+        // the value to a string, and sets the name of the mapped RDD to the path, which is already
+        // the name of the underlying HadoopRDD
+        || (rdd instanceof MapPartitionsRDD
+            && rdd.name().equals(((MapPartitionsRDD) rdd).prev().name()))) {
+      rddName =
+          rdd.getClass()
+              .getSimpleName()
+              .replaceAll("RDD\\d*$", "") // remove the trailing RDD from the class name
+              .replaceAll(CAMEL_TO_SNAKE_CASE, "_$1") // camel case to snake case
+              .toLowerCase(Locale.ROOT);
+    }
+    Seq<Dependency<?>> deps = (Seq<Dependency<?>>) rdd.dependencies();
+    List<Dependency<?>> dependencies = ScalaConversionUtils.fromSeq(deps);
+    if (dependencies.isEmpty()) {
+      return rddName;
+    }
+    List<String> dependencyNames = new ArrayList<>();
+    for (Dependency d : dependencies) {
+      dependencyNames.add(nameRDD(d.rdd()));
+    }
+    String dependencyName = Strings.join(dependencyNames, "_");
+    if (!dependencyName.startsWith(rddName)) {
+      return rddName + "_" + dependencyName;
+    } else {
+      return dependencyName;
+    }
   }
 
   @Override
@@ -84,12 +158,13 @@ public class RddExecutionContext implements ExecutionContext {
             .inputs(buildInputs(inputs))
             .outputs(buildOutputs(outputs))
             .run(buildRun(buildRunFacets(null)))
-            .job(buildJob())
+            .job(buildJob(jobStart.jobId()))
             .eventTime(toZonedTime(jobStart.time()))
             .eventType("START")
             .producer(OpenLineageClient.OPEN_LINEAGE_CLIENT_URI)
             .build();
 
+    log.debug("Posting event for start {}: {}", jobStart, event);
     marquezContext.emit(event);
   }
 
@@ -100,12 +175,13 @@ public class RddExecutionContext implements ExecutionContext {
             .inputs(buildInputs(inputs))
             .outputs(buildOutputs(outputs))
             .run(buildRun(buildRunFacets(buildJobErrorFacet(jobEnd.jobResult()))))
-            .job(buildJob())
+            .job(buildJob(jobEnd.jobId()))
             .eventTime(toZonedTime(jobEnd.time()))
             .eventType(getEventType(jobEnd.jobResult()))
             .producer(OpenLineageClient.OPEN_LINEAGE_CLIENT_URI)
             .build();
 
+    log.debug("Posting event for end {}: {}", jobEnd, event);
     marquezContext.emit(event);
   }
 
@@ -145,16 +221,15 @@ public class RddExecutionContext implements ExecutionContext {
     return null;
   }
 
-  protected LineageEvent.Job buildJob() {
-    String jobName =
-        sparkContextOption
-            .map(
-                ctx ->
-                    ctx.appName().replaceAll(CAMEL_TO_SNAKE_CASE, "_$1").toLowerCase(Locale.ROOT))
-            .orElse("unknown");
+  protected LineageEvent.Job buildJob(int jobId) {
+    String suffix = jobSuffix;
+    if (jobSuffix == null) {
+      suffix = String.valueOf(jobId);
+    }
+    String jobName = sparkContextOption.map(SparkContext::appName).orElse("unknown") + "." + suffix;
     return LineageEvent.Job.builder()
         .namespace(marquezContext.getJobNamespace())
-        .name(jobName)
+        .name(jobName.replaceAll(CAMEL_TO_SNAKE_CASE, "_$1").toLowerCase(Locale.ROOT))
         .build();
   }
 
@@ -171,15 +246,13 @@ public class RddExecutionContext implements ExecutionContext {
     return inputs.stream().map(this::buildDataset).collect(Collectors.toList());
   }
 
-  protected List<URI> findOutputs(Set<RDD<?>> rdds) {
-    List<URI> result = new ArrayList<>();
-    for (RDD<?> rdd : rdds) {
-      Path outputPath = getOutputPath(rdd);
-      if (outputPath != null) {
-        result.add(getDatasetUri(outputPath.toUri()));
-      }
+  protected List<URI> findOutputs(RDD<?> rdd, Configuration config) {
+    Path outputPath = getOutputPath(rdd, config);
+    log.info("Found output path {} from RDD {}", outputPath, rdd);
+    if (outputPath != null) {
+      return Collections.singletonList(getDatasetUri(outputPath.toUri()));
     }
-    return result;
+    return Collections.emptyList();
   }
 
   protected List<URI> findInputs(Set<RDD<?>> rdds) {
@@ -238,17 +311,22 @@ public class RddExecutionContext implements ExecutionContext {
     }
   }
 
-  protected static Path getOutputPath(RDD<?> rdd) {
-    Configuration conf = SparkListener.getConfigForRDD(rdd);
-    if (conf == null) {
+  protected static Path getOutputPath(RDD<?> rdd, Configuration config) {
+    if (config == null) {
       return null;
     }
     // "new" mapred api
-    Path path = org.apache.hadoop.mapred.FileOutputFormat.getOutputPath(new JobConf(conf));
+    JobConf jc;
+    if (config instanceof JobConf) {
+      jc = (JobConf) config;
+    } else {
+      jc = new JobConf(config);
+    }
+    Path path = org.apache.hadoop.mapred.FileOutputFormat.getOutputPath(jc);
     if (path == null) {
       try {
         // old fashioned mapreduce api
-        path = org.apache.hadoop.mapreduce.lib.output.FileOutputFormat.getOutputPath(new Job(conf));
+        path = org.apache.hadoop.mapreduce.lib.output.FileOutputFormat.getOutputPath(new Job(jc));
       } catch (IOException exception) {
         exception.printStackTrace(System.out);
       }
