e9d79f64a926161c9fc3fc9cbe2d632df7efef03
Willy Lulciuc <willy@datakin.com>
2021-08-20 16:23:35 -0700

    Add api and admin port overrides in docker/up.sh (#1583)
    * Add api and admin port overrides in docker/up.sh
    
    Signed-off-by: wslulciuc <willy@datakin.com>

--- docker/up.sh
+++ docker/up.sh
@@ -11,17 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Usage: $ ./up.sh [--tag TAG] [--build] [--seed]
 
 set -e
 
+title() {
+  echo -e "\033[1m${1}\033[0m"
+}
+
 usage() {
-  echo "usage: ./$(basename -- ${0}) [--tag TAG] [--build] [--seed]"
-  echo
+  echo "usage: ./$(basename -- ${0}) [--api-port PORT] [--web-port PORT] [--tag TAG] [--build] [--seed]"
   echo "A script used to run Marquez via Docker"
   echo
-  echo "Examples:"
+  title "EXAMPLES:"
   echo "  # Build image from source"
   echo "  $ ./up.sh --build"
   echo
@@ -34,10 +35,17 @@ usage() {
   echo "  # Use tagged image, then seed HTTP API server with metadata"
   echo "  ./up.sh --tag X.Y.X --seed"
   echo
+  echo "  # Set HTTP API server port"
+  echo "  ./up.sh --api-port 9000"
   echo
-  echo "Arguments:"
-  echo "  -t, --tag string      image tag (default: latest)"
-  echo "  -b, --build           build image from source"
+  title "ARGUMENTS:"
+  echo "  -a, --api-port int          api port (default: 5000)"
+  echo "  -m, --api-admin-port int    api admin port (default: 5001)"
+  echo "  -w, --web-port int          web port (default: 3000)"
+  echo "  -t, --tag string            image tag (default: latest)"
+  echo
+  title "FLAGS:"
+  echo "  -b, --build           build images from source"
   echo "  -s, --seed            seed HTTP API server with metadata"
   echo "  -h, --help            show help for script"
   exit 1
@@ -45,25 +53,40 @@ usage() {
 
 # Change working directory to project root
 project_root=$(git rev-parse --show-toplevel)
-cd "${project_root}"
+cd "${project_root}/"
 
 compose_files="-f docker-compose.yml"
 args="-V --force-recreate"
 
+API_PORT=5000
+API_ADMIN_PORT=5001
+WEB_PORT=3000
 TAG=0.16.1
 while [ $# -gt 0 ]; do
   case $1 in
-    '--tag'|-t)
+    -a|'--api-port')
+       shift
+       API_PORT="${1}"
+       ;;
+    -m|'--api-admin-port')
+       shift
+       API_ADMIN_PORT="${1}"
+       ;;
+    -w|'--web-port')
+       shift
+       WEB_PORT="${1}"
+       ;;
+    -t|'--tag')
        shift
        TAG="${1}"
        ;;
-    '--build'|-b)
+    -b|'--build')
        BUILD='true'
        ;;
-    '--seed'|-s)
+    -s|'--seed')
        SEED='true'
        ;;
-    '--help'|-h)
+    -h|'--help')
        usage
        exit 0
        ;;
@@ -83,4 +106,4 @@ if [[ "${SEED}" = "true" ]]; then
   compose_files+=" -f docker-compose.seed.yml"
 fi
 
-TAG=${TAG} docker-compose $compose_files up $args
+API_PORT=${API_PORT} API_ADMIN_PORT=${API_ADMIN_PORT} WEB_PORT=${WEB_PORT} TAG=${TAG} docker-compose $compose_files up $args
