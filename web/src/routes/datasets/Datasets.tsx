c43d74989b783421945c6d3672fb36e1c2681f28
Peter Hicks <peter@datakin.com>
2021-09-07 16:13:09 -0700

    Basic dataset view page working.
    Signed-off-by: Peter Hicks <peter@datakin.com>

--- web/src/routes/datasets/Datasets.tsx
+++ web/src/routes/datasets/Datasets.tsx
@@ -1,11 +1,15 @@
 import * as Redux from 'redux'
 import { Dataset } from '../../types/api'
 import { IState } from '../../store/reducers'
+import { Pagination } from '@material-ui/lab'
+import { Paper, Table, TableBody, TableCell, TableHead, TableRow, Tooltip } from '@material-ui/core'
 import { Theme } from '@material-ui/core/styles/createMuiTheme'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { fetchDatasets } from '../../store/actionCreators'
+import { formatUpdatedAt } from '../../helpers'
 import Box from '@material-ui/core/Box'
+import MqText from '../../components/core/text/MqText'
 import React from 'react'
 import createStyles from '@material-ui/core/styles/createStyles'
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles'
@@ -22,6 +26,8 @@ interface DispatchProps {
 
 type DatasetsProps = WithStyles<typeof styles> & StateProps & DispatchProps
 
+const DATASET_COLUMNS = ['Name', 'Namespace', 'Updated At']
+
 class Datasets extends React.Component<DatasetsProps> {
   componentDidMount() {
     this.props.fetchDatasets('food_delivery')
@@ -31,9 +37,42 @@ class Datasets extends React.Component<DatasetsProps> {
     const { datasets } = this.props
     return (
       <Box>
-        {datasets.map(dataset => {
-          return <Box key={dataset.id.name}>{dataset.name}</Box>
-        })}
+        <Box p={2}>
+          <MqText heading>Datasets</MqText>
+        </Box>
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              {DATASET_COLUMNS.map(field => {
+                return (
+                  <TableCell key={field} align='left'>
+                    <MqText subheading>{field}</MqText>
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {datasets.map(dataset => {
+              return (
+                <TableRow key={dataset.name}>
+                  <TableCell align='left'>
+                    <MqText>{dataset.name}</MqText>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <MqText>{dataset.namespace}</MqText>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <MqText>{formatUpdatedAt(dataset.updatedAt)}</MqText>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+        <Box display={'flex'} justifyContent={'flex-end'} mt={2} mr={2}>
+          <Pagination color={'standard'} shape={'rounded'} onChange={() => {}} count={10} />
+        </Box>
       </Box>
     )
   }
