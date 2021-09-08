922dda6a334bfc749009d24f1066766d0b006a7f
Peter Hicks <peter@datakin.com>
2021-09-08 13:49:29 -0700

    Storing the selected namespace in the browser.
    Signed-off-by: Peter Hicks <peter@datakin.com>

--- web/src/store/reducers/namespaces.ts
+++ web/src/store/reducers/namespaces.ts
@@ -2,27 +2,34 @@ import { Namespace } from '../../types/api'
 
 import { FETCH_NAMESPACES_SUCCESS, SELECT_NAMESPACE } from '../actionCreators/actionTypes'
 import { Nullable } from '../../types/util/Nullable'
+import { fetchNamespacesSuccess, setSelectedNode } from '../actionCreators'
+
+const selectedNamespace = window.localStorage.getItem('selectedNamespace')
 
 export type INamespacesState = { result: Namespace[]; selectedNamespace: Nullable<string> }
-const initialState: INamespacesState = { result: [], selectedNamespace: null }
-
-interface INamespacesAction {
-  type: string
-  payload: {
-    namespaces: Namespace[]
-  }
+const initialState: INamespacesState = {
+  result: [],
+  selectedNamespace: selectedNamespace ? selectedNamespace : null
 }
 
-export default (state = initialState, action: INamespacesAction) => {
+export default (
+  state = initialState,
+  action: ReturnType<typeof fetchNamespacesSuccess> & ReturnType<typeof setSelectedNode>
+) => {
   const { type, payload } = action
 
   switch (type) {
     case FETCH_NAMESPACES_SUCCESS:
       return {
         result: payload.namespaces,
-        selectedNamespace: payload.namespaces[0] ? payload.namespaces[0].name : null
+        selectedNamespace: state.selectedNamespace
+          ? state.selectedNamespace
+          : payload.namespaces[0]
+          ? payload.namespaces[0].name
+          : null
       }
     case SELECT_NAMESPACE:
+      window.localStorage.setItem('selectedNamespace', action.payload)
       return { ...state, selectedNamespace: action.payload }
     default:
       return state
