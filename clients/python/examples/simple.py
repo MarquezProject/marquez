bcd007b88219319fd15d354ea79a48d3d19389a7
wslulciuc <willy@datakin.com>
2021-09-21 14:28:52 -0700

    Add simple py client example
    Signed-off-by: wslulciuc <willy@datakin.com>

--- clients/python/examples/simple.py
+++ clients/python/examples/simple.py
@@ -1,10 +1,20 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from marquez_client import Clients
 from marquez_client.models import (
     DatasetType,
     DatasetId,
-    JobType,
-    JobId,
-    RunState
+    JobType
 )
 
 # Construct a new MarquezClient instance we'll use to collect metadata.
@@ -16,35 +26,26 @@ from marquez_client.models import (
 #     $ export MARQUEZ_URL=http://localhost:5000
 marquez_client = Clients.new_client()
 
-# Before we can begin collecting metadata, we must first create a namespace.
-# A namespace helps you organize related dataset and job metadata. Note that datasets
-# and jobs are unique within a namespace, but not across namespaces. In this example,
-# we'll use the namespace 'my-namespace'.
-marquez_client.create_namespace(
+# Create a namespace to organize related dataset and job metadata.
+namespace = marquez_client.create_namespace(
     namespace_name="my-namespace",
     owner_name="me",
-    description="My first namespace."
+    description="My first namespace!"
 )
 
-# Each dataset must be associated with a source. A source is the physical location of a
-# dataset, such as a table in a database, or a file on cloud storage. A source enables the
-# logical grouping and mapping of physical datasets to their physical source. Below, let's
-# create the source 'my-source' for the database 'mydb'.
+# Create the source 'my-source' for the database 'mydb'.
 marquez_client.create_source(
     source_type="POSTGRESQL",
     source_name="my-source",
     connection_url="jdbc:postgresql://localhost:5431/mydb",
-    description="My first source."
+    description="My first source!"
 )
 
-# Next, we need to create the dataset 'my-dataset' and associate it with the existing source 'my-source'.
-# In Marquez, datasets have both a logical and physical name. The logical name is how your dataset is
-# known to Marquez, while the physical name is how your dataset is known to your source. In this example,
-# we refer to 'my-dataset' as the logical name and 'public.mytable' (format: 'schema.table') as the physical name.
-dataset = marquez_client.create_dataset(
+# Create the dataset 'my-dataset' and associate it with the existing source 'my-source'.
+marquez_client.create_dataset(
     namespace_name="my-namespace",
     dataset_type=DatasetType.DB_TABLE,
-    dataset_name="my-dataset",
+    dataset_name="my-dataset", # You can also use the physical name as the dataset name.
     dataset_physical_name="public.mytable",
     source_name="my-source",
     fields=[
@@ -52,13 +53,29 @@ dataset = marquez_client.create_dataset(
         {'name': 'field1', 'type': 'INTEGER'},
         {'name': 'field2', 'type': 'STRING'}
     ],
-    description="My first dataset."
+    description="My first dataset!"
 )
 
-# ...
-dataset_version = marquez_client.get_dataset_version(
+# With 'my-dataset' in Marquez, we can collect metadata for the job 'my-job'.
+marquez_client.create_job(
     namespace_name="my-namespace",
-    dataset_name="my-dataset",
-    version=dataset["currentVersion"]
+    job_type=JobType.BATCH,
+    job_name="my-job",
+    inputs= [
+      DatasetId("my-namespace", "my-dataset")
+    ],
+    outputs=[],
+    description="My first job!"
 )
-println(dataset_version)
+
+# Now, let's create a run for 'my-job' and capture any runtime arguments:
+active_run = marquez_client.create_job_run(
+    namespace_name="my-namespace",
+    job_name="my-job",
+    mark_as_running=True
+)
+
+# ID of active run to mark as 'COMPLETE'.
+run_id = active_run['id']
+# Mark the run for `my-job` as 'COMPLETE'.
+marquez_client.mark_job_run_as_completed(run_id=run_id)
