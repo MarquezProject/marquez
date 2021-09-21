60cb312e4a963a53cbaa9522ee00b6fc75bf03d8
wslulciuc <willy@datakin.com>
2021-09-21 15:33:06 -0700

    Use single qoutes in example
    Signed-off-by: wslulciuc <willy@datakin.com>

--- clients/python/examples/simple.py
+++ clients/python/examples/simple.py
@@ -28,50 +28,50 @@ marquez_client = Clients.new_client()
 
 # Create a namespace to organize related dataset and job metadata.
 namespace = marquez_client.create_namespace(
-    namespace_name="my-namespace",
-    owner_name="me",
-    description="My first namespace!"
+    namespace_name='my-namespace',
+    owner_name='me',
+    description='My first namespace!'
 )
 
 # Create the source 'my-source' for the database 'mydb'.
 marquez_client.create_source(
-    source_type="POSTGRESQL",
-    source_name="my-source",
-    connection_url="jdbc:postgresql://localhost:5431/mydb",
-    description="My first source!"
+    source_type='POSTGRESQL',
+    source_name='my-source',
+    connection_url='jdbc:postgresql://localhost:5431/mydb',
+    description='My first source!'
 )
 
 # Create the dataset 'my-dataset' and associate it with the existing source 'my-source'.
 marquez_client.create_dataset(
-    namespace_name="my-namespace",
+    namespace_name='my-namespace',
     dataset_type=DatasetType.DB_TABLE,
-    dataset_name="my-dataset",  # You can also use the physical name as the dataset name.
-    dataset_physical_name="public.mytable",
-    source_name="my-source",
+    dataset_name='my-dataset',  # You can also use the physical name as the dataset name.
+    dataset_physical_name='public.mytable',
+    source_name='my-source',
     fields=[
         {'name': 'field0', 'type': 'INTEGER'},
         {'name': 'field1', 'type': 'INTEGER'},
         {'name': 'field2', 'type': 'STRING'}
     ],
-    description="My first dataset!"
+    description='My first dataset!'
 )
 
 # With 'my-dataset' in Marquez, we can collect metadata for the job 'my-job'.
 marquez_client.create_job(
-    namespace_name="my-namespace",
+    namespace_name='my-namespace',
     job_type=JobType.BATCH,
-    job_name="my-job",
+    job_name='my-job',
     inputs=[
-      DatasetId("my-namespace", "my-dataset")
+      DatasetId('my-namespace', 'my-dataset')
     ],
     outputs=[],
-    description="My first job!"
+    description='My first job!'
 )
 
 # Now, let's create a run for 'my-job' and mark the run as 'RUNNING'.
 active_run = marquez_client.create_job_run(
-    namespace_name="my-namespace",
-    job_name="my-job",
+    namespace_name='my-namespace',
+    job_name='my-job',
     mark_as_running=True
 )
 
