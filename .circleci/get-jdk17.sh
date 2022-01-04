bf108976b6c393f04825686e3677c57b496eddb0
Luke Singham <lukesingham@gmail.com>
2022-01-04 18:45:19 +0000

    Use JDK 17 in circleci
    Signed-off-by: Luke Singham <lukesingham@gmail.com>
    
    Signed-off-by: Luke Singham <lukesingham@gmail.com>

--- .circleci/get-jdk17.sh
+++ .circleci/get-jdk17.sh
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Usage: $ ./get-jdk11.sh
+# Usage: $ ./get-jdk17.sh
 
 set -e
 
-sudo apt-get update && sudo apt-get install openjdk-11-jdk
-sudo update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac
+sudo apt-get update && sudo apt-get install openjdk-17-jdk
+sudo update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-17-openjdk-amd64/bin/javac
 java -version
 
 echo "DONE!"
