917b0054ce362b369de037553c7a805c70280884
wslulciuc <willy@datakin.com>
2022-01-11 11:07:41 -0800

    continued: Fix up.sh for airflow integration
    Signed-off-by: wslulciuc <willy@datakin.com>

--- integrations/airflow/tests/integration/docker/up.sh
+++ integrations/airflow/tests/integration/docker/up.sh
@@ -34,6 +34,8 @@ echo "${MARQUEZ_AIRFLOW_WHL_ALL}" > requirements.txt
 
 # Add revision to integration-requirements.txt
 cat > integration-requirements.txt <<EOL
+psycopg2-binary==2.8.6
+retrying==1.3.3
 ${MARQUEZ_AIRFLOW_WHL}
 EOL
 
