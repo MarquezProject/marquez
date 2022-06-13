46b7ec0ae7f5cbdadd32a96c4b4c329e3b7feb55
Michael Robinson <merobi@gmail.com>
2022-06-13 15:42:25 -0400

    add copyright and license lines to Java files
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- clients/java/src/main/java/marquez/client/Clients.java
+++ clients/java/src/main/java/marquez/client/Clients.java
@@ -1,4 +1,8 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+/* 
+ * Copyright 2018-2022 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 
 package marquez.client;
 
