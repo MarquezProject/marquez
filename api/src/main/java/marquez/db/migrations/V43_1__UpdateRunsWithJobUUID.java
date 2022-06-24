2282f97b94cb2db1d1c755ac060fe076340ecdd6
Michael Robinson <68482867+merobi-hub@users.noreply.github.com>
2022-06-24 14:15:54 -0400

    add copyright lines to all source files (#1996)
    * add copyright and license lines to Java files
    
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    
    * add copyright and license lines to Bash files
    
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    
    * add copyright and license lines to all Python files
    
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    
    * run spotless to fix issues in Java files
    
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    
    * rerun spotless to fix more issues
    
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    
    Co-authored-by: Willy Lulciuc <willy@datakin.com>

--- api/src/main/java/marquez/db/migrations/V43_1__UpdateRunsWithJobUUID.java
+++ api/src/main/java/marquez/db/migrations/V43_1__UpdateRunsWithJobUUID.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018-2022 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package marquez.db.migrations;
 
 import java.sql.Connection;
--- api/src/test/java/marquez/db/migrations/V44_1__BackfillAirflowParentRunsTest.java
+++ api/src/test/java/marquez/db/migrations/V44_1__BackfillAirflowParentRunsTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018-2022 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package marquez.db.migrations;
 
 import static marquez.db.LineageTestUtils.NAMESPACE;
