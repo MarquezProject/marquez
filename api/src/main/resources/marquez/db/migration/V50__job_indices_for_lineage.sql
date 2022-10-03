b9053d80cd1236b7131a7cf8aff14c5d72353744
phix <peter.hicks@astronomer.io>
2022-10-03 16:05:13 -0700

    Adding lineage indices.

---
+++ api/src/main/resources/marquez/db/migration/V50__job_indices_for_lineage.sql
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+create index jobs_current_version_uuid_index
+    on jobs (current_version_uuid);
+
+create index jobs_symlink_target_uuid_index
+    on jobs (symlink_target_uuid);
+
