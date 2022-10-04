8a35583c0685ea7460fcf7d470ccf2211233729c
phix <peter.hicks@astronomer.io>
2022-10-04 11:00:43 -0700

    Adding another index.

--- api/src/main/resources/marquez/db/migration/V50__job_indices_for_lineage.sql
+++ api/src/main/resources/marquez/db/migration/V50__job_indices_for_lineage.sql
@@ -6,3 +6,5 @@ create index jobs_current_version_uuid_index
 create index jobs_symlink_target_uuid_index
     on jobs (symlink_target_uuid);
 
+create index jobs_current_job_context_uuid_index
+    on jobs (current_job_context_uuid);
