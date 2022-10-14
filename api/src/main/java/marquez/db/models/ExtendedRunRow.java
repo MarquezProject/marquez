cd2c1110352efe8fa756a5c1a2a3d6f1ac6f7030
Minkyu Park <minkyu.park.200@gmail.com>
2022-10-14 13:32:46 -0700

    Include external_id column to ExtendedRunRow model. (#2190)
    Signed-off-by: Minkyu Park <minkyu.park.200@gmail.com>
    
    Signed-off-by: Minkyu Park <minkyu.park.200@gmail.com>

--- api/src/main/java/marquez/db/models/ExtendedRunRow.java
+++ api/src/main/java/marquez/db/models/ExtendedRunRow.java
@@ -23,6 +23,7 @@ public class ExtendedRunRow extends RunRow {
   @Getter private final String namespaceName;
   @Getter private final String jobName;
   @Getter private final String args;
+  @Getter private final String externalId;
 
   public ExtendedRunRow(
       final UUID uuid,
@@ -32,6 +33,7 @@ public class ExtendedRunRow extends RunRow {
       @Nullable final UUID jobVersionUuid,
       @Nullable final UUID parentRunUuid,
       final UUID runArgsUuid,
+      final String externalId,
       final List<DatasetVersionId> inputVersions,
       final List<DatasetVersionId> outputVersions,
       @Nullable final Instant nominalStartTime,
@@ -64,6 +66,7 @@ public class ExtendedRunRow extends RunRow {
     this.args = args;
     this.jobName = jobName;
     this.namespaceName = namespaceName;
+    this.externalId = externalId;
   }
 
   public boolean hasInputVersionUuids() {
