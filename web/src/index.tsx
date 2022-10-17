969dba22a22a987b78d2922b0b0b5d2e9f299495
phix <peter.hicks@astronomer.io>
2022-10-17 12:02:13 -0700

    Allowing to build with node 18. Removing extra library.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-require('../node_modules/react-vis/dist/style.css')
 import App from './components/App'
 
 // fonts
-import './fonts/SourceCodePro-Bold.ttf'
-import './fonts/SourceCodePro-Regular.ttf'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
