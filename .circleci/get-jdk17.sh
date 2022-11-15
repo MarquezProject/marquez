e17536130e0809225fe9be3b3588fc65aaab6b84
Willy Lulciuc <willy@datakin.com>
2022-11-15 00:13:17 -0800

    Set `--allow-releaseinfo-change` (#2247)
    * Set `--allow-releaseinfo-change`
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Set `--allow-releaseinfo-change`
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Set `--allow-releaseinfo-change`
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Enable debug
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Enable debug
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Set `--allow-releaseinfo-change`
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    Signed-off-by: wslulciuc <willy@datakin.com>

--- .circleci/get-jdk17.sh
+++ .circleci/get-jdk17.sh
@@ -14,11 +14,9 @@
 #
 # Usage: $ ./get-jdk17.sh
 
-set -e
-
 wget -qO - https://adoptium.jfrog.io/adoptium/api/gpg/key/public | sudo apt-key add -
 sudo add-apt-repository --yes https://adoptium.jfrog.io/adoptium/deb
-sudo apt-get update && sudo apt-get install temurin-17-jdk
+sudo apt-get update --allow-releaseinfo-change && sudo apt-get install --yes temurin-17-jdk
 sudo update-alternatives --set java /usr/lib/jvm/temurin-17-jdk-amd64/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/temurin-17-jdk-amd64/bin/javac
 java -version
