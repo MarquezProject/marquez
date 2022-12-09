60ede8c84bffa684f3c29fad823f64c7f0078215
Prachi Mishra <prachi.mishra@astronomer.io>
2022-12-09 15:37:28 -0800

    removing backfill step
    Signed-off-by: Prachi Mishra <prachi.mishra@astronomer.io>

--- api/src/main/resources/marquez/db/migration/V54__lineage_events_created_at_indexed.sql
+++ api/src/main/resources/marquez/db/migration/V54__lineage_events_created_at_indexed.sql
@@ -1,9 +1,7 @@
+-- new column will be created with 'null' filled for existing rows
 ALTER TABLE lineage_events ADD created_at TIMESTAMP;
 
 create index lineage_events_created_at_index on lineage_events (created_at desc);
 
-UPDATE lineage_events
-SET created_at = (event_time AT TIME ZONE 'UTC')::timestamp;
-
+-- The new default set to UTC now() will only apply in subsequent INSERT or UPDATE commands; it does not cause rows already in the table to change.
 ALTER TABLE lineage_events ALTER COLUMN created_at SET DEFAULT (now() AT TIME ZONE 'UTC');
-
