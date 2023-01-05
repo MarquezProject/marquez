e68a0ace9815577ceaf7e6e6718ebb3e4ef1a063
wslulciuc <willy@datakin.com>
2023-01-05 00:03:42 -0800

    continued: Apply minor formatting
    Signed-off-by: wslulciuc <willy@datakin.com>

--- .circleci/db-migration.sh
+++ .circleci/db-migration.sh
@@ -62,7 +62,7 @@ if ! ./docker/up.sh \
   exit 1
 fi
 
-# Query, then display any additional schema migration applied on backup (if any)
+# Query, then display additional schema migration applied on backup (if any)
 query_db_migration
 
 log "DONE!"
