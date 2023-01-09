228b9357974dc83f27decd71929657a070577ab6
Michael Robinson <merobi@gmail.com>
2023-01-09 13:48:19 -0500

    update spdx headers in web
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- web/src/components/datasets/DatasetInfo.tsx
+++ web/src/components/datasets/DatasetInfo.tsx
@@ -1,3 +1,4 @@
+// Copyright 2018-2023 contributors to the Marquez project
 // SPDX-License-Identifier: Apache-2.0
 
 import { Box, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
