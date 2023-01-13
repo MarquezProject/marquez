9389660c354b9239105bc7cd2b59cfd5c1930193
wslulciuc <willy@datakin.com>
2023-01-12 23:14:50 -0800

    Add `load-test-api` CI job
    Signed-off-by: wslulciuc <willy@datakin.com>

---
+++ .circleci/api-load-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright 2018-2023 contributors to the Marquez project
+# SPDX-License-Identifier: Apache-2.0
+#
+# A script used in CI to load test HTTP API by:
+#   (1) ...
+#   (2) ...
+#   (3) ...
+#
+# Usage: $ ./api-load-test.sh
+
+echo "./api-load-test.sh"
+
+ls
