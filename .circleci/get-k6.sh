5acfcb45534a0b4988dc6341d1f6f963354f89a3
wslulciuc <willy@datakin.com>
2023-01-12 23:42:37 -0800

    Add `get-k6.sh`
    Signed-off-by: wslulciuc <willy@datakin.com>

---
+++ .circleci/get-k6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright 2018-2023 contributors to the Marquez project
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: $ ./get-jdk17.sh
+
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-get update
+sudo apt-get install k6
+
+echo "DONE!"
