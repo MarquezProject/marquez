4d03949c913e6c066c17b6e694150f628030fbda
Perttu Salonen <perttu.salonen@gmail.com>
2023-04-03 10:49:14 +0300

    generate default color with `NEW` `runState`
    Signed-off-by: Perttu Salonen <perttu.salonen@gmail.com>

--- web/src/routes/jobs/Jobs.tsx
+++ web/src/routes/jobs/Jobs.tsx
@@ -118,7 +118,7 @@ class Jobs extends React.Component<JobsProps> {
                           </TableCell>
                           <TableCell key={i18next.t('jobs_route.latest_run_col')} align='left'>
                             <MqStatus
-                              color={job.latestRun && runStateColor(job.latestRun.state)}
+                              color={job.latestRun && runStateColor(job.latestRun.state || 'NEW')}
                               label={job.latestRun && job.latestRun.state ? job.latestRun.state : 'N/A'}
                             />
                           </TableCell>
