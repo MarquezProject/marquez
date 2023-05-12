944c9485c161fc30d447fa5424a8be4310cfeb51
wslulciuc <willy@datakin.com>
2023-05-12 13:31:23 -0700

    Add docs to `DbException` and `DbRetentionException`
    Signed-off-by: wslulciuc <willy@datakin.com>

--- api/src/main/java/marquez/db/exceptions/DbException.java
+++ api/src/main/java/marquez/db/exceptions/DbException.java
@@ -7,10 +7,22 @@ package marquez.db.exceptions;
 
 import javax.annotation.Nullable;
 
+/** An exception thrown to indicate a database error. */
 public class DbException extends Exception {
   private static final long serialVersionUID = 1L;
 
+  /** Constructs a {@code DbException} with the provided {@code message}. */
   public DbException(@Nullable final String message) {
     super(message);
+  }
+
+  /** Constructs a {@code DbException} with the provided {@code cause}. */
+  DbException(@Nullable final Throwable cause) {
+    super(cause);
+  }
+
+  /** Constructs a {@code DbException} with the provided {@code message} and the {@code cause}. */
+  DbException(@Nullable final String message, @Nullable final Throwable cause) {
+    super(message, cause);
   }
 }
--- api/src/main/java/marquez/db/exceptions/DbRetentionException.java
+++ api/src/main/java/marquez/db/exceptions/DbRetentionException.java
@@ -7,7 +7,10 @@ package marquez.db.exceptions;
 
 import javax.annotation.Nullable;
 
+/** An exception thrown to indicate a database retention policy error. */
 public final class DbRetentionException extends DbException {
+
+  /** Constructs a {@code DbRetentionException} with the provided {@code message}. */
   public DbRetentionException(@Nullable String message) {
     super(message);
   }
