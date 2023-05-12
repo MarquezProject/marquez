7be8ea1958269c87454f467a6ba4b3d97b2ee1a5
wslulciuc <willy@datakin.com>
2023-05-12 12:57:16 -0700

    continued: Add docs to `DbRetention`
    Signed-off-by: wslulciuc <willy@datakin.com>

--- api/src/test/java/marquez/db/DbRetentionTest.java
+++ api/src/test/java/marquez/db/DbRetentionTest.java
@@ -1,2 +1,8 @@
-package marquez.db;public class DbRetentionTest {
-}
+/*
+ * Copyright 2018-2023 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package marquez.db;
+
+public class DbRetentionTest {}
