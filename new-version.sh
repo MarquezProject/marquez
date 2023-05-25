14dee0b9a111197da5f91278dddff835e30a3685
Willy Lulciuc <willy@datakin.com>
2023-05-25 01:10:59 -0700

    Remove volumes on `docker/down.sh` (#2491)
    * Remove volumes on `docker/down.sh`
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * Fix api load test
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    ---------
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    Signed-off-by: Xavier-Cliquennois <xavier.cliquennois@wearegraphite.io>

--- new-version.sh
+++ new-version.sh
@@ -132,7 +132,6 @@ sed -i "" -E -e "/postgresql/,\$b" -e "s/tag:.*/tag: ${RELEASE_VERSION}/g" ./cha
 # (3) Bump version in scripts
 sed -i "" "s/VERSION=.*/VERSION=${RELEASE_VERSION}/g" ./docker/up.sh
 sed -i "" "s/MARQUEZ_VERSION=.*/MARQUEZ_VERSION=${RELEASE_VERSION}/g" ./.circleci/db-migration.sh
-sed -i "" "s/MARQUEZ_VERSION=.*/MARQUEZ_VERSION=${RELEASE_VERSION}/g" ./.circleci/api-load-test.sh
 sed -i "" "s/TAG=.*/TAG=${RELEASE_VERSION}/g" .env.example
 
 # (4) Bump version in docs
@@ -163,6 +162,7 @@ if [[ "${NEXT_VERSION}" == *-rc.? ||
 fi
 sed -i "" "s/version=.*/version=${NEXT_VERSION}/g" gradle.properties
 sed -i "" "s/^  version:.*/  version: ${NEXT_VERSION}/g" ./spec/openapi.yml
+sed -i "" "s/MARQUEZ_VERSION=.*/MARQUEZ_VERSION=${NEXT_VERSION}/g" ./.circleci/api-load-test.sh
 
 # (9) Prepare next development version commit
 git commit -sam "Prepare next development version ${NEXT_VERSION}" --no-verify
