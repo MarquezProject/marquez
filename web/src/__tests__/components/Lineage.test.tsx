0148af895204e36ba7f267bc1b8d5460af77d78c
John Lukenoff <johnlukenoff@asana.com>
2023-06-09 09:47:34 -0700

    Formatting
    Signed-off-by: John Lukenoff <johnlukenoff@asana.com>

--- web/src/__tests__/components/Lineage.test.tsx
+++ web/src/__tests__/components/Lineage.test.tsx
@@ -6,91 +6,91 @@ import { LineageNode } from '../../components/lineage/types'
 import { LineageGraph } from '../../types/api'
 
 const mockGraphWithCycle = [
-    {
-      id: 'job_foo',
-      inEdges: [
-        {
-          origin: 'dataset_foo',
-          destination: 'job_foo'
-        }
-      ],
-      outEdges: [
-        {
-          origin: 'job_foo',
-          destination: 'dataset_bar'
-        }
-      ]
-    },
-    {
-      id: 'dataset_bar',
-      inEdges: [
-        {
-          origin: 'job_foo',
-          destination: 'dataset_bar'
-        }
-      ],
-      outEdges: [
-        {
-          origin: 'dataset_bar',
-          destination: 'job_bar'
-        }
-      ]
-    },
-    {
-      id: 'job_bar',
-      inEdges: [
-        {
-          origin: 'dataset_bar',
-          destination: 'job_bar'
-        }
-      ],
-      outEdges: [
-        {
-          origin: 'job_bar',
-          destination: 'dataset_foo'
-        }
-      ]
-    },
-    {
-      id: 'dataset_foo',
-      inEdges: [
-        {
-          origin: 'job_bar',
-          destination: 'dataset_foo'
-        }
-      ],
-      outEdges: [
-        {
-          origin: 'dataset_foo',
-          destination: 'job_foo'
-        }
-      ]
-    }
-  ]
+  {
+    id: 'job_foo',
+    inEdges: [
+      {
+        origin: 'dataset_foo',
+        destination: 'job_foo'
+      }
+    ],
+    outEdges: [
+      {
+        origin: 'job_foo',
+        destination: 'dataset_bar'
+      }
+    ]
+  },
+  {
+    id: 'dataset_bar',
+    inEdges: [
+      {
+        origin: 'job_foo',
+        destination: 'dataset_bar'
+      }
+    ],
+    outEdges: [
+      {
+        origin: 'dataset_bar',
+        destination: 'job_bar'
+      }
+    ]
+  },
+  {
+    id: 'job_bar',
+    inEdges: [
+      {
+        origin: 'dataset_bar',
+        destination: 'job_bar'
+      }
+    ],
+    outEdges: [
+      {
+        origin: 'job_bar',
+        destination: 'dataset_foo'
+      }
+    ]
+  },
+  {
+    id: 'dataset_foo',
+    inEdges: [
+      {
+        origin: 'job_bar',
+        destination: 'dataset_foo'
+      }
+    ],
+    outEdges: [
+      {
+        origin: 'dataset_foo',
+        destination: 'job_foo'
+      }
+    ]
+  }
+]
 
 function mockSetState(newState: any) {
-    this.state = {...this.state, ...newState}
+  this.state = { ...this.state, ...newState }
 }
 
 describe('Lineage Component', () => {
- let instance: Lineage
+  let instance: Lineage
 
   beforeEach(() => {
-      instance = new Lineage({selectedNode: "job_foo"} as unknown as LineageProps)
-      instance.setState = mockSetState
-      instance.initGraph()
-      instance.buildGraphAll(mockGraphWithCycle as unknown as LineageNode[])
+    instance = new Lineage(({ selectedNode: 'job_foo' } as unknown) as LineageProps)
+    instance.setState = mockSetState
+    instance.initGraph()
+    instance.buildGraphAll((mockGraphWithCycle as unknown) as LineageNode[])
   })
 
   it("doesn't follow cycles in the lineage graph", () => {
     const paths = instance.getSelectedPaths()
 
-      const pathCounts = paths.reduce((acc, p) => {
-        const pathId = p.join(":")
-          acc[pathId] = acc[pathId] ? 1 : acc[pathId];
-          return acc;
-      }, {} as Record<string, number>)
+    const pathCounts = paths.reduce((acc, p) => {
+      const pathId = p.join(':')
+      acc[pathId] = acc[pathId] ? 1 : acc[pathId]
+      return acc
+    }, {} as Record<string, number>)
 
-      expect(Object.values(pathCounts).some(c => c > 2)).toBe(false);
+    expect(Object.values(pathCounts).some(c => c > 2)).toBe(false)
   })
 })
