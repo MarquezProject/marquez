48825700bfca9d0c1a93bf0ac2c640c1acf9c24e
wslulciuc <willy@datakin.com>
2023-07-05 13:31:08 +0200

    Add `DbTest`
    Signed-off-by: wslulciuc <willy@datakin.com>

---
+++ api/src/test/java/marquez/db/DbTest.java
@@ -0,0 +1,54 @@
+package marquez.db;
+
+import javax.sql.DataSource;
+import org.jdbi.v3.jackson2.Jackson2Plugin;
+import org.jdbi.v3.postgres.PostgresPlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jdbi.v3.testing.junit5.JdbiExtension;
+import org.jdbi.v3.testing.junit5.tc.JdbiTestcontainersExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * The base class for tests needing to interact with database. A {@code postgres} container is
+ * managed automatically and started only once for a given test suite. Note, the container will be
+ * shared between test methods. When querying the test database, use the {@code DB} instance.
+ *
+ * <p>After the underlying {@code postgres} container starts, but before a given test suite is
+ * executed, the latest {@code flyway} migrations for Marquez will be applied to the database using
+ * {@link DbMigration#migrateDbOrError(DataSource)}
+ */
+@Tag("DataAccessTests")
+@Testcontainers
+class DbTest {
+  private static final DockerImageName POSTGRES_12_1 = DockerImageName.parse("postgres:12.1");
+
+  @Container
+  private static final PostgreSQLContainer<?> DB_CONTAINER =
+      new PostgreSQLContainer<>(POSTGRES_12_1);
+
+  @RegisterExtension
+  static final JdbiExtension jdbiExtension =
+      JdbiTestcontainersExtension.instance(DB_CONTAINER)
+          .withPlugin(new SqlObjectPlugin())
+          .withPlugin(new PostgresPlugin())
+          .withPlugin(new Jackson2Plugin())
+          .withInitializer(
+              (source, handle) -> {
+                // Apply migrations.
+                DbMigration.migrateDbOrError(source);
+              });
+
+  // An open connection to test database.
+  static TestingDb DB;
+
+  @BeforeAll
+  public static void setUpOnce() {
+    DB = TestingDb.newInstance(jdbiExtension.getJdbi());
+  }
+}
