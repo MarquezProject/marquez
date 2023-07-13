c13ad1192822c5d71b3c852a9139d3b38b9ebf32
phix <peter.hicks@astronomer.io>
2023-07-13 09:44:11 -0700

    If not exists.

--- api/src/main/resources/marquez/db/migration/V62__run_state_indices.sql
+++ api/src/main/resources/marquez/db/migration/V62__run_state_indices.sql
@@ -1,5 +1,4 @@
-create index run_states_uuid_index on run_states(run_uuid);
-create index parent_run_uuid_index on runs(parent_run_uuid);
-
-create index start_run_state_uuid_index on runs(start_run_state_uuid);
-create index end_run_state_uuid_index on runs(end_run_state_uuid);
+create index if not exists run_states_uuid_index on run_states(run_uuid);
+create index if not exists parent_run_uuid_index on runs(parent_run_uuid);
+create index if not exists start_run_state_uuid_index on runs(start_run_state_uuid);
+create index if not exists end_run_state_uuid_index on runs(end_run_state_uuid);
