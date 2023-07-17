381de6b331de923a8c958ba8c1c2aa05007d6fa7
phix <peter.hicks@astronomer.io>
2023-07-17 13:08:57 -0700

    Renaming indices

--- api/src/main/resources/marquez/db/migration/V62__index_run_states_and_parent_run.sql
+++ api/src/main/resources/marquez/db/migration/V62__index_run_states_and_parent_run.sql
@@ -1,4 +1,4 @@
 create index if not exists run_states_uuid_index on run_states(run_uuid);
-create index if not exists parent_run_uuid_index on runs(parent_run_uuid);
-create index if not exists start_run_state_uuid_index on runs(start_run_state_uuid);
-create index if not exists end_run_state_uuid_index on runs(end_run_state_uuid);
+create index if not exists runs_parent_run_uuid_index on runs(parent_run_uuid);
+create index if not exists runs_start_run_state_uuid_index on runs(start_run_state_uuid);
+create index if not exists runs_end_run_state_uuid_index on runs(end_run_state_uuid);
