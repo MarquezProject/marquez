985d33530bf3de92f1c3386bb8a28277e1d8c5d8
Amanda Yao <amanda.yao@berkeley.edu>
2023-08-10 15:59:45 -0700

    fixed prettier
    Signed-off-by: Amanda Yao <amanda.yao@berkeley.edu>

--- web/src/components/core/copy/MqCopy.tsx
+++ web/src/components/core/copy/MqCopy.tsx
@@ -1,49 +1,49 @@
 // Copyright 2018-2023 contributors to the Marquez project
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react'
+import { Snackbar, Tooltip } from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import IconButton from '@mui/material/IconButton'
-import { Tooltip, Snackbar } from '@mui/material'
+import React from 'react'
 
 interface MqCopyProps {
-    string: string
+  string: string
 }
 
 const MqEmpty: React.FC<MqCopyProps> = ({ string }) => {
-    const [open, setOpen] = React.useState(false)
-    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return
-        }
-
-        setOpen(false)
+  const [open, setOpen] = React.useState(false)
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
     }
-    return (
-        <>
-            <Tooltip title='Copy'>
-                <IconButton
-                    onClick={(event) => {
-                        event.stopPropagation()
-                        navigator.clipboard.writeText(string)
-                        setOpen(true)
-                    }}
-                    aria-label='copy'
-                    size={'small'}
-                    color={'primary'}
-                >
-                    <ContentCopyIcon fontSize={'small'} />
-                </IconButton>
-            </Tooltip>
-            <Snackbar
-                open={open}
-                autoHideDuration={2000}
-                onClose={handleClose}
-                message={`Copied ${string}`}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-            />
-        </>
-    )
+
+    setOpen(false)
+  }
+  return (
+    <>
+      <Tooltip title='Copy'>
+        <IconButton
+          onClick={(event) => {
+            event.stopPropagation()
+            navigator.clipboard.writeText(string)
+            setOpen(true)
+          }}
+          aria-label='copy'
+          size={'small'}
+          color={'primary'}
+        >
+          <ContentCopyIcon fontSize={'small'} />
+        </IconButton>
+      </Tooltip>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message={`Copied ${string}`}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      />
+    </>
+  )
 }
 
 export default MqEmpty
