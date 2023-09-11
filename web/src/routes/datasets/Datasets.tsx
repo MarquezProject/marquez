4d0482ec274f29606b236ead39af0f5404aead1e
Xavier-Cliquennois <Xavier-Cliquennois@users.noreply.github.com>
2023-09-11 15:58:36 +0200

    feat: :children_crossing: Use DataGrid instead of table in dataset page.

--- web/src/routes/datasets/Datasets.tsx
+++ web/src/routes/datasets/Datasets.tsx
@@ -131,8 +131,7 @@ const Datasets: React.FC<DatasetsProps> = ({
     {
       field: 'name',
       headerName: i18next.t('datasets_route.name_col'),
-      renderCell: (params) => {
-        return <MqText
+      renderCell: (params) => (<MqText
           link
           linkTo={`/lineage/${encodeNode(
             'DATASET',
@@ -141,33 +140,31 @@ const Datasets: React.FC<DatasetsProps> = ({
           )}`}
         >
           {params.row.name}
-        </MqText>
-      },
-      width: 400
+      </MqText>),
+      flex: 2,
+      editable: false,
     },
-    { field: 'namespace', headerName: i18next.t('datasets_route.namespace_col'), width: 200 },
-    { field: 'sourceName', headerName: i18next.t('datasets_route.source_col'), width: 200 },
+    { field: 'namespace', headerName: i18next.t('datasets_route.namespace_col'), flex: 1, editable: false, },
+    { field: 'sourceName', headerName: i18next.t('datasets_route.source_col'), flex: 1, editable: false, },
     {
       field: 'updatedAt',
       headerName: i18next.t('datasets_route.updated_col'),
-      renderCell: (params) => {
-        return <MqText>{formatUpdatedAt(params.row.updatedAt)}</MqText>
-      },
-      width: 200
+      renderCell: (params) => (<MqText>{formatUpdatedAt(params.row.updatedAt)}</MqText>),
+      flex: 1,
+      editable: false,
     },
     {
       field: 'facets',
       headerName: i18next.t('datasets_route.status_col'),
-      renderCell: (params) => {
-        return (datasetFacetsStatus(params.row.facets) ? (
+      renderCell: (params) => (datasetFacetsStatus(params.row.facets) ? (
           <>
             <MqStatus color={datasetFacetsStatus(params.row.facets)} />
           </>
         ) : (
           <MqText>N/A</MqText>
-        ))
-      },
-      width: 200
+      )),
+      flex: 1,
+      editable: false,
     }
   ]
 
