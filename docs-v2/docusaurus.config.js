a2466d9895ddd01681751bd1f34c7676c8d31131
Michael Robinson <merobi@gmail.com>
2023-10-02 16:09:08 -0400

    Configures docusaurus for test deployment on netlify.
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- docs-v2/docusaurus.config.js
+++ docs-v2/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
 
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://peppy-sprite-186812.netlify.app',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
