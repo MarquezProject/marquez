c43345dc1d83ef1d9243bcbcbbc4ed9b32a517c5
Michael Robinson <merobi@gmail.com>
2023-10-03 06:37:22 -0400

    Changes site social card - continued.
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- docs-v2/docusaurus.config.js
+++ docs-v2/docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
                 disableSwitch: true
             },
             // Replace with your project's social card
-            image: 'img/marquez-social-card-name.jpg',
+            image: 'img/marquez-social-card.jpg',
             navbar: {
                 logo: {
                     alt: 'My Site Logo',
