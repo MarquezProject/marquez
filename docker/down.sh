649e5d0588b34d6f9f27a5f16aca651855a96f08
merobi-hub <merobi@gmail.com>
2023-11-05 08:24:46 -0500

    Adds no-compose option and arg and help arg to docker/down.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- docker/down.sh
+++ docker/down.sh
@@ -14,6 +14,7 @@ title() {
 usage() {
   echo "usage: ./$(basename -- ${0})"
   echo "A script used to bring down Marquez when run via Docker"
+  echo "If not using Docker Compose, use -v or --volumes"
   echo
 }
 
@@ -22,14 +23,40 @@ project_root=$(git rev-parse --show-toplevel)
 cd "${project_root}/"
 
 compose_files="-f docker-compose.yml"
-args="--remove-orphans"
+compose_args="--remove-orphans"
+
+# Default args
+NO_COMPOSE="false"
+
+# Parse args
+while [ $# -gt 0 ]; do
+  case $1 in
+    -v|'--volumes')
+       shift
+       NO_COMPOSE="true"
+       ;;
+    -h|'--help')
+       usage
+       exit 0
+       ;;
+  esac
+  shift
+done
 
 # We can ignore the tag and port(s) when cleaning up running
 # containers and volumes
 TAG=any
 
-API_PORT=${RANDOM} API_ADMIN_PORT=${RANDOM} WEB_PORT=${RANDOM} TAG=${RANDOM} docker-compose $compose_files down $args && \
-  docker volume rm marquez_data && \
+API_PORT=${RANDOM}
+API_ADMIN_PORT=${RANDOM}
+WEB_PORT=${RANDOM}
+TAG=${RANDOM}
+
+if [[ "${NO_COMPOSE}" = "false" ]]; then
+  docker-compose $compose_files down $compose_args
+fi
+
+docker volume rm marquez_data && \
   docker volume rm marquez_db-backup && \
   docker volume rm marquez_db-conf && \
   docker volume rm marquez_db-init
