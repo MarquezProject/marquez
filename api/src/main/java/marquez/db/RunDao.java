94f528ca49628fbd43bf93baa9679ca02c5bf2ba
Michael Robinson <merobi@gmail.com>
2023-11-06 07:18:52 -0500

    Fixes formatting issues.
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- api/src/main/java/marquez/db/RunDao.java
+++ api/src/main/java/marquez/db/RunDao.java
@@ -192,7 +192,7 @@ public interface RunDao extends BaseDao {
   List<Run> findAll(String namespace, String jobName, int limit, int offset);
 
   @SqlQuery(
-    "SELECT count(*) FROM runs_view AS r WHERE r.job_name = :jobName")
+      "SELECT count(*) FROM runs_view AS r WHERE r.job_name = :jobName")
   int countFor(String jobName);
 
   @SqlQuery(
