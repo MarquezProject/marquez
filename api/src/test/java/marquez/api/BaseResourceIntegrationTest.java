4194d284087e856b8d1b30675bca65adac5199c1
David Sharp <34074888+davidsharp7@users.noreply.github.com>
2023-12-16 07:08:57 +1300

    add DELETE end point for dataset tags (#2698)
    * add DELETE end point for dataset tags
    
    Signed-off-by: sharpd <number6labs@gmail.com>
    
    * ran code linter to re-format
    
    Signed-off-by: sharpd <number6labs@gmail.com>
    
    * add integration tests and Marquez client
    
    Signed-off-by: sharpd <number6labs@gmail.com>
    
    ---------
    
    Signed-off-by: sharpd <number6labs@gmail.com>
    Co-authored-by: sharpd <number6labs@gmail.com>

--- api/src/test/java/marquez/api/BaseResourceIntegrationTest.java
+++ api/src/test/java/marquez/api/BaseResourceIntegrationTest.java
@@ -5,9 +5,16 @@
 
 package marquez.api;
 
+import static marquez.common.models.CommonModelGenerator.newConnectionUrlFor;
+import static marquez.common.models.CommonModelGenerator.newDatasetName;
+import static marquez.common.models.CommonModelGenerator.newDbSourceType;
+import static marquez.common.models.CommonModelGenerator.newDescription;
 import static marquez.common.models.CommonModelGenerator.newNamespaceName;
+import static marquez.common.models.CommonModelGenerator.newOwnerName;
+import static marquez.common.models.CommonModelGenerator.newSourceName;
 import static marquez.db.DbTest.POSTGRES_14;
 
+import com.google.common.collect.ImmutableSet;
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
@@ -16,11 +23,17 @@ import io.openlineage.client.OpenLineageClient;
 import io.openlineage.client.transports.HttpTransport;
 import java.net.URI;
 import java.net.URL;
+import java.util.Set;
 import marquez.MarquezApp;
 import marquez.MarquezConfig;
 import marquez.client.MarquezClient;
 import marquez.client.Utils;
+import marquez.client.models.DatasetId;
+import marquez.client.models.DbTableMeta;
+import marquez.client.models.NamespaceMeta;
+import marquez.client.models.SourceMeta;
 import marquez.client.models.Tag;
+import marquez.common.models.SourceType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -56,17 +69,61 @@ abstract class BaseResourceIntegrationTest {
   static final Tag PII = new Tag("PII", "Personally identifiable information");
   static final Tag SENSITIVE = new Tag("SENSITIVE", "Contains sensitive information");
 
+  // Namespace
   static String NAMESPACE_NAME;
-  static DropwizardAppExtension<MarquezConfig> MARQUEZ_APP;
+  static String NAMESPACE_DESCRIPTION;
+  static String OWNER_NAME;
 
+  // Source
+  static String SOURCE_TYPE;
+  static String SOURCE_NAME;
+  static URI CONNECTION_URL;
+  static String SOURCE_DESCRIPTION;
+
+  // Dataset
+  static String DB_TABLE_SOURCE_TYPE;
+  static String DB_TABLE_SOURCE_NAME;
+  static URI DB_TABLE_CONNECTION_URL;
+  static String DB_TABLE_SOURCE_DESCRIPTION;
+  static DatasetId DB_TABLE_ID;
+  static String DB_TABLE_NAME;
+  static String DB_TABLE_PHYSICAL_NAME;
+  static String DB_TABLE_DESCRIPTION;
+  static Set<String> DB_TABLE_TAGS;
+  static DbTableMeta DB_TABLE_META;
+
+  static DropwizardAppExtension<MarquezConfig> MARQUEZ_APP;
   static OpenLineage OL;
   static OpenLineageClient OL_CLIENT;
   static MarquezClient MARQUEZ_CLIENT;
 
   @BeforeAll
   public static void setUpOnce() throws Exception {
-    // (1) Use a randomly generated namespace.
+    // (1) Use a randomly generated namespace/dataset
     NAMESPACE_NAME = newNamespaceName().getValue();
+    NAMESPACE_DESCRIPTION = newDescription();
+    OWNER_NAME = newOwnerName().getValue();
+
+    SOURCE_TYPE = newDbSourceType().getValue();
+    SOURCE_NAME = newSourceName().getValue();
+    SOURCE_DESCRIPTION = newDescription();
+
+    DB_TABLE_SOURCE_TYPE = SourceType.of("POSTGRESQL").getValue();
+    DB_TABLE_SOURCE_NAME = SOURCE_NAME;
+    DB_TABLE_SOURCE_DESCRIPTION = newDescription();
+    DB_TABLE_ID = newDatasetIdWith(NAMESPACE_NAME);
+    DB_TABLE_NAME = DB_TABLE_ID.getName();
+    DB_TABLE_PHYSICAL_NAME = DB_TABLE_NAME;
+    DB_TABLE_DESCRIPTION = newDescription();
+    DB_TABLE_TAGS = ImmutableSet.of(PII.getName());
+    DB_TABLE_CONNECTION_URL = newConnectionUrlFor(SourceType.of("POSTGRESQL"));
+    DB_TABLE_META =
+        DbTableMeta.builder()
+            .physicalName(DB_TABLE_PHYSICAL_NAME)
+            .sourceName(DB_TABLE_SOURCE_NAME)
+            .tags(DB_TABLE_TAGS)
+            .description(DB_TABLE_DESCRIPTION)
+            .build();
 
     // (2) Configure Marquez application using test configuration and database.
     MARQUEZ_APP =
@@ -87,6 +144,28 @@ abstract class BaseResourceIntegrationTest {
             .transport(HttpTransport.builder().uri(url.toURI()).build())
             .build();
     MARQUEZ_CLIENT = MarquezClient.builder().baseUrl(url).build();
+  }
+
+  protected void createNamespace(String namespaceName) {
+    // (1) Create namespace for db table
+    final NamespaceMeta namespaceMeta =
+        NamespaceMeta.builder().ownerName(OWNER_NAME).description(NAMESPACE_DESCRIPTION).build();
+
+    MARQUEZ_CLIENT.createNamespace(namespaceName, namespaceMeta);
+  }
+
+  protected static DatasetId newDatasetIdWith(final String namespaceName) {
+    return new DatasetId(namespaceName, newDatasetName().getValue());
+  }
+
+  protected void createSource(String sourceName) {
+    final SourceMeta sourceMeta =
+        SourceMeta.builder()
+            .type(DB_TABLE_SOURCE_TYPE)
+            .connectionUrl(DB_TABLE_CONNECTION_URL)
+            .description(DB_TABLE_SOURCE_DESCRIPTION)
+            .build();
+    MARQUEZ_CLIENT.createSource(sourceName, sourceMeta);
   }
 
   @AfterAll
