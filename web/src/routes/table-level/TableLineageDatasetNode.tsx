afbe960e3b4ee0ac64676fb64c0b3947f03d6294
phix <peter.hicks@astronomer.io>
2024-02-21 18:13:59 -0800

    Making display better.

--- web/src/routes/table-level/TableLineageDatasetNode.tsx
+++ web/src/routes/table-level/TableLineageDatasetNode.tsx
@@ -62,98 +62,109 @@ const TableLineageDatasetNode = ({ node }: TableLineageDatasetNodeProps & StateP
   }
 
   return (
-    <MQTooltip title={addToToolTip(node.data.dataset)}>
-      <g>
-        <Box
-          component={'rect'}
-          sx={{
-            x: 0,
-            y: 0,
-            width: node.width,
-            height: node.height,
-            filter: isSelected ? `drop-shadow( 0 0 4px ${theme.palette.primary.main})` : 'none',
-            rx: 4,
-            fill: theme.palette.background.paper,
-            cursor: 'pointer',
-            transition: 'all 0.3',
-          }}
-          cursor={'pointer'}
-          onClick={handleClick}
-        />
-        <Box
-          component={'rect'}
-          x={0}
-          y={0}
-          height={24}
-          width={24}
-          sx={{ rx: 4, fill: theme.palette.info.main }}
-        />
-        <FontAwesomeIcon
-          aria-hidden={'true'}
-          title={'Job'}
-          icon={faDatabase}
-          width={ICON_SIZE}
-          height={ICON_SIZE}
-          x={6}
-          y={ICON_SIZE / 2}
-          color={theme.palette.common.white}
-          cursor={'pointer'}
-          onClick={handleClick}
-        />
-        <foreignObject width={16} height={24} x={node.width - 18} y={0}>
-          <MQTooltip title={isCollapsed ? 'Expand' : 'Collapse'} placement={'top'}>
-            <IconButton
-              sx={{ width: 16, height: 16 }}
-              onClick={(event) => {
-                event.stopPropagation()
-                const collapsedNodes = searchParams.get('collapsedNodes')
-                if (collapsedNodes) {
-                  const collapsedNodesArray = collapsedNodes.split(',')
-                  if (collapsedNodesArray.includes(node.id)) {
-                    collapsedNodesArray.splice(collapsedNodesArray.indexOf(node.id), 1)
-                  } else {
-                    collapsedNodesArray.push(node.id)
-                  }
-                  searchParams.set('collapsedNodes', collapsedNodesArray.toString())
+    <g>
+      <Box
+        component={'rect'}
+        sx={{
+          x: 0,
+          y: 0,
+          width: node.width,
+          height: node.height,
+          filter: isSelected ? `drop-shadow( 0 0 4px ${theme.palette.primary.main})` : 'none',
+          rx: 4,
+          fill: theme.palette.background.paper,
+          cursor: 'pointer',
+          transition: 'all 0.3',
+        }}
+        cursor={'pointer'}
+        onClick={handleClick}
+      />
+      <Box
+        component={'rect'}
+        x={0}
+        y={0}
+        height={24}
+        width={24}
+        sx={{ rx: 4, fill: theme.palette.info.main }}
+      />
+
+      <FontAwesomeIcon
+        aria-hidden={'true'}
+        title={'Job'}
+        icon={faDatabase}
+        width={ICON_SIZE}
+        height={ICON_SIZE}
+        x={6}
+        y={ICON_SIZE / 2}
+        color={theme.palette.common.white}
+        onClick={handleClick}
+      />
+      <foreignObject width={16} height={24} x={node.width - 18} y={0}>
+        <MQTooltip title={isCollapsed ? 'Expand' : 'Collapse'} placement={'top'}>
+          <IconButton
+            sx={{ width: 10, height: 10 }}
+            onClick={(event) => {
+              event.stopPropagation()
+              const collapsedNodes = searchParams.get('collapsedNodes')
+              if (collapsedNodes) {
+                const collapsedNodesArray = collapsedNodes.split(',')
+                if (collapsedNodesArray.includes(node.id)) {
+                  collapsedNodesArray.splice(collapsedNodesArray.indexOf(node.id), 1)
                 } else {
-                  searchParams.set('collapsedNodes', node.id)
+                  collapsedNodesArray.push(node.id)
                 }
-                setSearchParams(searchParams)
+                searchParams.set('collapsedNodes', collapsedNodesArray.toString())
+              } else {
+                searchParams.set('collapsedNodes', node.id)
+              }
+              setSearchParams(searchParams)
+            }}
+          >
+            <ChevronLeft
+              sx={{
+                width: 10,
+                height: 10,
+                rotate: !isCollapsed ? '-90deg' : 0,
+                transition: 'rotate .3s',
               }}
+            />
+          </IconButton>
+        </MQTooltip>
+      </foreignObject>
+      <MQTooltip title={addToToolTip(node.data.dataset)}>
+        <g>
+          <text
+            fontSize='8'
+            fontFamily={`${'Source Code Pro'}, mono`}
+            fill={'white'}
+            x={28}
+            y={10}
+            onClick={handleClick}
+            cursor={'pointer'}
+          >
+            DATASET
+          </text>
+          <text fontSize='8' fill={'white'} x={28} y={20} cursor={'pointer'} onClick={handleClick}>
+            {truncateText(node.data.dataset.name, 15)}
+          </text>
+        </g>
+      </MQTooltip>
+
+      {!isCompact &&
+        node.data.dataset.fields.map((field, index) => {
+          return (
+            <text
+              key={field.name}
+              fontSize='8'
+              fill={THEME_EXTRA.typography.subdued}
+              x={10}
+              y={14 + 10 + 10 * (index + 1)}
             >
-              <ChevronLeft
-                sx={{
-                  width: 16,
-                  height: 16,
-                  rotate: !isCollapsed ? '-90deg' : 0,
-                  transition: 'rotate .3s',
-                }}
-              />
-            </IconButton>
-          </MQTooltip>
-        </foreignObject>
-        <text fontSize='8' fill={'white'} x={28} y={10} onClick={handleClick} cursor={'pointer'}>
-          DATASET
-        </text>
-        <text fontSize='8' fill={'white'} x={28} y={20} cursor={'pointer'} onClick={handleClick}>
-          {truncateText(node.data.dataset.name, 15)}
-        </text>
-        {!isCompact &&
-          node.data.dataset.fields.map((field, index) => {
-            return (
-              <text
-                key={field.name}
-                fontSize='8'
-                fill={THEME_EXTRA.typography.subdued}
-                x={10}
-                y={14 + 10 + 10 * (index + 1)}
-              >
-                - {truncateText(field.name, 20)}
-              </text>
-            )
-          })}
-      </g>
-    </MQTooltip>
+              - {truncateText(field.name, 20)}
+            </text>
+          )
+        })}
+    </g>
   )
 }
 
