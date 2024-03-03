61696354202da143b75f4ccd470d9b2c0fa7c87e
merobi-hub <merobi@gmail.com>
2024-03-03 15:18:38 -0500

    Vectorized screencaps in banner and fixed media query.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- docs/v2/src/pages/index.tsx
+++ docs/v2/src/pages/index.tsx
@@ -8,48 +8,48 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 import HomepageTextBox from "@site/src/components/HomepageTextBox";
 
+
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <header className={clsx('hero', 'dashed', styles.heroBanner)}>
-            <div>
-                <div className={styles.flexContainer}>
-                    
-                    <div className={styles.titleContainer}>
-                        <img className={styles.logo} src="img/logo_txt_grn.svg"/>
-                        <p className="hero__subtitle">{siteConfig.tagline}</p>
-                        <div className={styles.buttons}>
-                            <Link
-                                className="button button--secondary button--md"
-                                href="https://github.com/MarquezProject/marquez">
-                                Quickstart
-                            </Link>
-                            <Link
-                                className="button button--secondary button--md margin-left--md"
-                                href="https://github.com/MarquezProject/marquez">
-                                <img 
-                                    className={styles.btn_logos} 
-                                    src="img/github.svg" 
-                                    alt="GitHub logo" 
-                                />
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GitHub
-                            </Link>
-                            <Link
-                                className="button button--secondary button--md margin-left--md"
-                                href="https://bit.ly/Mqz_invite">
-                                <img 
-                                    className={styles.btn_logos} 
-                                    src="img/slack.svg" 
-                                    alt="Slack logo" 
-                                />
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Slack
-                            </Link>
-                        </div>
-                    </div>
-                    <div className={styles.imageContainer}>
-                        <img className={styles.image} src="img/marquez_caps_triplet.png" alt="Lineage Diagram"/>
-                    </div>
+            <div className={styles.titleContainer}>
+                <img className={styles.logo} src="img/logo_txt_grn.svg"/>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--md"
+                        href="https://github.com/MarquezProject/marquez">
+                        Quickstart
+                    </Link>
+                    <Link
+                        className="button button--secondary button--md margin-left--md"
+                        href="https://github.com/MarquezProject/marquez">
+                        <img 
+                            className={styles.btn_logos} 
+                            src="img/github.svg" 
+                            alt="GitHub logo" 
+                        />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GitHub
+                    </Link>
+                    <Link
+                        className="button button--secondary button--md margin-left--md"
+                        href="https://bit.ly/Mqz_invite">
+                        <img 
+                            className={styles.btn_logos} 
+                            src="img/slack.svg" 
+                            alt="Slack logo" 
+                        />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Slack
+                    </Link>
                 </div>
+            </div>
+            <div className={styles.imageSubDiv}>
+                <img 
+                    className={styles.image} 
+                    src="img/marquez_caps_couple.svg" 
+                    alt="Marquez UI Screencaps"
+                />
             </div>
         </header>
     );
