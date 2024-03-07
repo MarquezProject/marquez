a9e2b649d582a81048016ee3e712cc2451f5cbec
merobi-hub <merobi@gmail.com>
2024-03-07 13:14:33 -0500

    HomepageTextBox edits.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- docs/v2/src/components/HomepageTextBox/index.tsx
+++ docs/v2/src/components/HomepageTextBox/index.tsx
@@ -8,7 +8,7 @@ export default function HomepageTextBox(): JSX.Element {
             <div className="container">
                 <div>
                     <h1 className="text--center">One Source of Truth</h1>
-                    <p className={clsx("text--center", styles.constrain)}>Marquez enables consuming, storing, and visualizing OpenLineage metadata from across an organization, serving use cases including data governance, data quality, and performance analytics.</p>
+                    <p className={clsx("text--center", styles.constrain)}>Marquez enables consuming, storing, and visualizing <a target={"_blank"} href={"https://openlineage.io"}>OpenLineage</a> metadata from across an organization, serving use cases including data governance, data quality monitoring, and performance analytics.</p>
                     <h5 className="text--center">Marquez was released and open sourced by <a target={"_blank"} href={"https://wework.com"}>WeWork</a>.</h5>
                 </div>
             </div>
