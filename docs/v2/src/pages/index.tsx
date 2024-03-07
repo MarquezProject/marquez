fd4eab9e9262dfc44253ebc956f1da68a69f9269
merobi-hub <merobi@gmail.com>
2024-03-07 16:07:42 -0500

    Fix broken link in index.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- docs/v2/src/pages/index.tsx
+++ docs/v2/src/pages/index.tsx
@@ -23,7 +23,7 @@ function HomepageHeader() {
                         <div className={styles.buttons}>
                             <Link
                                 className="button button--secondary button--md"
-                                href="https://github.com/MarquezProject/marquez">
+                                href="/docs/quickstart">
                                 Quickstart
                             </Link>
                             <Link
