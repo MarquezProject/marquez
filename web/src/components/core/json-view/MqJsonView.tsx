67e3922f81741f2c1a6393bd71ad21e22c47eb61
phix <peter.hicks@astronomer.io>
2024-05-01 15:24:58 -0700

    Updating colors for json theme.

--- web/src/components/core/json-view/MqJsonView.tsx
+++ web/src/components/core/json-view/MqJsonView.tsx
@@ -1,28 +1,39 @@
 // SPDX-License-Identifier: Apache-2.0
 import { Box } from '@mui/system'
-import { darkTheme } from '@uiw/react-json-view/dark'
-import { theme } from '../../../helpers/theme'
+import { THEME_EXTRA, theme } from '../../../helpers/theme'
+import { githubDarkTheme } from '@uiw/react-json-view/githubDark'
 import JsonView from '@uiw/react-json-view'
 import React from 'react'
 
 interface OwnProps {
   data: object
-  searchable?: boolean
-  placeholder?: string
 }
 
 type JsonViewProps = OwnProps
 
-darkTheme.background = theme.palette.background.default
-darkTheme.backgroundColor = theme.palette.background.default
-darkTheme.borderLeftWidth = 2
-darkTheme.borderLeftColor = theme.palette.grey[500]
-darkTheme.borderLeftStyle = 'dashed'
+githubDarkTheme.background = theme.palette.background.default
+githubDarkTheme.backgroundColor = theme.palette.background.default
+githubDarkTheme.borderLeftWidth = 2
+githubDarkTheme.borderLeftColor = theme.palette.grey[500]
+githubDarkTheme.borderLeftStyle = 'dashed'
+
+const mqTheme = {
+  ...githubDarkTheme,
+  '--w-rjv-info-color': THEME_EXTRA.typography.subdued,
+  '--w-rjv-type-null-color': theme.palette.warning.main,
+  '--w-rjv-type-boolean-color': theme.palette.error.main,
+  '--w-rjv-copied-color': theme.palette.primary.main,
+  '--w-rjv-key-string': theme.palette.common.white,
+  '--w-rjv-type-string-color': theme.palette.info.main,
+  '--w-rjv-ellipsis-color': theme.palette.info.main,
+  '--w-rjv-key-number': theme.palette.primary.main,
+  '--w-rjv-type-float-color': theme.palette.primary.main,
+}
 
 const MqJsonView: React.FC<JsonViewProps> = ({ data }) => {
   return (
     <Box my={2}>
-      <JsonView style={darkTheme} collapsed={2} value={data} />
+      <JsonView style={mqTheme} collapsed={2} value={data} />
     </Box>
   )
 }
