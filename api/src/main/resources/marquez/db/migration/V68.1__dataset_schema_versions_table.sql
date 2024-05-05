031c529712e8147670e050921fda58456980e42d
David Goss <david@davidgoss.co>
2024-05-05 08:12:37 +0100

    feat: add ON CASCADE DELETE clause so cleanup works
    Signed-off-by: David Goss <david@davidgoss.co>

--- api/src/main/resources/marquez/db/migration/V68.1__dataset_schema_versions_table.sql
+++ api/src/main/resources/marquez/db/migration/V68.1__dataset_schema_versions_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dataset_schema_versions(
     uuid         UUID PRIMARY KEY,
-    dataset_uuid UUID REFERENCES datasets(uuid),
+    dataset_uuid UUID REFERENCES datasets(uuid) ON DELETE CASCADE,
     created_at   TIMESTAMPTZ NOT NULL
 );
--- api/src/main/resources/marquez/db/migration/V68.2__dataset_schema_versions_field_mapping_table.sql
+++ api/src/main/resources/marquez/db/migration/V68.2__dataset_schema_versions_field_mapping_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dataset_schema_versions_field_mapping(
-    dataset_schema_version_uuid UUID REFERENCES dataset_schema_versions(uuid),
-    dataset_field_uuid          UUID REFERENCES dataset_fields(uuid),
+    dataset_schema_version_uuid UUID REFERENCES dataset_schema_versions(uuid) ON DELETE CASCADE,
+    dataset_field_uuid          UUID REFERENCES dataset_fields(uuid) ON DELETE CASCADE,
     PRIMARY KEY (dataset_schema_version_uuid, dataset_field_uuid)
 );
