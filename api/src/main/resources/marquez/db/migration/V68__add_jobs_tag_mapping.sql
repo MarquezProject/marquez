10703c14d7244e8fcad19d05a803c23106977b8d
sharpd <davidsharp7@gmail.com>
2024-05-07 18:39:48 +1200

    correct db field error
    Signed-off-by: sharpd <davidsharp7@gmail.com>

--- api/src/main/resources/marquez/db/migration/V68__add_jobs_tag_mapping.sql
+++ api/src/main/resources/marquez/db/migration/V68__add_jobs_tag_mapping.sql
@@ -2,7 +2,7 @@
 CREATE TABLE jobs_tag_mapping (
   job_uuid UUID REFERENCES jobs(uuid),
   tag_uuid     UUID REFERENCES tags(uuid),
-  tagged_at    TIMESTAMPZ NOT NULL,
+  tagged_at    TIMESTAMPTZ NOT NULL,
   PRIMARY KEY (tag_uuid, job_uuid)
 );
 
