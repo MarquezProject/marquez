a618f93c5baa7c6402e2710d4efb034ee977d469
David Goss <david@davidgoss.co>
2024-05-24 11:26:26 +0100

    chore: update flyway version and add license comment
    Signed-off-by: David Goss <david@davidgoss.co>

--- api/src/main/resources/marquez/db/migration/V69.1__dataset_schema_versions_table.sql
+++ api/src/main/resources/marquez/db/migration/V69.1__dataset_schema_versions_table.sql
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 CREATE TABLE dataset_schema_versions(
     uuid         UUID PRIMARY KEY,
     dataset_uuid UUID REFERENCES datasets(uuid) ON DELETE CASCADE,
--- api/src/main/resources/marquez/db/migration/V69.2__dataset_schema_versions_field_mapping_table.sql
+++ api/src/main/resources/marquez/db/migration/V69.2__dataset_schema_versions_field_mapping_table.sql
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 CREATE TABLE dataset_schema_versions_field_mapping(
     dataset_schema_version_uuid UUID REFERENCES dataset_schema_versions(uuid) ON DELETE CASCADE,
     dataset_field_uuid          UUID REFERENCES dataset_fields(uuid) ON DELETE CASCADE,
