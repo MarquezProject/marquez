4158d54d8c7dc5d33e11e56be9e8854abc7e5045
phixMe <peter.hicks@pdtechsolutions.com>
2024-08-06 16:28:01 -0700

    Adding header

--- api/src/main/java/marquez/api/OpenSearchResource.java
+++ api/src/main/java/marquez/api/OpenSearchResource.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018-2024 contributors to the Marquez project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package marquez.api;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
