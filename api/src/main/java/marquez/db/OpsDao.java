b563c8dd92affdcf904feedf03dd7c8820686234
phixMe <peter.hicks@pdtechsolutions.com>
2024-08-26 23:14:18 -0700

    Weekly metric query.

--- api/src/main/java/marquez/db/OpsDao.java
+++ api/src/main/java/marquez/db/OpsDao.java
@@ -44,6 +44,39 @@ public interface OpsDao extends BaseDao {
               """)
   List<LineageMetric> getLastDayMetrics();
 
+  @SqlQuery(
+          """
+                  SELECT
+                      gs.start_interval AS start_interval,
+                      gs.start_interval + INTERVAL '1 day' AS end_interval,
+                      COALESCE(COUNT(dom.id) FILTER (WHERE dom.state = 'FAIL'), 0) AS fail,
+                      COALESCE(COUNT(dom.id) FILTER (WHERE dom.state = 'START'), 0) AS start,
+                      COALESCE(COUNT(dom.id) FILTER (WHERE dom.state = 'COMPLETE'), 0) AS complete,
+                      COALESCE(COUNT(dom.id) FILTER (WHERE dom.state = 'ABORT'), 0) AS abort
+                  FROM
+                      generate_series(
+                              DATE_TRUNC('day', NOW() - INTERVAL '7 days'),  -- Start 7 days ago, truncated to the day
+                              DATE_TRUNC('day', NOW() - INTERVAL '1 day'),   -- End at the start of the current day
+                              '1 day'::interval                             -- Step by 1 day
+                      ) AS gs(start_interval)
+                          LEFT JOIN (
+                          SELECT
+                              id,
+                              DATE_TRUNC('day', metric_time) AS metric_day,
+                              state
+                          FROM
+                              data_ops_lineage_metrics
+                          WHERE
+                              metric_time >= NOW() - INTERVAL '7 days'
+                      ) dom ON gs.start_interval = dom.metric_day
+                  GROUP BY
+                      gs.start_interval
+                  ORDER BY
+                      gs.start_interval;
+                  """
+  )
+  List<LineageMetric> getLastWeekMetrics();
+
   @SqlUpdate(
       """
               INSERT INTO data_ops_lineage_metrics (metric_time, state)
