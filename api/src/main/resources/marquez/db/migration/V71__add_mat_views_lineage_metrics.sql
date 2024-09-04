bb5d79fd01ec0593106c635a8500437980573be3
phixMe <peter.hicks@pdtechsolutions.com>
2024-09-04 15:02:11 -0700

    Fixing table name.

--- api/src/main/resources/marquez/db/migration/V71__add_mat_views_lineage_metrics.sql
+++ api/src/main/resources/marquez/db/migration/V71__add_mat_views_lineage_metrics.sql
@@ -10,7 +10,7 @@ GROUP BY DATE_TRUNC('hour', event_time);
 CREATE INDEX IF NOT EXISTS idx_metrics_hourly_start_interval
     ON lineage_metrics_hourly (start_interval);
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS data_ops_lineage_metrics_daily AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS lineage_metrics_daily AS
 SELECT DATE_TRUNC('day', event_time)                   AS start_interval,
        COUNT(*) FILTER (WHERE event_type = 'FAIL')     AS fail,
        COUNT(*) FILTER (WHERE event_type = 'START')    AS start,
@@ -20,4 +20,4 @@ FROM lineage_events
 GROUP BY DATE_TRUNC('day', event_time);
 
 CREATE INDEX IF NOT EXISTS idx_metrics_daily_start_interval
-    ON data_ops_lineage_metrics_daily (start_interval);
+    ON lineage_metrics_daily (start_interval);
