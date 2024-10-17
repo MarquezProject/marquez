caa49cf096d781e034f2d1b387f8bc1b7160bd84
Willy Lulciuc <willy.lulciuc@gmail.com>
2024-10-17 12:45:13 -0700

    Order by started_at (desc)
    Signed-off-by: Willy Lulciuc <willy.lulciuc@gmail.com>

--- api/src/main/resources/marquez/db/migration/V74__alter_jobs_to_add_current_run_uuid.sql
+++ api/src/main/resources/marquez/db/migration/V74__alter_jobs_to_add_current_run_uuid.sql
@@ -3,12 +3,11 @@ CREATE INDEX jobs_current_run_uuid_idx ON jobs(current_run_uuid);
 
 -- Backfill current runs for jobs
 WITH latest_runs AS (
-    SELECT DISTINCT ON (job_uuid)
-        job_uuid, uuid AS current_run_uuid
+  SELECT DISTINCT ON (job_uuid) job_uuid, transitioned_at, uuid AS current_run_uuid
     FROM runs
-    ORDER BY job_uuid, transitioned_at DESC
+   ORDER BY job_uuid, transitioned_at DESC, started_at DESC
 )
 UPDATE jobs AS j
-SET current_run_uuid = lr.current_run_uuid
-FROM latest_runs AS lr
-WHERE j.uuid = lr.job_uuid;
+   SET current_run_uuid = lr.current_run_uuid
+  FROM latest_runs AS lr
+ WHERE j.uuid = lr.job_uuid;
