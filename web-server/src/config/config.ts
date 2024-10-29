d7b5c99decc7178eff8b85de9d70014e2c2ae35e
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-10-29 18:49:23 -0300

    Implementing https and removing hard-coded endpoints

--- web-server/src/config/config.ts
+++ web-server/src/config/config.ts
@@ -2,14 +2,14 @@ const config = {
     saml: {
         cert: './src/config/saml.pem',
         entryPoint: 'https://nubank.okta.com/app/nubank_marquezui_1/exk1zathd6ug5a1dY0h8/sso/saml',
-        issuer: 'http://localhost:1337',
+        issuer: `http://${process.env.MARQUEZ_WEB_AUTH_SERVER_HOST}:${process.env.MARQUEZ_WEB_AUTH_SERVER_PORT}`,
         options: {
             failureRedirect: '/login',
             failureFlash: true
         }
     },
     server: {
-        port: 1337
+        port: `${process.env.MARQUEZ_WEB_AUTH_SERVER_PORT}`
     },
     session: {
         resave: false,
