2d57a18b9831870fc5c64382ba0f0df308c89cff
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-10-30 11:50:47 -0300

    Fixing local variables

--- web-server/src/config/config.ts
+++ web-server/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
     saml: {
-        cert: process.env.SAML_CERT ?? './src/config/saml.pem',
+        cert: './src/config/saml.pem',
         entryPoint: process.env.SAML_ENTRY_POINT ?? 'https://nubank.okta.com/app/nubank_marquezui_1/exk1zathd6ug5a1dY0h8/sso/saml',
         issuer: process.env.SAML_ISSUER ?? 'http://www.okta.com/exk1zathd6ug5a1dY0h8',
         options: {
