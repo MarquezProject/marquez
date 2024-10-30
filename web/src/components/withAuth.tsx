1d5c8705dc11828efa029e14f91b9c114ef34020
jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>
2024-10-30 10:48:15 -0300

    Testing routes with http in k8s

--- web/src/components/withAuth.tsx
+++ web/src/components/withAuth.tsx
@@ -12,7 +12,7 @@ const withAuth = (WrappedComponent: React.ComponentType) => {
 
       axios({
         method: 'GET',
-        url: `http://localhost:1337/whoami`,
+        url: `http://${process.env.MARQUEZ_WEB_AUTH_SERVER_HOST}:${process.env.MARQUEZ_WEB_AUTH_SERVER_PORT}/whoami`,
         withCredentials: true,
       })
         .then((response) => {
@@ -32,7 +32,7 @@ const withAuth = (WrappedComponent: React.ComponentType) => {
     }, []);
 
     const RedirectToLogin = () => {
-      window.location.replace(`http://localhost:1337/login`);
+      window.location.replace(`http://${process.env.MARQUEZ_WEB_AUTH_SERVER_HOST}:${process.env.MARQUEZ_WEB_AUTH_SERVER_PORT}/login`);
     };
 
     if (loading) return <p>Loading ...</p>;
