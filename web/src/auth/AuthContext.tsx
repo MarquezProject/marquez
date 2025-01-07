cd6e8347502cf6c4d8514a1cd1d2009bbc8d6565
Kess220 <prodvictorkaio@gmail.com>
2025-01-07 11:01:38 -0300

    add okta_auth

--- web/src/auth/AuthContext.tsx
+++ web/src/auth/AuthContext.tsx
@@ -2,10 +2,9 @@ import { AuthState, OktaAuth } from '@okta/okta-auth-js'
 import React, { createContext, useContext, useEffect, useState } from 'react'
 
 export const oktaAuth = new OktaAuth({
-  issuer: 'https://dev-15036446.okta.com/oauth2/default',
-  clientId: '0oam2eo020yRwwPbu5d7',
-  // redirectUri: window.location.origin + '/login/callback',
-  redirectUri: 'http://localhost:1337/login/callback',
+  issuer: 'https://nubank.okta.com/oauth2/default',
+  clientId: '0oa20ehmjv97g8jZP0h8',
+  redirectUri: window.location.origin + '/login/callback',
   pkce: true,
   tokenManager: {
     storage: 'localStorage',
