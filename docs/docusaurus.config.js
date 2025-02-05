59e073bf58dfe479968a6c330f2f5746b0e807de
merobi-hub <merobi@gmail.com>
2025-02-05 12:24:08 -0500

    Continued.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://marquezproject.github.io',
+    url: 'https://marquezproject.ai',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
