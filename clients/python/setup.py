e16b8038a8261450bb94ef84e88a6b71624856b6
Willy Lulciuc <willy.lulciuc@gmail.com>
2025-03-27 00:32:36 -0700

    Prepare next development version 0.51.2-SNAPSHOT
    Signed-off-by: Willy Lulciuc <willy.lulciuc@gmail.com>
    Signed-off-by: swar00pduthks <swaroopduthks@gmail.com>

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -25,7 +25,7 @@ extras_require["dev"] = set(sum(extras_require.values(), []))
 
 setup(
     name="marquez-python",
-    version="0.52.0",
+    version="0.51.2",
     description="Marquez Python Client",
     long_description=readme,
     long_description_content_type="text/markdown",
