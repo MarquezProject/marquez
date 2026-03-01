0b10a78d00cc9a530aac17eb5f1d85b27d7640ac
merobi-hub <merobi@gmail.com>
2026-02-28 21:49:29 -0500

    React in scope in index.tsx
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,7 @@
 // Copyright 2018-2026 contributors to the Marquez project
 // SPDX-License-Identifier: Apache-2.0
 
+import * as React from 'react'
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/App'
 
